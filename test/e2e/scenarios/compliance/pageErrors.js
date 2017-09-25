9f336dd0d41a7f1a23c21ff6af39d90c2a8303be
Anthony Gilks <anthonygilks@Anthonys-MacBook-Pro.local>
2017-09-25 15:53:41 +0100

    SSCS-1997-mrn-month-logic-tests

--- test/e2e/scenarios/compliance/pageErrors.js
+++ test/e2e/scenarios/compliance/pageErrors.js
@@ -1,7 +1,7 @@
 const noMRNContentErrors = require('steps/compliance/no-mrn/content').en.translation.errors;
 const mrnDateErrors = require('steps/compliance/mrn-date/content').en.translation.errors;
-// const mrnDateOverMonthErrors = require('steps/compliance/mrn-over-month-late/content').en.translation.errors;
-// const mrnDateOverThirteenMonthsLateErrors = require('steps/compliance/mrn-over-thirteen-months-late/content').resources.en.translation.errors;
+const mrnDateOverMonthErrors = require('steps/compliance/mrn-over-month-late/content').en.translation.errors;
+const mrnDateOverThirteenMonthsLateErrors = require('steps/compliance/mrn-over-thirteen-months-late/content').resources.en.translation.errors;
 
 Feature('Page errors');
 
@@ -16,23 +16,23 @@ Scenario('I have a MRN but I do not enter the day, month or the year', (I) => {
 
 });
 
-//Scenario('I have an MRN that is over one month late, I do not enter a reason why my appeal is late, I see errors', (I) => {
-//
-//    I.amOnPage('/mrn-over-one-month-late');
-//    I.click('Continue');
-//    I.seeCurrentUrlEquals('/mrn-over-one-month-late');
-//    I.see(mrnDateOverMonthErrors.reasonForMRNOverOneMonthLate.required);
-//
-//});
+Scenario('I have an MRN that is over one month late, I do not enter a reason why my appeal is late, I see errors', (I) => {
 
-//Scenario('I have an MRN that is over thirteen months late, I do not enter a reason why my appeal is late, I see errors', (I) => {
-//
-//    I.amOnPage('/mrn-over-thirteen-months-late');
-//    I.click('Continue');
-//    I.seeCurrentUrlEquals('/mrn-over-thirteen-months-late');
-//    I.see(mrnDateOverThirteenMonthsLateErrors.reasonForMRNOverThirteenMonthsLate.required);
-//
-//});
+    I.amOnPage('/mrn-over-one-month-late');
+    I.click('Continue');
+    I.seeCurrentUrlEquals('/mrn-over-one-month-late');
+    I.see(mrnDateOverMonthErrors.reasonForMRNOverOneMonthLate.required);
+
+});
+
+Scenario('I have an MRN that is over thirteen months late, I do not enter a reason why my appeal is late, I see errors', (I) => {
+
+    I.amOnPage('/mrn-over-thirteen-months-late');
+    I.click('Continue');
+    I.seeCurrentUrlEquals('/mrn-over-thirteen-months-late');
+    I.see(mrnDateOverThirteenMonthsLateErrors.reasonForMRNOverThirteenMonthsLate.required);
+
+});
 
 Scenario('I do not have an MRN, I have not entered anything, I see errors', (I) => {
 
