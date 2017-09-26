fb6735b9c0a835ddac5d46f9e7f767a8359c4cde
Ben Diggle <ben.diggle5@gmail.com>
2017-09-26 10:46:57 +0100

    SSCS-1863 SMS confirmation (#12)
    Add SMS-confirmation page.
    Add template with link to go back to the enter-mobile step and link to progress to the representative step. Display the number the user has entered. If mobile number has been entered in the /enter-mobile step then display that else display the appellant phone number entered in the appellant-details step.
    Add content to the json file.

--- steps/sms-notify/sms-confirmation/SmsConfirmation.js
+++ steps/sms-notify/sms-confirmation/SmsConfirmation.js
@@ -1,5 +1,5 @@
 const { Question, goTo } = require('@hmcts/one-per-page');
-// const content = require('./content');
+const content = require('./content');
 
 class SmsConfirmation extends Question {
 
@@ -8,17 +8,17 @@ class SmsConfirmation extends Question {
     }
 
     get template() {
-        return `sms-notify/sms-confirmation/template`;
+        return 'sms-notify/sms-confirmation/template';
     }
 
-    // get i18NextContent() {
-    //     return content;
-    // }
+    get i18NextContent() {
+        return content;
+    }
 
     get form() {}
 
     next() {
-        return goTo(undefined); // To define the next step
+        return goTo(this.journey.Representative);
     }
 }
 
