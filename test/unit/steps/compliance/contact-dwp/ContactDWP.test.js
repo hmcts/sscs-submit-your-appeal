6eaf0eaa66e086cede1cfbff9348b9f4de9328d7
Ben Diggle <ben.diggle5@gmail.com>
2017-09-28 15:19:24 +0100

    Contact-DWP unit tests (#24)
    Add unit tests for the contact-dwp step.

---
+++ test/unit/steps/compliance/contact-dwp/ContactDWP.test.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const { expect } = require('test/util/chai');
+const ContactDWP = require('steps/compliance/contact-dwp/ContactDWP');
+const content = require('steps/compliance/contact-dwp/content.json');
+const urls = require('urls');
+
+describe('ContactDWP.js', () => {
+
+    let contactDWPClass;
+
+    beforeEach(() => {
+        contactDWPClass = new ContactDWP();
+    });
+
+    after(() => {
+        contactDWPClass = undefined;
+    });
+
+    describe('get url()', () => {
+
+        it('returns url /contact-dwp', () => {
+            expect(contactDWPClass.url).to.equal(urls.compliance.contactDWP);
+        });
+
+    });
+
+    describe('get template()', () => {
+
+        it('returns template path compliance/contact-dwp/template', () => {
+            expect(contactDWPClass.template).to.equal('compliance/contact-dwp/template');
+        });
+
+    });
+
+    describe('get i18NextContent()', () => {
+
+        it('returns the correct content for the page', () => {
+            expect(contactDWPClass.i18NextContent).to.equal(content);
+        });
+
+    });
+
+});
