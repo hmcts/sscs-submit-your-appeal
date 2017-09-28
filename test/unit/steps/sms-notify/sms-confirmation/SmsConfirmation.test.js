f30d9a0942fbc49c30aa5f36586d3707f1611f9c
Ben Diggle <ben.diggle5@gmail.com>
2017-09-28 10:46:25 +0100

    Sms-confirmation unit tests (#20)
    Add unit tests for the SmsConfirmation class.

---
+++ test/unit/steps/sms-notify/sms-confirmation/SmsConfirmation.test.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const { expect } = require('test/util/chai');
+const SmsConfirmation = require('steps/sms-notify/sms-confirmation/SmsConfirmation');
+const content = require('steps/sms-notify/sms-confirmation/content.json');
+
+describe('SmsConfirmation.js', () => {
+
+    let smsConfirmationClass;
+
+    beforeEach(() => {
+        smsConfirmationClass = new SmsConfirmation();
+    });
+
+    after(() => {
+        smsConfirmationClass = undefined;
+    });
+
+    describe('get url()', () => {
+
+        it('returns url /sms-confirmation', () => {
+            expect(smsConfirmationClass.url).to.equal('/sms-confirmation');
+        });
+
+    });
+
+    describe('get template()', () => {
+
+        it('returns template path sms-notify/sms-confirmation/template', () => {
+            expect(smsConfirmationClass.template).to.equal('sms-notify/sms-confirmation/template');
+        });
+
+    });
+
+    describe('get i18NextContent()', () => {
+
+        it('returns the correct content for the page', () => {
+            expect(smsConfirmationClass.i18NextContent).to.equal(content);
+        });
+
+    });
+
+    describe('next()', () => {
+
+        it('returns the next step url /representative', () => {
+            const redirector = {
+                nextStep: '/representative'
+            };
+            smsConfirmationClass.journey = {
+                Representative: '/representative'
+            };
+            expect(smsConfirmationClass.next()).to.eql(redirector);
+        });
+
+    });
+
+});
