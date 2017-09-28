ffaf1a6ad994efab3799f5193333f0b14b572da2
Ben Diggle <ben.diggle5@gmail.com>
2017-09-28 15:34:18 +0100

    MRN over one month late unit tests (#28)
    Add unit tests for mrn over one month late page

---
+++ test/unit/steps/compliance/mrn-over-month-late/MRNOverOneMonthLate.test.js
@@ -0,0 +1,80 @@
+'use strict';
+
+const { expect } = require('test/util/chai');
+const MRNOverOneMonthLate = require('steps/compliance/mrn-over-month-late/MRNOverOneMonthLate');
+const content = require('steps/compliance/mrn-over-month-late/content.json');
+const urls = require('urls');
+
+describe('MRNOverOneMonth.js', () => {
+
+    let mrnOverOneMonthClass;
+
+    beforeEach(() => {
+        mrnOverOneMonthClass = new MRNOverOneMonthLate();
+    });
+
+    after(() => {
+        mrnOverOneMonthClass = undefined;
+    });
+
+    describe('get url()', () => {
+
+        it('returns url /mrn-over-month-late', () => {
+            expect(mrnOverOneMonthClass.url).to.equal(urls.compliance.mrnOverMonthLate);
+        });
+
+    });
+
+    describe('get template()', () => {
+
+        it('returns template path compliance/mrn-over-month-late/template', () => {
+            expect(mrnOverOneMonthClass.template).to.equal('compliance/mrn-over-month-late/template');
+        });
+
+    });
+
+    describe('get form()', () => {
+
+        let field;
+
+        beforeEach(() => {
+            field = mrnOverOneMonthClass.form.fields[0];
+        });
+
+        after(() => {
+            field = undefined;
+        });
+
+        it('contains the field name reasonForBeingLate', () => {
+            expect(field.name).to.equal('reasonForBeingLate');
+        });
+
+        it('contains field content', () => {
+            expect(field.content).to.eql(content.en.translation.fields.reasonForBeingLate);
+        });
+
+    });
+
+    describe('get i18NextContent()', () => {
+
+        it('returns the correct content for the page', () => {
+            expect(mrnOverOneMonthClass.i18NextContent).to.equal(content);
+        });
+
+    });
+
+    describe('next()', () => {
+
+        it('returns the next step url /are-you-an-appointee', () => {
+            const redirector = {
+                nextStep: urls.identity.areYouAnAppointee
+            };
+            mrnOverOneMonthClass.journey = {
+                Appointee: urls.identity.areYouAnAppointee
+            };
+            expect(mrnOverOneMonthClass.next()).to.eql(redirector);
+        });
+
+    });
+
+});
