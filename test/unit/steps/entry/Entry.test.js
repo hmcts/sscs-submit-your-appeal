7c196e84bc94a4c8b4e20d9c61d57e7302f61b50
Ben Diggle <ben.diggle5@gmail.com>
2017-10-04 15:03:20 +0100

    Entry unit tests (#35)
    Add unit tests for the entry class

---
+++ test/unit/steps/entry/Entry.test.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const { expect } = require('test/util/chai');
+const Entry = require('steps/entry/Entry');
+const urls = require('urls');
+
+describe('Entry.js', () => {
+
+    let entryClass;
+
+    beforeEach(() => {
+       entryClass = new Entry();
+    });
+
+    after(() => {
+        entryClass = undefined;
+    });
+
+    describe('get url()', () => {
+
+        it('returns url /', () => {
+            expect(entryClass.url).to.equal(urls.session.entry);
+        });
+
+    });
+
+    describe('next()', () => {
+
+        it('returns the next step url /benefit-type', () => {
+            const redirector = {
+                nextStep: urls.start.benefitType
+            };
+            entryClass.journey = {
+                BenefitType: urls.start.benefitType
+            };
+            expect(entryClass.next()).to.eql(redirector);
+        });
+
+    });
+
+});
