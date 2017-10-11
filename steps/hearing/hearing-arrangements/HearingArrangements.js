90161e65b66a60f469d933b4196535fc479393e1
Paul Gain <paul.gain@javexconsulting.com>
2017-10-11 15:58:00 +0100

    Hearing arrangements step with field validation.

--- steps/hearing/hearing-arrangements/HearingArrangements.js
+++ steps/hearing/hearing-arrangements/HearingArrangements.js
@@ -1,4 +1,6 @@
-const { Question, form } = require('@hmcts/one-per-page');
+const { Question, form, field, goTo } = require('@hmcts/one-per-page');
+const { whitelist } = require('utils/regex');
+const Joi = require('joi');
 const urls = require('urls');
 
 class HearingArrangements extends Question {
@@ -9,10 +11,31 @@ class HearingArrangements extends Question {
 
     get form() {
 
-        return form();
+        const answers = [
+            "languageInterpreter",
+            "signLanguageInterpreter",
+            "hearingLoop",
+            "disabledAccess",
+        ];
+
+        return form(
+
+            field('selection')
+                .joi(
+                    this.content.fields.selection.error.required,
+                    Joi.array().items(Joi.string().valid(answers)).single()
+                ),
+
+            field('anythingElse')
+                .joi(
+                    this.content.fields.anythingElse.error.required,
+                    Joi.string().regex(whitelist).required().allow('')
+                )
+        );
     }
 
     next() {
+        return goTo(this.journey.HearingAvailibility);
     }
 }
 
