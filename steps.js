8b6dda92fe7bc258a1f89d550c3c6886aff7fa27
bendiggle <ben.diggle5@gmail.com>
2017-10-12 11:07:28 +0100

    Add exit page back

--- steps.js
+++ steps.js
@@ -1,6 +1,6 @@
 const { concat } = require('lodash');
 const Entry = require('steps/entry/Entry');
-const Exit = require('steps/confirmation/Confirmation');
+const Exit = require('steps/exit/Exit');
 const Sessions = require('steps/session/Sessions');
 const Error404 = require('steps/errors/404');
 const Error500 = require('steps/errors/500');
