7567aeb23fcbcf35de39c198157be97ac2603a9e
Paul Gain <paul.gain@javexconsulting.com>
2017-10-19 10:33:47 +0100

    Adding accessible autocomplete and styling.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,27 @@
 import $ from 'jquery';
 import ShowHideContent from 'govuk/show-hide-content';
+import accessibleAutocomplete from 'accessible-autocomplete';
 
 $(document).ready(() => {
     initShowHideContent();
+    initAutocomplete();
 });
 
 function initShowHideContent() {
     const showHideContent = new ShowHideContent();
     showHideContent.init();
+}
+
+function initAutocomplete() {
+
+    const benefitsDropdown = $('#BenefitType_benefitType')[0];
+
+    if(!benefitsDropdown) {
+        return;
+    }
+
+    accessibleAutocomplete.enhanceSelectElement({
+        selectElement: benefitsDropdown
+    })
+
 }
