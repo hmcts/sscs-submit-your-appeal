6b3cf23e9f6845c287445285596c587ffbbfbf6c
Paul Gain <paul.gain@javexconsulting.com>
2017-10-22 18:45:42 +0100

    Send to number
    * Created a new function appellantPhoneNumber() this is accessible from
    the contnet
    * Require 'forms' from the new one-per-page/forms namespace.
    * Reference the appellant phone number from a previous step.
    * Updated unit tests.
    * Updated e2e tests that includes filling out the appellant details
    before signing up to text message reminders.

--- steps/sms-notify/send-to-number/SendToNumber.js
+++ steps/sms-notify/send-to-number/SendToNumber.js
@@ -1,4 +1,5 @@
-const { Question, form, field, branch, goTo } = require('@hmcts/one-per-page');
+const { Question, goTo, branch } = require('@hmcts/one-per-page');
+const { form, textField } = require('@hmcts/one-per-page/forms');
 const { whitelist } = require('utils/regex');
 const Joi = require('joi');
 const urls = require('urls');
@@ -10,15 +11,17 @@ class SendToNumber extends Question {
         return urls.smsNotify.sendToNumber;
     }
 
+    get appellantPhoneNumber() {
+        return this.fields.appellantPhoneNumber.value;
+    }
+
     get form() {
-
         return form(
-
-            field('useSameNumber')
-                .joi(
-                    this.content.fields.useSameNumber.error.required,
-                    Joi.string().regex(whitelist).required()
-                )
+            textField.ref(this.journey.AppellantDetails, 'appellantPhoneNumber'),
+            textField('useSameNumber').joi(
+                this.content.fields.useSameNumber.error.required,
+                Joi.string().regex(whitelist).required()
+            )
         );
     }
 
