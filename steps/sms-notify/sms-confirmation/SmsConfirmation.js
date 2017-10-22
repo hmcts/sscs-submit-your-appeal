1635811ae20d2d329ca37ce21127998cf328b66d
Paul Gain <paul.gain@javexconsulting.com>
2017-10-22 18:52:22 +0100

    SMS confirmation
    * Reference enterMobile, useSameNumber and appellantPhoneNumber fields
    from a previous step.
    * Added a new function called mobileNumber() which moves logic from the
    template into it's corresponding class, this makes it easier to test
    and reference the aforemnetioned referenced fields.
     defined above.
    * Updated unit tests
    * Updated e2e tests.

--- steps/sms-notify/sms-confirmation/SmsConfirmation.js
+++ steps/sms-notify/sms-confirmation/SmsConfirmation.js
@@ -1,5 +1,8 @@
 const { Question, goTo } = require('@hmcts/one-per-page');
+const { form, textField } = require('@hmcts/one-per-page/forms');
+const regex = require('../../../utils/regex');
 const urls = require('urls');
+const answer = require('utils/answer');
 
 class SmsConfirmation extends Question {
 
@@ -7,7 +10,25 @@ class SmsConfirmation extends Question {
         return urls.smsNotify.smsConfirmation;
     }
 
-    get form() {}
+    get mobileNumber() {
+
+        const isMobile = regex.internationalMobileNumber.test(this.fields.appellantPhoneNumber.value);
+
+        if(isMobile) {
+            return this.fields.useSameNumber.value === answer.YES ?
+                this.fields.appellantPhoneNumber.value : this.fields.enterMobile.value;
+        }
+
+        return this.fields.enterMobile.value;
+    }
+
+    get form() {
+        return form(
+            textField.ref(this.journey.EnterMobile, 'enterMobile'),
+            textField.ref(this.journey.SendToNumber, 'useSameNumber'),
+            textField.ref(this.journey.AppellantDetails, 'appellantPhoneNumber')
+        )
+    }
 
     next() {
         return goTo(this.journey.Representative);
--- test/unit/steps/sms-notify/sms-confirmation/SmsConfirmation.test.js
+++ test/unit/steps/sms-notify/sms-confirmation/SmsConfirmation.test.js
@@ -2,6 +2,8 @@
 
 const { expect } = require('test/util/chai');
 const SmsConfirmation = require('steps/sms-notify/sms-confirmation/SmsConfirmation');
+const urls = require('urls');
+const answer = require('utils/answer');
 
 describe('SmsConfirmation.js', () => {
 
@@ -9,10 +11,12 @@ describe('SmsConfirmation.js', () => {
 
     beforeEach(() => {
         smsConfirmationClass = new SmsConfirmation();
-    });
-
-    after(() => {
-        smsConfirmationClass = undefined;
+        smsConfirmationClass.journey = {};
+        smsConfirmationClass.fields = {
+            appellantPhoneNumber: {},
+            enterMobile: {},
+            useSameNumber: {}
+        };
     });
 
     describe('get url()', () => {
@@ -23,16 +27,69 @@ describe('SmsConfirmation.js', () => {
 
     });
 
+    describe('get mobileNumber()', () => {
+
+        it('should return enterMobile when the appellantPhoneNumber is an empty string', () => {
+            smsConfirmationClass.fields.appellantPhoneNumber.value = '';
+            smsConfirmationClass.fields.enterMobile.value = '07411738663';
+            expect(smsConfirmationClass.mobileNumber).to.eq(smsConfirmationClass.fields.enterMobile.value);
+        });
+
+        it('should return enterMobile when the appellantPhoneNumber is not a mobile', () => {
+            smsConfirmationClass.fields.appellantPhoneNumber.value = '01277345672';
+            smsConfirmationClass.fields.enterMobile.value = '07411738663';
+            expect(smsConfirmationClass.mobileNumber).to.eq(smsConfirmationClass.fields.enterMobile.value);
+        });
+
+        it('should return enterMobile when the appellantPhoneNumber is a mobile but provides another', () => {
+            smsConfirmationClass.fields.appellantPhoneNumber.value = '07411738765';
+            smsConfirmationClass.fields.useSameNumber.value = answer.NO;
+            smsConfirmationClass.fields.enterMobile.value = '07411738371';
+            expect(smsConfirmationClass.mobileNumber).to.eq(smsConfirmationClass.fields.enterMobile.value);
+        });
+
+        it('should return appellantPhoneNumber which is a mobile', () => {
+            smsConfirmationClass.fields.appellantPhoneNumber.value = '07411738765';
+            smsConfirmationClass.fields.useSameNumber.value = answer.YES;
+            expect(smsConfirmationClass.mobileNumber).to.eq(smsConfirmationClass.fields.appellantPhoneNumber.value);
+        });
+
+    });
+
+    describe('get form()', () => {
+
+        it('should contain 3 fields', () => {
+            expect(smsConfirmationClass.form.fields.length).to.equal(3);
+        });
+
+        it('should contain a textField reference called \'enterMobile\'', () => {
+            const textField = smsConfirmationClass.form.fields[0];
+            expect(textField.constructor.name).to.eq('Reference');
+            expect(textField.name).to.equal('enterMobile');
+            expect(textField.validations).to.be.empty;
+        });
+
+        it('should contain a textField reference called \'useSameNumber\'', () => {
+            const textField = smsConfirmationClass.form.fields[1];
+            expect(textField.constructor.name).to.eq('Reference');
+            expect(textField.name).to.equal('useSameNumber');
+            expect(textField.validations).to.be.empty;
+        });
+
+        it('should contain a textField reference called \'year\'', () => {
+            const textField = smsConfirmationClass.form.fields[2];
+            expect(textField.constructor.name).to.eq('Reference');
+            expect(textField.name).to.equal('appellantPhoneNumber');
+            expect(textField.validations).to.be.empty;
+        });
+
+    });
+
     describe('next()', () => {
 
         it('returns the next step url /representative', () => {
-            const redirector = {
-                nextStep: '/representative'
-            };
-            smsConfirmationClass.journey = {
-                Representative: '/representative'
-            };
-            expect(smsConfirmationClass.next()).to.eql(redirector);
+            smsConfirmationClass.journey.Representative = urls.representative.representative;
+            expect(smsConfirmationClass.next()).to.eql({ nextStep: urls.representative.representative });
         });
 
     });
