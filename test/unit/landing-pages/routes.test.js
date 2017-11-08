0af45a47bbedb7051d7720087bf17fff4d4b77ed
Paul Gain <paul.gain@hmcts.net>
2017-11-08 10:36:49 +0000

    Added Rest HTTP constants
    * Added a new dependency called http-status-codes.
    * Updated unit tests using the constants.

--- test/unit/landing-pages/routes.test.js
+++ test/unit/landing-pages/routes.test.js
@@ -1,3 +1,4 @@
+const HttpStatus = require('http-status-codes');
 const testServer = require('test/testServer');
 const paths = require('paths');
 const request = require('supertest');
@@ -23,31 +24,31 @@ describe('routes.js', () => {
         it('should respond to /overview', done => {
             request(httpServer)
                 .get(paths.landingPages.overview)
-                .expect(200, done);
+                .expect(HttpStatus.OK, done);
         });
 
         it('should respond to /before-you-appeal', done => {
             request(httpServer)
                 .get(paths.landingPages.beforeYouAppeal)
-                .expect(200, done);
+                .expect(HttpStatus.OK, done);
         });
 
         it('should respond to /help-with-appeal', done => {
             request(httpServer)
                 .get(paths.landingPages.helpWithAppeal)
-                .expect(200, done);
+                .expect(HttpStatus.OK, done);
         });
 
         it('should respond to /start-an-appeal', done => {
             request(httpServer)
                 .get(paths.landingPages.startAnAppeal)
-                .expect(200, done);
+                .expect(HttpStatus.OK, done);
         });
 
         it('should respond to /after-you-appeal', done => {
             request(httpServer)
                 .get(paths.landingPages.afterYouAppeal)
-                .expect(200, done);
+                .expect(HttpStatus.OK, done);
         });
 
     });
