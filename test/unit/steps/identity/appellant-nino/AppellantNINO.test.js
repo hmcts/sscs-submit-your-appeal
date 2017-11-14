883f188cf4007c8b170d1b75f20ca92cdb4ecf76
David Crespo <david.crespo@hmcts.net>
2017-11-14 22:26:18 +0100

    SSCS-2191 Add unit test to cover the isAppointee function

--- test/unit/steps/identity/appellant-nino/AppellantNINO.test.js
+++ test/unit/steps/identity/appellant-nino/AppellantNINO.test.js
@@ -5,7 +5,7 @@ const { expect } = require('test/util/chai');
 const paths = require('paths');
 const answer = require('utils/answer');
 
-describe('AppellantNINO.js', () => {
+describe.only('AppellantNINO.js', () => {
 
     let appellantNINOSut;
 
@@ -23,6 +23,20 @@ describe('AppellantNINO.js', () => {
 
         it('returns url /enter-appellant-nino', () => {
             expect(appellantNINOSut.url).to.equal('/enter-appellant-nino');
+        });
+
+    });
+
+    describe('get isAppointee()', () => {
+
+        it('should return true', () => {
+            appellantNINOSut.fields.appointee.value = answer.YES;
+            expect(appellantNINOSut.isAppointee).to.eq(true);
+        });
+
+        it('should return false', () => {
+            appellantNINOSut.fields.appointee.value = answer.NO;
+            expect(appellantNINOSut.isAppointee).to.eq(false);
         });
 
     });
