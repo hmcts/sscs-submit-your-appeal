3b65a0b909f181e972db309bee3f96ccf9a457ad
David Crespo <david.crespo@hmcts.net>
2017-11-15 02:26:22 +0100

    SSCS-2191 Add e2e to cover scenario2 and production code to pass it

--- test/e2e/scenarios/identity/appellantNINO.js
+++ test/e2e/scenarios/identity/appellantNINO.js
@@ -13,20 +13,35 @@ After((I) => {
 });
 
 Scenario('When I go to the Enter Appellant NINO page I see the correct information is displayed', function*(I) {
-	const expected = 'Enter your National Insurance number';
+
+  const expected = 'Enter your National Insurance number';
 	I.see(expected);
 	const actual = yield I.grabTextFrom('.heading-large');
 	const result = actual.search(/Enter your|appellants National Insurance Number/g);
 	assert.notEqual(result, -1, `actual: ${actual} \n expected: ${expected} \n Does Not Match`);
 	I.see('You can find your National Insurance number on any letter about the benefit.');
 	I.seeElement('#AppellantNINO_nino');
+
 });
 
 Scenario('Given that user has entered their NINO \n' +
 	'When it is in the correct format AA123456A \n' +
 	'And they click next \n' +
 	'Then they should go to the next page', (I) => {
+
 		I.fillField('#AppellantNINO_nino', 'AA123456A');
-    I.click('Continue');
-    I.seeInCurrentUrl('/enter-appellant-contact');
+		I.click('Continue');
+		I.seeInCurrentUrl('/enter-appellant-contact');
+
+	});
+
+Scenario('Given that user has entered their NINO \n' +
+	'When it is NOT in the correct format AA123456A \n' +
+	'And they click next \n' +
+	'Then they should see the error message', (I) => {
+
+		I.fillField('#AppellantNINO_nino', 'AA1234');
+		I.click('Continue');
+		I.seeElement('#error-summary-heading');
+
 	});
