562e5df4e7c0d92d3870a915e80733d5452163cb
Paul Gain <paul.gain@hmcts.net>
2017-11-20 11:06:28 +0000

    No MRN CYA

--- steps/compliance/no-mrn/NoMRN.js
+++ steps/compliance/no-mrn/NoMRN.js
@@ -3,12 +3,14 @@
 const { Question, goTo } = require('@hmcts/one-per-page');
 const { form, textField } = require('@hmcts/one-per-page/forms');
 const { whitelist } = require('utils/regex');
+const sections = require('steps/check-your-appeal/sections');
 const Joi = require('joi');
 const paths = require('paths');
 
 class NoMRN extends Question {
 
     get url() {
+
         return paths.compliance.noMRN;
     }
 
@@ -23,7 +25,20 @@ class NoMRN extends Question {
         );
     }
 
+    answers() {
+
+        return [
+
+            answer(this, {
+                question: this.content.cya.reasonForNoMRN.question,
+                section: sections.noMRN.id,
+                answer: `${this.fields.reasonForNoMRN.value}`
+            })
+        ];
+    }
+
     next() {
+
         return goTo(this.journey.Appointee);
     }
 }
