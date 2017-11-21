255e55632fbe2f9e6ea3958fbc25684d293957ae
David Crespo <david.crespo@hmcts.net>
2017-11-21 18:22:01 +0000

    SSCS-2293 Refactor code to use content file
    Move text to the content file
    Reuse test from the content file in template and in test files.

--- terms-and-conditions-page/routes.js
+++ terms-and-conditions-page/routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const paths = require('paths');
+const termsAndConditionsContent = require('terms-and-conditions-page/content.en.json');
 
 router.get(paths.termsAndConditions, (req, res) => {
-    res.render('template.html');
+    res.render('template.html', termsAndConditionsContent);
 });
 
 module.exports = router;
--- test/e2e/scenarios/terms-and-conditions-page/termAndConditionPage.js
+++ test/e2e/scenarios/terms-and-conditions-page/termAndConditionPage.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const paths = require('paths');
+const termsAndConditionsContent = require('terms-and-conditions-page/content.en.json');
 
 Feature('Terms and Conditions Page');
 
 Scenario.only('When I go to the Terms And Conditions page, I see the page information', (I) => {
 
     I.amOnPage(paths.termsAndConditions);
-    I.see('Terms and conditions');
+    I.see(termsAndConditionsContent.title);
     I.seeElement('.link-back');
 
 });
