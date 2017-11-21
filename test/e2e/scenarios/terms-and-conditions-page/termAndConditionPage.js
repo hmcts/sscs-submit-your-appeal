67753437dfb2e39008d6300a7aa139adbf5a3fc2
David Crespo <david.crespo@hmcts.net>
2017-11-21 17:58:12 +0000

    SSCS-2293 Add template content
    Remove hearder from nino template because it is not used.
    Remove commented out lines.
    Add test for the back link.
    Add more text and back link to template.

--- test/e2e/scenarios/terms-and-conditions-page/termAndConditionPage.js
+++ test/e2e/scenarios/terms-and-conditions-page/termAndConditionPage.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const paths = require('paths');
-// const overviewContent = require('landing-pages/overview/content.en.json');
 
 Feature('Terms and Conditions Page');
 
-Scenario.only('When I go to the Terms And Conditions page, I see the page heading', (I) => {
+Scenario.only('When I go to the Terms And Conditions page, I see the page information', (I) => {
 
     I.amOnPage(paths.termsAndConditions);
     I.see('Terms and conditions');
+    I.seeElement('.link-back');
 
 });
