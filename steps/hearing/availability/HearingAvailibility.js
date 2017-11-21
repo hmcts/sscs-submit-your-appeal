f2dd6b7f2bb45553038fb50ec39b83b5d0653575
Paul Gain <paul.gain@hmcts.net>
2017-11-21 07:32:04 +0000

    Dummy Hearing Availability Step
    Added a dummy step allowing us to temporarily add a single date the user cannot make the hearing, this allows us to further out journey to enable check your answers.

--- steps/hearing/availability/HearingAvailibility.js
+++ steps/hearing/availability/HearingAvailibility.js
@@ -1,20 +1,55 @@
 'use strict';
 
-const { Question } = require('@hmcts/one-per-page');
-const { form } = require('@hmcts/one-per-page/forms');
+const { Question, goTo } = require('@hmcts/one-per-page');
+const { form, textField } = require('@hmcts/one-per-page/forms');
+const { numbers } = require('utils/regex');
+const sections = require('steps/check-your-appeal/sections');
+const Joi = require('joi');
 const paths = require('paths');
 
 class HearingAvailibility extends Question {
 
     get url() {
+
         return paths.hearing.hearingAvailability;
     }
 
     get form() {
-        return form();
+
+        return form(
+
+            textField('day').joi(
+                this.content.fields.day.error.required,
+                Joi.string().regex(numbers).required()),
+
+            textField('month').joi(
+                this.content.fields.month.error.required,
+                Joi.string().regex(numbers).required()
+            ),
+
+            textField('year').joi(
+                this.content.fields.year.error.required,
+                Joi.string().regex(numbers).required()
+            )
+        );
     }
 
-    next() {}
+    answers() {
+
+        return [
+
+            answer(this, {
+                question: this.content.cya.dateYouCantAttend.question,
+                section: sections.hearing.availability,
+                answer: `${this.fields.day.value}/${this.fields.month.value}/${this.fields.year.value}`
+            })
+        ];
+    }
+
+    next() {
+
+        return goTo(this.journey.CheckYourAppeal);
+    }
 }
 
 module.exports = HearingAvailibility;
