07477cca5395ea077a4eca2c041c7858b4b6f518
Paul Gain <paul.gain@hmcts.net>
2017-11-21 07:38:03 +0000

    Modified TextReminders so we POST user selection We now post whether a user wants text msg reminders or not.

--- steps/sms-notify/text-reminders/TextReminders.js
+++ steps/sms-notify/text-reminders/TextReminders.js
@@ -1,31 +1,41 @@
 'use strict';
 
-const { Question, goTo } = require('@hmcts/one-per-page');
+const { Question, goTo, branch } = require('@hmcts/one-per-page');
 const { form, textField } = require('@hmcts/one-per-page/forms');
 const regex = require('../../../utils/regex');
+const Joi = require('joi');
 const paths = require('paths');
+const userAnswer = require('utils/answer');
 
 class TextReminders extends Question {
 
     get url() {
-        return paths.smsNotify.appellantTextReminders
-    }
 
-    get signUpLink() {
-        return regex.mobileNumber.test(this.fields.appellantPhoneNumber.value) ?
-            paths.smsNotify.sendToNumber : paths.smsNotify.enterMobile
+        return paths.smsNotify.appellantTextReminders
     }
 
     get form() {
 
         return form(
 
-            textField.ref(this.journey.AppellantDetails, 'appellantPhoneNumber')
-        )
+            textField('doYouWantTextMsgReminders').joi(
+                this.content.fields.doYouWantTextMsgReminders.error.required,
+                Joi.string().valid([userAnswer.YES, userAnswer.NO])
+            ),
+
+            textField.ref(this.journey.AppellantContactDetails, 'phoneNumber')
+        );
     }
 
     next() {
-        return goTo(this.journey.EnterMobile);
+
+        const wantsTextMsgReminders = () => this.fields.doYouWantTextMsgReminders.value === userAnswer.YES;
+        const nextStep = regex.mobileNumber.test(this.fields.phoneNumber.value) ? this.journey.SendToNumber : this.journey.EnterMobile;
+
+        return branch(
+            goTo(nextStep).if(wantsTextMsgReminders),
+            goTo(this.journey.Representative)
+        );
     }
 }
 
