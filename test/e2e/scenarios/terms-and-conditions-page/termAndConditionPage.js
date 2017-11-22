1bbb2612e9cede1f49d282a4852fbde9ad8e0440
David Crespo <david.crespo@hmcts.net>
2017-11-22 13:15:33 +0000

    SSCS-2293: Add hyper links to the text
    Refactor json field names to something more meaningful
    Adjust template to use link
    Adjust e2e test to cover new elements

--- test/e2e/scenarios/terms-and-conditions-page/termAndConditionPage.js
+++ test/e2e/scenarios/terms-and-conditions-page/termAndConditionPage.js
@@ -5,10 +5,17 @@ const termsAndConditionsContent = require('terms-and-conditions-page/content.en.
 
 Feature('Terms and Conditions Page');
 
-Scenario.only('When I go to the Terms And Conditions page, I see the page information', (I) => {
+Scenario.only('When I go to the Terms And Conditions page, I see the Term and Conditions section', (I) => {
 
     I.amOnPage(paths.termsAndConditions);
-    I.see(termsAndConditionsContent.title);
     I.seeElement('.link-back');
+    I.see(termsAndConditionsContent.title);
+    I.see(termsAndConditionsContent.termsAndConditions.termsOfuse);
+    I.see('These include the GOV.UK privacy policy and terms and conditions.');
+    I.click('privacy policy');
+    I.seeInCurrentUrl('https://www.gov.uk/help/privacy-policy');
+    I.amOnPage(paths.termsAndConditions);
+    I.click('terms and conditions');
+    I.seeInCurrentUrl('https://www.gov.uk/help/terms-conditions');
 
 });
