d9dc5f5af8942ef019d91b9f2ecc478413e8b097
David Crespo <david.crespo@hmcts.net>
2017-11-22 15:23:07 +0000

    SSCS-2293: Add privacy related link Add e2e test Add production code to pass it Add text to the json content file

--- test/e2e/scenarios/terms-and-conditions-page/termAndConditionPage.js
+++ test/e2e/scenarios/terms-and-conditions-page/termAndConditionPage.js
@@ -5,26 +5,36 @@ const termsAndConditionsContent = require('terms-and-conditions-page/content.en.
 
 Feature('Terms and Conditions Page');
 
+Before((I) => {
+	I.amOnPage(paths.termsAndConditions);
+});
+
+
 Scenario('When I go to the Terms And Conditions page, I see the Term and Conditions section', (I) => {
 
-    I.amOnPage(paths.termsAndConditions);
-    I.seeElement('.link-back');
-    I.see(termsAndConditionsContent.title);
-    I.see(termsAndConditionsContent.termsAndConditions.termsOfuse);
-    I.see('These include the GOV.UK privacy policy and terms and conditions.');
-    I.click('privacy policy');
-    I.seeInCurrentUrl('https://www.gov.uk/help/privacy-policy');
-    I.amOnPage(paths.termsAndConditions);
-    I.click('terms and conditions');
-    I.seeInCurrentUrl('https://www.gov.uk/help/terms-conditions');
+	I.seeElement('.link-back');
+	I.see(termsAndConditionsContent.title);
+	I.see(termsAndConditionsContent.termsAndConditions.termsOfuse);
+	I.see('These include the GOV.UK privacy policy and terms and conditions.');
+	I.click('privacy policy');
+	I.seeInCurrentUrl('https://www.gov.uk/help/privacy-policy');
+	I.amOnPage(paths.termsAndConditions);
+	I.click('terms and conditions');
+	I.seeInCurrentUrl('https://www.gov.uk/help/terms-conditions');
 
 });
 
 Scenario.only('When I go to the Terms And Conditions page, I see the Related links section', (I) => {
 
-    I.amOnPage(paths.termsAndConditions);
-    I.see(termsAndConditionsContent.relatedLinks.title);
-    I.see(termsAndConditionsContent.relatedLinks.links.cookie.text);
-    I.click(termsAndConditionsContent.relatedLinks.links.cookie.text);
-    I.seeInCurrentUrl(termsAndConditionsContent.relatedLinks.links.cookie.url);
+	I.see(termsAndConditionsContent.relatedLinks.title);
+	//cookie policy
+	I.see(termsAndConditionsContent.relatedLinks.links.cookie.text);
+	I.click(termsAndConditionsContent.relatedLinks.links.cookie.text);
+	I.seeInCurrentUrl(termsAndConditionsContent.relatedLinks.links.cookie.url);
+	//privacy policy
+	I.amOnPage(paths.termsAndConditions);
+	I.see(termsAndConditionsContent.relatedLinks.links.privacy.text);
+	I.click(termsAndConditionsContent.relatedLinks.links.privacy.text);
+	I.seeInCurrentUrl(termsAndConditionsContent.relatedLinks.links.privacy.url);
+
 });
