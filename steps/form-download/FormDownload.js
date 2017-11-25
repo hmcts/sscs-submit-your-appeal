f8de347ffe9e2065f129268b4eb42004fa5534fa
David Crespo <david.crespo@hmcts.net>
2017-11-25 13:59:31 +0100

    SSCS-2226 Clean up code
    Clean up unused stuff

--- steps/form-download/FormDownload.js
+++ steps/form-download/FormDownload.js
@@ -1,8 +1,7 @@
 'use strict';
 
-const {form, textField} = require('@hmcts/one-per-page/forms');
+const {form} = require('@hmcts/one-per-page/forms');
 const {Question, goTo} = require('@hmcts/one-per-page');
-const answer = require('utils/answer');
 const paths = require('paths');
 
 class FormDownload extends Question {
@@ -11,15 +10,8 @@ class FormDownload extends Question {
         return paths.formDownload;
     }
 
-    get isAppointee() {
-        return this.fields.appointee.value === answer.YES;
-    }
-
     get form() {
-
-        return form(
-            textField.ref(this.journey.Appointee, 'appointee')
-        );
+        return form();
     }
 
     next() {
