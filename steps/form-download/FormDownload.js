a292666c2326d71b87426ba320f872e14c5cf4db
David Crespo <david.crespo@hmcts.net>
2017-11-28 15:52:15 +0000

    SSCS-2226: Revert extend from Page
    extend from Question because currently it's not possible to retrieve an
    answer from previous step from a Page type.
    
    Add function benefitType to get the content for the wording in the page.
    
    Add text in the content json file
    
    call the benefitType function from the template to display the wording
    
    Add unit test to cover production code

--- steps/form-download/FormDownload.js
+++ steps/form-download/FormDownload.js
@@ -1,13 +1,29 @@
 'use strict';
 
-const {form} = require('@hmcts/one-per-page/forms');
-const { Page } = require('@hmcts/one-per-page');
 const paths = require('paths');
+const { form, textField } = require('@hmcts/one-per-page/forms');
+const { Question, goTo } = require('@hmcts/one-per-page');
+const content = require('steps/form-download/content.en.json');
 
-class FormDownload extends Page {
+class FormDownload extends Question {
 
     get url() {
         return paths.formDownload;
+    }
+
+    get benefitType() {
+        return content.subtitle.replace(content.benefitTypePlaceholder, this.fields.benefitType.value)
+        .replace(content.formTypePlaceholder, "Form name here");
+    }
+
+    get form() {
+        return form(
+            textField.ref(this.journey.BenefitType, 'benefitType')
+        );
+    }
+
+    next() {
+        return goTo(undefined);
     }
 
 }
--- test/unit/steps/form-download/FormDownload.test.js
+++ test/unit/steps/form-download/FormDownload.test.js
@@ -10,9 +10,13 @@ describe('FormDownload.js', () => {
 
     beforeEach(() => {
         formDownloadSut = new FormDownload();
+
+        formDownloadSut.fields = {
+            benefitType: {}
+        }
     });
 
-    describe.only('get url()', () => {
+    describe('get url()', () => {
 
         it('returns url /form-download', () => {
             expect(formDownloadSut.url).to.equal(paths.formDownload);
@@ -20,5 +24,13 @@ describe('FormDownload.js', () => {
 
     });
 
+    describe.only('get benefitType()', () => {
+
+        it('returns correct wording', () => {
+            formDownloadSut.fields.benefitType.value = 'some benefitType';
+            expect(formDownloadSut.benefitType).to.equal('Download and fill out a [Form name here] form to appeal a [some benefitType] benefit decision.');
+        });
+
+    });
 
 });
