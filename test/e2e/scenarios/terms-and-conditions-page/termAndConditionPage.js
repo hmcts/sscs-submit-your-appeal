1691dc2a629eeb439d9120f208028b722f080a32
David Crespo <david.crespo@hmcts.net>
2017-11-30 15:24:39 +0000

    SSCS-2293: Change tabs to spaces

--- test/e2e/scenarios/terms-and-conditions-page/termAndConditionPage.js
+++ test/e2e/scenarios/terms-and-conditions-page/termAndConditionPage.js
@@ -6,21 +6,21 @@ const termsAndConditionsContent = require('terms-and-conditions-page/content.en.
 Feature('Terms and Conditions Page');
 
 Before((I) => {
-	I.amOnPage(paths.termsAndConditions);
+    I.amOnPage(paths.termsAndConditions);
 });
 
 Scenario('When I go to the Terms And Conditions page, I see the page title text', (I) => {
 
-	I.see(termsAndConditionsContent.title);
+    I.see(termsAndConditionsContent.title);
 
 });
 
 Scenario('When I go to the Terms And Conditions page, I see expected links and go to expected urls', (I) => {
 
-	I.seeAndGoToGivenLink(termsAndConditionsContent.relatedLinks.cookie.text, termsAndConditionsContent.relatedLinks.cookie.url);
-	I.seeAndGoToGivenLink(termsAndConditionsContent.relatedLinks.privacy.text, termsAndConditionsContent.relatedLinks.privacy.url);
-	I.seeAndGoToGivenLink(termsAndConditionsContent.relatedLinks.conditions.text, termsAndConditionsContent.relatedLinks.conditions.url);
-	I.seeAndGoToGivenLink(termsAndConditionsContent.termsAndConditions.links.privacy.name, termsAndConditionsContent.relatedLinks.privacy.url);
-	I.seeAndGoToGivenLink(termsAndConditionsContent.termsAndConditions.links.terms.name, termsAndConditionsContent.relatedLinks.conditions.url);
+    I.seeAndGoToGivenLink(termsAndConditionsContent.relatedLinks.cookie.text, termsAndConditionsContent.relatedLinks.cookie.url);
+    I.seeAndGoToGivenLink(termsAndConditionsContent.relatedLinks.privacy.text, termsAndConditionsContent.relatedLinks.privacy.url);
+    I.seeAndGoToGivenLink(termsAndConditionsContent.relatedLinks.conditions.text, termsAndConditionsContent.relatedLinks.conditions.url);
+    I.seeAndGoToGivenLink(termsAndConditionsContent.termsAndConditions.links.privacy.name, termsAndConditionsContent.relatedLinks.privacy.url);
+    I.seeAndGoToGivenLink(termsAndConditionsContent.termsAndConditions.links.terms.name, termsAndConditionsContent.relatedLinks.conditions.url);
 
 });
