4f399c454269745265b2e9b315620f9db6eb2ed9
Paul Gain <paul.gain@hmcts.net>
2017-12-07 14:06:11 +0000

    Adding spaces.

--- test/e2e/scenarios/reasons-for-appealing/sendingEvidence.js
+++ test/e2e/scenarios/reasons-for-appealing/sendingEvidence.js
@@ -14,26 +14,34 @@ After((I) => {
 });
 
 Scenario('When I omit my email address from my contact details I should see the correct content on /sending-evidence', (I) => {
+
     I.amOnPage(paths.identity.enterAppellantContactDetails);
     I.enterAppellantContactDetailsAndContinue();
     I.amOnPage(paths.reasonsForAppealing.sendingEvidence);
     I.see(content.postEvidence);
+
 });
 
 Scenario('When I add my email address to my contact details I should see the correct content on /sending-evidence', (I) => {
+
     I.amOnPage(paths.identity.enterAppellantContactDetails);
     I.enterAppellantContactDetailsWithEmailAndContinue();
     I.amOnPage(paths.reasonsForAppealing.sendingEvidence);
     I.see(content.postEvidenceWithEmail);
+
 });
 
 Scenario('When I go to the /sending-evidence page I see the title', (I) => {
+
     I.amOnPage(paths.reasonsForAppealing.sendingEvidence);
     I.see(content.title);
+
 });
 
 Scenario('When I go to the /sending-evidence page and select continue I see the path /the-hearing', (I) => {
+
     I.amOnPage(paths.reasonsForAppealing.sendingEvidence);
     I.click('Continue');
     I.seeInCurrentUrl(paths.hearing.theHearing);
+
 });
