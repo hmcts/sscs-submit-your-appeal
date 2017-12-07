7305a084dca87438762fdf6933075e688f321fba
bendiggle <ben.diggle5@gmail.com>
2017-12-07 10:59:37 +0000

    changes based on pr comments

--- test/e2e/scenarios/start/benefitType.js
+++ test/e2e/scenarios/start/benefitType.js
@@ -14,16 +14,14 @@ After((I) => {
 
 Scenario('When I enter PIP, I am taken to the DWP Issuing office page', (I) => {
 
-    I.fillField('#benefitType', 'Personal Independence Payment (PIP)');
-    I.click('Continue');
+    I.enterBenefitTypeAndContinue('Personal Independence Payment (PIP)');
     I.seeInCurrentUrl(paths.compliance.dwpIssuingOffice);
 
 });
 
 Scenario('When I enter a non PIP benefit type, I am taken to the download form page', (I) => {
 
-    I.fillField('#benefitType', 'Disability Living Allowance (DLA)');
-    I.click('Continue');
+    I.enterBenefitTypeAndContinue('Disability Living Allowance (DLA)');
     I.seeInCurrentUrl(paths.identity.downloadAppointeeForm);
 
 });
