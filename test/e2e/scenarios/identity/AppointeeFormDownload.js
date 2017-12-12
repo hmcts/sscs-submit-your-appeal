d28e3e0453d5a8ca897d49e0323b48b3bf7b6edc
bendiggle <ben.diggle5@gmail.com>
2017-12-12 15:01:33 +0000

    use types object for e2e tests

--- test/e2e/scenarios/identity/AppointeeFormDownload.js
+++ test/e2e/scenarios/identity/AppointeeFormDownload.js
@@ -1,5 +1,6 @@
 const content = require('steps/identity/appointee-form-download/content.en.json');
 const paths = require('paths');
+const benefitTypes = require('steps/start/benefit-type/types');
 
 Feature('Appointee form download page');
 
@@ -13,21 +14,21 @@ After((I) => {
 
 Scenario('I see SSCS1 content when I select any Benefit type other than Carer’s Allowance or Child Benefit', (I) => {
 
-    I.enterBenefitTypeAndContinue('Disability Living Allowance (DLA)');
+    I.enterBenefitTypeAndContinue(benefitTypes.disabilityLivingAllowance);
     I.see('Download and fill out a SSCS1 form to appeal a Disability Living Allowance (DLA) benefit decision.');
 
 });
 
 Scenario('I see SSCS5 content when I select Carer’s Allowance as a benefit type', (I) => {
 
-    I.enterBenefitTypeAndContinue('Carer’s Allowance');
+    I.enterBenefitTypeAndContinue(benefitTypes.carersAllowance);
     I.see('Download and fill out a SSCS5 form to appeal a Carer’s Allowance benefit decision.');
 
 });
 
 Scenario('I see SSCS5 content when I select Child Benefit as a benefit type', (I) => {
 
-    I.enterBenefitTypeAndContinue('Child Benefit');
+    I.enterBenefitTypeAndContinue(benefitTypes.childBenefit);
     I.see('Download and fill out a SSCS5 form to appeal a Child Benefit benefit decision.');
 
 });
