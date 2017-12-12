ef50c40183423b9596f8a5424f2fea4572b7b76c
bendiggle <ben.diggle5@gmail.com>
2017-12-12 18:38:31 +0000

    rebase master

--- steps/check-your-appeal/CheckYourAppeal.js
+++ steps/check-your-appeal/CheckYourAppeal.js
@@ -19,7 +19,6 @@ class CheckYourAppeal extends CYA {
 
         return [
             section('benefit-type',                     { title: this.content.benefitType }),
-            section('have-a-mrn',                       { title: this.content.compliance.haveAMRN }),
             section('mrn-date',                         { title: this.content.compliance.mrnDate }),
             section('no-mrn',                           { title: this.content.compliance.noMRN }),
             section('appellant-details',                { title: this.content.appellantDetails }),
