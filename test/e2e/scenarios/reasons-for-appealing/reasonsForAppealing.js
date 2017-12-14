59b625a8560f968067fed8ee2c9276ce6b00652b
David Crespo <david.crespo@hmcts.net>
2017-12-14 12:56:02 +0100

    SSCS-2437 Keep consistency
    For consistency we always put the I.amOnPage directly
    after I.createTheSession();

--- test/e2e/scenarios/reasons-for-appealing/reasonsForAppealing.js
+++ test/e2e/scenarios/reasons-for-appealing/reasonsForAppealing.js
@@ -6,15 +6,15 @@ Feature('Reason For Appealing');
 
 Before((I) => {
     I.createTheSession();
+    I.amOnPage(paths.reasonsForAppealing.reasonForAppealing);
 });
-
 After((I) => {
     I.endTheSession();
+
 });
 
-Scenario ('Textfield allows to enter new lines', (I) => {
+Scenario.only('Textfield allows to enter new lines', (I) => {
 
-    I.amOnPage(paths.reasonsForAppealing.reasonForAppealing);
     const text = 'This is a text. \r This is a new line';
     I.enterReasonForAppealingAndContinue(text);
     I.seeInCurrentUrl(paths.reasonsForAppealing.otherReasonForAppealing);
