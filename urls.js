2623c6026b03f68223a4942796150165b3797316
bendiggle <ben.diggle5@gmail.com>
2017-12-14 10:27:10 +0000

    add beta banner

--- urls.js
+++ urls.js
@@ -1,5 +1,6 @@
 module.exports = {
 
+    phaseBanner: 'https://www.smartsurvey.co.uk/s/QQQ7L/',
     surveyLink: 'https://www.smartsurvey.co.uk/s/4XAAH/',
     formDownload: {
         sscs1: 'http://formfinder.hmctsformfinder.justice.gov.uk/sscs1-eng.pdf',
