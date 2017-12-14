5f183f6ccf39d753e6631e7b00f3ac5395f24faf
sivatfl23 <sivatfl23@gmail.com>
2017-12-14 12:22:22 +0000

    added for loop instead datatable and removed css

--- test/e2e/scenarios/start/benefitType.js
+++ test/e2e/scenarios/start/benefitType.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const paths = require('paths');
+const types = require('steps/start/benefit-type/types');
 
 Feature('Benefit Type');
 
@@ -27,31 +28,22 @@ Scenario('When I enter a non PIP benefit type, I am taken to the download form p
 
 });
 
-const benefit = new DataTable(['BenefitType', 'BenefitName']); //
-benefit.add(['Attendance Allowance', 'Attendance Allowance']); // adding records to a table
-benefit.add(['Bereavement Benefit', 'Bereavement Benefit']);
-benefit.add(['Carer’s Allowance', 'Carer’s Allowance']);
-benefit.add(['Disability Living Allowance (DLA)', 'Disability Living Allowance (DLA)']);
-benefit.add(['Employment and Support Allowance (ESA)', 'Employment and Support Allowance (ESA)']);
-benefit.add(['Incapacity Benefit', 'Incapacity Benefit']);
-benefit.add(['Industrial Injuries Disablement', 'Industrial Injuries Disablement']);
-benefit.add(['Jobseeker’s Allowance (JSA)', 'Jobseeker’s Allowance (JSA)']);
-benefit.add(['Maternity Allowance', 'Maternity Allowance']);
-benefit.add(['Severe Disablement Allowance', 'Severe Disablement Allowance']);
-benefit.add(['Social Fund', 'Social Fund']);
-benefit.add(['Universal Credit (UC)', 'Universal Credit (UC)']);
-
-Data(benefit).Scenario('When I enter differernt non PIP benefit types, I am taken to the download form page', (I, current) => {
-    I.enterBenefitTypeAndContinue(current.BenefitName);
-    I.see(current.BenefitName, {css: '.column-two-thirds>p'});
-    I.seeInCurrentUrl(paths.identity.downloadAppointeeForm);
-    I.see('Continue to form download');
-    I.click({css: '.link-back'});
+Scenario('When I enter differernt non PIP benefit types, I am taken to the download form page', (I) => {
+    let i;
+    for(i=0; i<types.length; i++){
+        if(types[i]!='Personal Independence Payment (PIP)'){
+            I.enterBenefitTypeAndContinue(types[i]);
+            I.see(types[i]);
+            I.seeInCurrentUrl(paths.identity.downloadAppointeeForm);
+            I.see('Continue to form download');
+            I.click('Back'); //click on Back link
+        }
+    }
 });
 
 Scenario('Check the Benefit form, I am taken to the download form page', (I) => {
     I.enterBenefitTypeAndContinue('Disability Living Allowance (DLA)');
     I.seeInCurrentUrl(paths.identity.downloadAppointeeForm);
-    I.click({css: '.button'}); //Continue to form download
+    I.click('Continue to form download'); //Continue to form download
     I.seeInCurrentUrl("https://hmctsformfinder.justice.gov.uk/HMCTS/GetForm.do?original_id=3038");
 });
