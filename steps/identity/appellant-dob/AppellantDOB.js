df9994cf214121ff434a5eb2b8c2b97d2b84460e
bendiggle <ben.diggle5@gmail.com>
2017-12-19 12:44:44 +0000

    add dob stuff

--- steps/identity/appellant-dob/AppellantDOB.js
+++ steps/identity/appellant-dob/AppellantDOB.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const { Question, goTo } = require('@hmcts/one-per-page');
-const { form, textField } = require('@hmcts/one-per-page/forms');
+const { form, textField, dateField } = require('@hmcts/one-per-page/forms');
 const { answer } = require('@hmcts/one-per-page/checkYourAnswers');
 const { numbers } = require('utils/regex');
 const sections = require('steps/check-your-appeal/sections');
 const userAnswer = require('utils/answer');
 const Joi = require('joi');
 const paths = require('paths');
+const moment = require('moment');
 
 class AppellantDOB extends Question {
 
@@ -25,24 +26,44 @@ class AppellantDOB extends Question {
 
         const fields = this.content.fields;
 
+
         return form(
 
-            textField('day').joi(
-                fields.day.error.required,
-                Joi.string().regex(numbers).required()),
-
-            textField('month').joi(
-                fields.month.error.required,
-                Joi.string().regex(numbers).required()
-            ),
-
-            textField('year').joi(
-                fields.year.error.required,
-                Joi.string().regex(numbers).required()
+            dateField(
+                'date',
+                {
+                    allRequired: fields.date.error.allRequired,
+                    dayRequired: fields.date.error.dayRequired,
+                    monthRequired: fields.date.error.monthRequired,
+                    yearRequired: fields.date.error.yearRequired
+                }
+            ).check(
+                'MEOW',
+                (something)=>{
+                    console.log('meow ' + JSON.stringify(something))
+                    return true
+                }
             ),
 
             textField.ref(this.journey.steps.Appointee, 'appointee')
         );
+
+        //     textField('day').joi(
+        //         fields.day.error.required,
+        //         Joi.string().regex(numbers).required()),
+        //
+        //     textField('month').joi(
+        //         fields.month.error.required,
+        //         Joi.string().regex(numbers).required()
+        //     ),
+        //
+        //     textField('year').joi(
+        //         fields.year.error.required,
+        //         Joi.string().regex(numbers).required()
+        //     ),
+        //
+        //     textField.ref(this.journey.steps.Appointee, 'appointee')
+        // );
     }
 
     answers() {
