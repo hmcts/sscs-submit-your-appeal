a3dd8f1a07a8d9b9493a9e12496dbc8cccb41837
David Crespo <david.crespo@hmcts.net>
2017-12-20 13:15:56 +0100

    SSCS-2421: Update the test wording to capture better the scenario

--- test/e2e/scenarios/check-your-appeal/checkYourAppeal.js
+++ test/e2e/scenarios/check-your-appeal/checkYourAppeal.js
@@ -6,17 +6,15 @@ Feature('Check-your-appeal');
 
 Before((I) => {
     I.createTheSession();
-    I.enterBenefitTypeAndContinue('pip');
-    I.amOnPage(paths.checkYourAppeal);
 });
 
 After((I) => {
     I.endTheSession();
 });
 
-//TODO: Please, review this test since the wording does not seem to match the test implementation
-xScenario('When I click submit your appeal, I am taken to the confirmation page', (I) => {
+Scenario('When the appeal is incomplete, I am taken to the next step that needs completing', (I) => {
 
+    I.amOnPage(paths.checkYourAppeal);
     I.see('Check your answers');
     I.see('Your application is incomplete');
     I.see('There are still some questions to answer');
@@ -27,6 +25,8 @@ xScenario('When I click submit your appeal, I am taken to the confirmation page'
 
 Scenario('When I go to the check your appeal page, I see the Sign and submit section', (I) => {
 
+    I.enterBenefitTypeAndContinue('pip');
+    I.amOnPage(paths.checkYourAppeal);
     I.see('Sign and submit');
     I.click('terms and conditions');
     I.seeInCurrentUrl('/terms-and-conditions');
