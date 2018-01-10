63248bb99d0be69cd812ba3b4ad9217250cfab14
Paul Gain <paul.gain@hmcts.net>
2018-01-10 17:22:01 +0000

    Fix for broken e2e tests after updating content.

--- test/e2e/scenarios/identity/appointee.js
+++ test/e2e/scenarios/identity/appointee.js
@@ -2,6 +2,7 @@
 
 const appellantName = require('steps/identity/appellant-name/content.en.json');
 const appointeeFormDownload = require('steps/identity/appointee-form-download/content.en.json');
+const appointee = require('steps/identity/appointee/content.en');
 const paths = require('paths');
 
 Feature('Appointee form');
@@ -17,7 +18,7 @@ After((I) => {
 
 Scenario('When I select Yes, I am taken to the download appointee form page', (I) => {
 
-    I.selectAreYouAnAppointeeAndContinue('Yes');
+    I.selectAreYouAnAppointeeAndContinue(appointee.fields.isAppointee.yes);
     I.seeInCurrentUrl(paths.identity.appointeeFormDownload);
     I.see(appointeeFormDownload.title);
 
@@ -25,7 +26,7 @@ Scenario('When I select Yes, I am taken to the download appointee form page', (I
 
 Scenario('When I select No, I am taken to the enter your details page', (I) => {
 
-    I.selectAreYouAnAppointeeAndContinue('No');
+    I.selectAreYouAnAppointeeAndContinue(appointee.fields.isAppointee.no);
     I.seeInCurrentUrl(paths.identity.enterAppellantName);
     I.see(appellantName.title);
 
