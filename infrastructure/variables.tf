6104d19f16f864e0abbc914a889da96480cfee89
Sathish Jeganathan <Satish.Jaganathan@HMCTS.NET>
2018-01-15 13:56:20 +0000

    SSCS-2560 Added infrastructure code for CNP environment

---
+++ infrastructure/variables.tf
@@ -0,0 +1,28 @@
+variable "product" {
+  type    = "string"
+  default = "sscs-tribunals"
+}
+
+variable "location" {
+  type    = "string"
+  default = "UK South"
+}
+
+variable "env" {
+  type = "string"
+}
+
+variable "infrastructure_env" {
+  default     = "dev"
+  description = "Infrastructure environment to point to"
+}
+
+variable "sya_server_port" {
+  default = "3000"
+}
+
+variable "node_environment" {
+  default = "development"
+}
+
+
