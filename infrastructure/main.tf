e0c69af50171d3812843ae3e1fa43f9338c8ab0e
Sathish Jeganathan <Satish.Jaganathan@HMCTS.NET>
2018-01-25 11:27:22 +0000

    SSCS-2560 Removed PORT env and changed NODE version in infrastructure code

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -9,11 +9,9 @@ module "submit-your-appeal-frontend" {
     TRIBUNALS_CASE_API_URL       = "http://sscs-tribunals-api-${var.env}.service.${data.terraform_remote_state.core_apps_compute.ase_name[0]}.internal"
     REDIS_URL                    = "redis://ignore:${urlencode(module.redis-cache.access_key)}@${module.redis-cache.host_name}:${module.redis-cache.redis_port}?tls=true"
     SESSION_SECRET               = "${module.redis-cache.access_key}"
-    WEBSITE_NODE_DEFAULT_VERSION = "8.8.1"
     NODE_ENV                     = "${var.node_environment}"
     HTTP_PROTOCOL                = "https"
-    PORT                         = "${var.sya_server_port}"
-    WEBSITE_NODE_DEFAULT_VERSION = "8.9.0"
+    WEBSITE_NODE_DEFAULT_VERSION = "8.9.3"
   }
 }
 
