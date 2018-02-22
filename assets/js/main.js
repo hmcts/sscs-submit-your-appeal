ebe35c832a550d7375dfc36bd43cc08fd267d707
Ben Diggle <ben.diggle5@gmail.com>
2018-02-22 09:20:56 +0000

    Add GA to main.js and add the js file to globals while removing it from the specific page template (#314)
    Add GA to main.js
    Add the js file to globals template
    Remove js file from benefit type and appellant dob templates

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,13 @@
 import $ from 'jquery';
 import ShowHideContent from 'govuk/show-hide-content';
 import accessibleAutocomplete from 'accessible-autocomplete';
+import GoogleAnalyticsUniversalTracker from 'govuk/analytics/google-analytics-universal-tracker.js';
+import Analytics from 'govuk/analytics/analytics.js';
 
 $(document).ready(() => {
     initShowHideContent();
     initAutocomplete();
+    initSYAAnalyticsTrack()
 });
 
 function initShowHideContent() {
@@ -19,4 +22,30 @@ function initAutocomplete() {
             selectElement: select
         });
     });
+}
+
+function initSYAAnalyticsTrack() {
+    // Load Google Analytics libraries
+    Analytics.load();
+
+    // Use document.domain in dev, preview and staging so that tracking works
+    // Otherwise explicitly set the domain as www.gov.uk (and not gov.uk).
+    const cookieDomain = (document.domain === 'www.gov.uk') ? '.www.gov.uk' : document.domain;
+
+    // Configure profiles and make interface public
+    // for custom dimensions, virtual pageviews and events
+    const analytics = new Analytics({
+        universalId: 'UA-91309785-4',
+        cookieDomain: cookieDomain
+    });
+
+    // Set custom dimensions before tracking pageviews
+    // GOVUK.analytics.setDimension(…)
+
+    // Activate any event plugins eg. print intent, error tracking
+    // GOVUK.analyticsPlugins.error();
+    // GOVUK.analyticsPlugins.printIntent();
+
+    // Track initial pageview
+    analytics.trackPageview();
 }
