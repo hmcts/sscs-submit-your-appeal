c64c0193935627717559bda1410d45f090db8aeb
Paul Gain <paul.gain@hmcts.net>
2018-02-22 11:12:24 +0000

    adding an example smoke test which illustrates the tag @smoke

--- test/e2e/smoke/smoking.test.js
+++ test/e2e/smoke/smoking.test.js
@@ -1,5 +1,3 @@
-const benefitTypes = require('steps/start/benefit-type/types');
-
 Feature('Smoking HOT!');
 
 Before((I) => {
@@ -12,7 +10,6 @@ After((I) => {
 
 Scenario('Smoke test demo @smoke', (I) => {
 
-    I.enterBenefitTypeAndContinue(benefitTypes.disabilityLivingAllowance);
-    I.see('Download and fill out a SSCS1 form to appeal a Disability Living Allowance (DLA) benefit decision.');
+    I.see('Which benefit is your appeal about?');
 
 });
