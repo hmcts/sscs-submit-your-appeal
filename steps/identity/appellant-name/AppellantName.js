efca9e8ecbca4299fccb51181c48c1c74a9a87ef
Ben Diggle <ben.diggle5@gmail.com>
2018-02-28 14:20:20 +0000

    fix bug where trailing whitespace would cause a validation error (#338)
    Fix bug where when a name is entered with a space after, the validation would fail and display the error e.g. Ben
    
    Add Joi().trim() to the name fields for appellant-name, rep-name and check-your-appeal to remove the whitespace when validating

--- steps/identity/appellant-name/AppellantName.js
+++ steps/identity/appellant-name/AppellantName.js
@@ -27,11 +27,11 @@ class AppellantName extends Question {
 
             textField('firstName')
                 .joi(fields.firstName.error.required, Joi.string().required())
-                .joi(fields.firstName.error.invalid, Joi.string().regex(firstName)),
+                .joi(fields.firstName.error.invalid, Joi.string().trim().regex(firstName)),
 
             textField('lastName')
                 .joi(fields.lastName.error.required, Joi.string().required())
-                .joi(fields.lastName.error.invalid, Joi.string().regex(lastName))
+                .joi(fields.lastName.error.invalid, Joi.string().trim().regex(lastName))
         );
     }
 
