47d38e12303234cf5f3f409b1f6e1c70e91457c7
satya.chundur@gmail.com <>
2018-03-05 15:12:02 +0000

    updated the shell script to reflect the end point

--- bin/runsmoketest.sh
+++ bin/runsmoketest.sh
@@ -14,5 +14,16 @@ trap shutdownDocker INT TERM QUIT EXIT
 docker-compose --version
 
 
+if [[ "${TEST_URL}" == *"aat.internal" ]]; then
+
+  cat "${TEST_URL} benefit-appeal.nonprod.platform.hmcts.net" > hosts
+  EXPORT ACTUAL_TEST_URL="https://benefit-appeal.nonprod.platform.hmcts.net"
+
+else
+  cat "${TEST_URL} benefit-appeal.platform.hmcts.net" > hosts
+  EXPORT ACTUAL_TEST_URL="https://benefit-appeal.platform.hmcts.net"
+fi
+
+
 docker-compose -f docker-compose.smoke-tests.yml run syatests
 docker-compose -f docker-compose.smoke-tests.yml down
