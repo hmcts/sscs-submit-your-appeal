29b0f4b59df2e3a4b8816de5d61ec86ea49521ad
satya <1426759+satyachundur@users.noreply.github.com>
2018-03-06 13:11:53 +0000

    updated to include sandbox environment

--- bin/runsmoketest.sh
+++ bin/runsmoketest.sh
@@ -14,16 +14,23 @@ trap shutdownDocker INT TERM QUIT EXIT
 docker-compose --version
 
 
-if [[ "${TEST_URL}" == *"aat.internal" ]]; then
+if [[ ${TEST_URL} == *"aat.internal" ]]; then
 
   cat "${TEST_URL} benefit-appeal.nonprod.platform.hmcts.net" > hosts
   EXPORT ACTUAL_TEST_URL="https://benefit-appeal.nonprod.platform.hmcts.net"
 
-else
+elif [[ ${TEST_URL} == *"prod.internal" ]]; then
+
   cat "${TEST_URL} benefit-appeal.platform.hmcts.net" > hosts
   EXPORT ACTUAL_TEST_URL="https://benefit-appeal.platform.hmcts.net"
-fi
+  
+elif [[ ${TEST_URL} == *"sandbox.internal" ]]; then
 
+  cat "${TEST_URL} benefit-appeal.demo.platform.hmcts.net/" > hosts
+  EXPORT ACTUAL_TEST_URL="https://benefit-appeal.demo.platform.hmcts.net/"
+else
+  EXPORT ACTUAL_TEST_URL=${TEST_URL}
+fi
 
 docker-compose -f docker-compose.smoke-tests.yml run syatests
 docker-compose -f docker-compose.smoke-tests.yml down
