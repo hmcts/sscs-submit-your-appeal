1d23160f133c9de740bd868b1ccbb897bd6af844
satya <1426759+satyachundur@users.noreply.github.com>
2018-03-06 14:05:35 +0000

    updated from cat to echo

--- bin/runsmoketest.sh
+++ bin/runsmoketest.sh
@@ -16,17 +16,17 @@ docker-compose --version
 
 if [[ ${TEST_URL} == *"aat.internal" ]]; then
 
-  cat "${TEST_URL} benefit-appeal.nonprod.platform.hmcts.net" > hosts
+  echo "${TEST_URL} benefit-appeal.nonprod.platform.hmcts.net" > hosts
   EXPORT ACTUAL_TEST_URL="https://benefit-appeal.nonprod.platform.hmcts.net"
 
 elif [[ ${TEST_URL} == *"prod.internal" ]]; then
 
-  cat "${TEST_URL} benefit-appeal.platform.hmcts.net" > hosts
+  echo "${TEST_URL} benefit-appeal.platform.hmcts.net" > hosts
   EXPORT ACTUAL_TEST_URL="https://benefit-appeal.platform.hmcts.net"
   
 elif [[ ${TEST_URL} == *"sandbox.internal" ]]; then
 
-  cat "${TEST_URL} benefit-appeal.demo.platform.hmcts.net" > hosts
+  echo "${TEST_URL} benefit-appeal.demo.platform.hmcts.net" > hosts
   EXPORT ACTUAL_TEST_URL="https://benefit-appeal.demo.platform.hmcts.net/"
 else
   EXPORT ACTUAL_TEST_URL=${TEST_URL}
