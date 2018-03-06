cdcb121cf9541bf9063075ced37b2d959d84f6f6
satya <1426759+satyachundur@users.noreply.github.com>
2018-03-06 14:44:05 +0000

    Update runsmoketest.sh

--- bin/runsmoketest.sh
+++ bin/runsmoketest.sh
@@ -17,19 +17,19 @@ docker-compose --version
 if [[ ${TEST_URL} == *"aat.internal" ]]; then
 
   echo "${TEST_URL} benefit-appeal.nonprod.platform.hmcts.net" > hosts
-  EXPORT ACTUAL_TEST_URL="https://benefit-appeal.nonprod.platform.hmcts.net"
+  export ACTUAL_TEST_URL="https://benefit-appeal.nonprod.platform.hmcts.net"
 
 elif [[ ${TEST_URL} == *"prod.internal" ]]; then
 
   echo "${TEST_URL} benefit-appeal.platform.hmcts.net" > hosts
-  EXPORT ACTUAL_TEST_URL="https://benefit-appeal.platform.hmcts.net"
+  export ACTUAL_TEST_URL="https://benefit-appeal.platform.hmcts.net"
   
 elif [[ ${TEST_URL} == *"sandbox.internal" ]]; then
 
   echo "${TEST_URL} benefit-appeal.demo.platform.hmcts.net" > hosts
-  EXPORT ACTUAL_TEST_URL="https://benefit-appeal.demo.platform.hmcts.net/"
+  export ACTUAL_TEST_URL="https://benefit-appeal.demo.platform.hmcts.net/"
 else
-  EXPORT ACTUAL_TEST_URL=${TEST_URL}
+  export ACTUAL_TEST_URL=${TEST_URL}
 fi
 
 docker-compose -f docker-compose.smoke-tests.yml run syatests
