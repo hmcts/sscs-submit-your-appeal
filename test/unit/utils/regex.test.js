e7d41b7a263a68c16b2ba16430e71c68d1cbce54
Ben Diggle <ben.diggle5@gmail.com>
2018-03-25 13:56:06 +0100

    Add more characters to the whitelist regex (#360)

--- test/unit/utils/regex.test.js
+++ test/unit/utils/regex.test.js
@@ -274,3 +274,97 @@ describe('validating a international mobile number', () => {
     });
 
 });
+
+describe('validating strings with special characters', () => {
+
+    it('should validate a string that contains a forward slash', () => {
+        const string = 'special/characters';
+        const stringValidator = string.match(whitelist);
+        expect(stringValidator).to.not.equal(null);
+    });
+
+    it('should validate a string that contains a backwards slash', () => {
+        const string = 'special\characters';
+        const stringValidator = string.match(whitelist);
+        expect(stringValidator).to.not.equal(null);
+    });
+
+    it('should validate a string that contains an underscore', () => {
+        const string = 'special_characters';
+        const stringValidator = string.match(whitelist);
+        expect(stringValidator).to.not.equal(null);
+    });
+
+    it('should validate a string that contains square brackets', () => {
+        const string = '[special characters]';
+        const stringValidator = string.match(whitelist);
+        expect(stringValidator).to.not.equal(null);
+    });
+
+    it('should validate a string that contains a plus symbol', () => {
+        const string = 'special+characters';
+        const stringValidator = string.match(whitelist);
+        expect(stringValidator).to.not.equal(null);
+    });
+
+    it('should validate a string that contains a percentage symbol', () => {
+        const string = 'special%characters';
+        const stringValidator = string.match(whitelist);
+        expect(stringValidator).to.not.equal(null);
+    });
+
+    it('should validate a string that contains an ampersand', () => {
+        const string = 'special&characters';
+        const stringValidator = string.match(whitelist);
+        expect(stringValidator).to.not.equal(null);
+    });
+
+    it('should validate a string that contains a full stop', () => {
+        const string = 'special characters.';
+        const stringValidator = string.match(whitelist);
+        expect(stringValidator).to.not.equal(null);
+    });
+
+    it('should validate a string that contains double quotes', () => {
+        const string = '"special characters"';
+        const stringValidator = string.match(whitelist);
+        expect(stringValidator).to.not.equal(null);
+    });
+
+    it('should validate a string that contains single quotes', () => {
+        const string = '\'special characters\'';
+        const stringValidator = string.match(whitelist);
+        expect(stringValidator).to.not.equal(null);
+    });
+
+    it('should validate a string that contains a question mark', () => {
+        const string = 'special characters?';
+        const stringValidator = string.match(whitelist);
+        expect(stringValidator).to.not.equal(null);
+    });
+
+    it('should validate a string that contains an exclamation mark', () => {
+        const string = 'special characters!';
+        const stringValidator = string.match(whitelist);
+        expect(stringValidator).to.not.equal(null);
+    });
+
+    it('should validate a string that contains parentheses', () => {
+        const string = 'special (characters)';
+        const stringValidator = string.match(whitelist);
+        expect(stringValidator).to.not.equal(null);
+    });
+
+    it('should validate a string that contains a colon', () => {
+        const string = 'special: characters';
+        const stringValidator = string.match(whitelist);
+        expect(stringValidator).to.not.equal(null);
+    });
+
+    it('should validate a string that contains a hyphen', () => {
+        const string = 'special-characters';
+        const stringValidator = string.match(whitelist);
+        expect(stringValidator).to.not.equal(null);
+    });
+
+});
