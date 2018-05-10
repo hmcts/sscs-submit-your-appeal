6f15494ef3444c928936bd509614774817896049
bendiggle <ben.diggle5@gmail.com>
2018-05-10 16:46:03 +0100

    functional tests and unit tests

---
+++ assets/js/date-picker/date-picker-utils.js
@@ -0,0 +1,49 @@
+const $ = require('jquery');
+const moment = require('moment');
+const { find } = require('lodash');
+
+const datePickerUtils = {
+
+  getIndexFromDate: (data, date) => find(data, { value: new Date(date) }).index,
+
+  getIndexOfDate: element => $(element).data('index').split('-').pop(),
+
+  getValueOfDate: element => new Date($(element).text()),
+
+  buildDatesArray: (index, value) => {
+    return {
+      index,
+      value
+    };
+  },
+
+  formatDateForDisplay: d => {
+    const date = moment(new Date(d));
+    return date.format('dddd D MMMM YYYY');
+  },
+
+  isDateAdded: (currentDateList, newDateList) => newDateList.length > currentDateList.length,
+
+  isDateRemoved: (currentDateList, newDateList) => currentDateList.length > newDateList.length,
+
+  sortDates: dates => dates.sort((date1, date2) => {
+    if (date1.value > date2.value) return 1;
+    if (date1.value < date2.value) return -1;
+    return 0;
+  }),
+
+  displayFirstOfMonth: date => {
+    const mDate = moment(date);
+    const day = mDate.format('D');
+    const month = mDate.format('MMM');
+    const displayMonth = {};
+    if (day === '1') {
+      const html = `${day} <p class="first-of-month">${month}</p>`;
+      displayMonth.content = html;
+    }
+    return displayMonth;
+  }
+
+};
+
+module.exports = datePickerUtils;
--- test/e2e/page-objects/hearing/datesCantAttend.js
+++ test/e2e/page-objects/hearing/datesCantAttend.js
@@ -1,3 +1,6 @@
+const assert = require('assert');
+const moment = require('moment');
+
 function enterDateCantAttendAndContinue(date, link) {
   const I = this;
 
@@ -20,8 +23,50 @@ function dontSeeFormattedDate(date) {
   I.dontSee(date.format('dddd D MMMM YYYY'));
 }
 
+function* hasSelectedClass(element) {
+  const I = this;
+
+  const classes = yield I.grabAttributeFrom(element, 'class');
+  const hasClass = classes.includes('active');
+  assert.equal(hasClass, true);
+}
+
+function* doesntHaveSelectedClass(element) {
+  const I = this;
+
+  const classes = yield I.grabAttributeFrom(element, 'class');
+  const hasClass = classes.includes('active');
+  assert.equal(hasClass, false);
+}
+
+function* selectDates(dates) {
+  const I = this;
+
+   for (let date of dates) {
+     const element = `//*[@data-date="${date}"]`;
+     I.click(element);
+     I.seeFormattedDate(moment(date));
+     yield * I.hasSelectedClass(element);
+   }
+}
+
+function* deselectDates(dates) {
+  const I = this;
+
+  for (let date of dates) {
+    const element = `//*[@data-date="${date}"]`;
+    I.click(element);
+    I.dontSeeFormattedDate(moment(date));
+    yield * I.doesntHaveSelectedClass(element);
+  }
+}
+
 module.exports = {
   enterDateCantAttendAndContinue,
   seeFormattedDate,
-  dontSeeFormattedDate
+  dontSeeFormattedDate,
+  hasSelectedClass,
+  doesntHaveSelectedClass,
+  selectDates,
+  deselectDates
 };
