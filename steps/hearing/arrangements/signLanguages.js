f427b2817bf49100fa98b93fec66a23c5d834f1b
barbaracassani <barbara@kitsch-en.net>
2018-06-01 17:30:55 +0100

    Separated list of sign languages from foreign languages

---
+++ steps/hearing/arrangements/signLanguages.js
@@ -0,0 +1,15 @@
+module.exports = [
+  'American Sign Language (ASL)',
+  'British Sign Language (BSL)',
+  'Deaf Blind ‑ Hands on Sign Language',
+  'International Sign Language',
+  'Lip Speaker Sign Language',
+  'Makaton Sign Language',
+  'Manual Alphabet',
+  'Note taker',
+  'Relay',
+  'SSE',
+  'Visual Frame',
+  'Sign Language (Others)',
+  'Sign Language Palantypist/Speech to TX'
+];
