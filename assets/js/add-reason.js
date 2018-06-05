4de7e1f1236d7dc5b877df2934b3acf991808a33
barbaracassani <barbara@kitsch-en.net>
2018-06-05 10:20:56 +0100

    render textarea

--- assets/js/add-reason.js
+++ assets/js/add-reason.js
@@ -1,4 +1,4 @@
-import nunjucks from 'nunjucks';
+const nunjucks = require('nunjucks');
 // todo: how on Earth do I define correctly this path for both dev and prod? Ben, heeelp... !
 const fields = require('../../dist/nunjucks/look-and-feel/components/fields.njk');
 const formElements = require('../../dist/nunjucks/formElements.njk');
@@ -7,12 +7,15 @@ import $ from 'jquery';
 class AddReason {
 
   constructor() {
-    let renderedHTML = fields.getExported((err, components) => {
+    fields.getExported((err, components) => {
       const textbox = components.textbox;
-      const renderedHTML = textbox('bla', 'bla', true);
-      $('.grid-row').append(renderedHTML.val)
-      const moreso = formElements.getExported((err, comps) => {
+      const renderedTextbox = textbox('bla', 'bla', true);
+      $('.grid-row').append(renderedTextbox.val)
+      formElements.getExported((err, comps) => {
         console.info('an so it contnues')
+        const textarea = comps.textarea;
+        const renderedTextarea = textarea('ugo', 'labelforugo', null, false, 'You can write as much as you want');
+        $('.grid-row').append(renderedTextarea.val)
       })
     })
   }
