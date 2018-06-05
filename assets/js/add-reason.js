6898dd0bcfb64670d9ba4bb550494f467415d980
barbaracassani <barbara@kitsch-en.net>
2018-06-05 10:23:52 +0100

    Form with all the elements

--- assets/js/add-reason.js
+++ assets/js/add-reason.js
@@ -7,15 +7,19 @@ import $ from 'jquery';
 class AddReason {
 
   constructor() {
+    $('.grid-row').append(`
+      <form id="dynamic-form" action="/reason-for-appealing/item-0" method="post" class="form">
+          <input class="button" type="submit" value="Continue">
+      </form>`);
     fields.getExported((err, components) => {
       const textbox = components.textbox;
       const renderedTextbox = textbox('bla', 'bla', true);
-      $('.grid-row').append(renderedTextbox.val)
+      $('#dynamic-form').append(renderedTextbox.val)
       formElements.getExported((err, comps) => {
         console.info('an so it contnues')
         const textarea = comps.textarea;
         const renderedTextarea = textarea('ugo', 'labelforugo', null, false, 'You can write as much as you want');
-        $('.grid-row').append(renderedTextarea.val)
+        $('#dynamic-form').append(renderedTextarea.val)
       })
     })
   }
