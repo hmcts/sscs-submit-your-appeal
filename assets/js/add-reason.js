cb1b4370ea9ab0143cef38f7ea736cf56de9108e
barbaracassani <barbara@kitsch-en.net>
2018-06-05 14:46:09 +0100

    Cleaning up a bit the branch

--- assets/js/add-reason.js
+++ assets/js/add-reason.js
@@ -1,5 +1,6 @@
 const nunjucks = require('nunjucks');
 // todo: how on Earth do I define correctly this path for both dev and prod? Ben, heeelp... !
+// ps: this works but doesn't strike me as very robust
 const fields = require('../../dist/nunjucks/look-and-feel/components/fields.njk');
 const formElements = require('../../dist/nunjucks/formElements.njk');
 import $ from 'jquery';
@@ -12,12 +13,13 @@ class AddReason {
     this.formId = 'dynamic-form';
     this.textboxId = 'item.whatYouDisagreeWith';
     this.textareaId = 'item.reasonForAppealing';
-
+    // you'll need a custom element to append the form to, this is just to play with it
     $('.grid-row').append(`
       <form id="${this.formId}" action="/reason-for-appealing/item-${this.counter}" method="post" class="form"></form>`);
 
     fields.getExported((err, components) => {
       const textbox = components.textbox;
+      // todo: be a star and import the json tokens as well, instead of hardcoding the text. We may as well go the whole hog!
       const renderedTextbox = textbox({
         id: this.textboxId
       }, 'What you disagree with');
@@ -35,21 +37,27 @@ class AddReason {
     this.addClickHandlers();
   }
   addClickHandlers() {
-    $(`#${this.formId}-submit`).on('click', (event) => {
-      console.info('handler invoked')
+    $(`#${this.formId}`).on('submit', (event) => {
       event.preventDefault();
       $.ajax({
         url: `/reason-for-appealing/item-${this.counter}`,
         type: 'post',
         dataType: 'json',
-        success: function (data) {
-          console.info('ah we so managed')
+        accepts: {
+          onlyJson: 'application/json'
+        },
+        success: (data) => {
+          // refresh the page so you get the updated list
+        },
+        error: (err) => {
+          // display the errors on the ajax form!
         },
         data: $(`#${this.formId}`).serializeArray()
       });
     })
   }
   render() {
+    // this method is pointless
     console.info('yo! render!');
   }
   static startAddReason() {
