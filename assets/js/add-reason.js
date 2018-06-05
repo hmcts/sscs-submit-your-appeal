21b9f6c34eee2c9dc8ddd73de572687d5c59c89e
barbaracassani <barbara@kitsch-en.net>
2018-06-05 09:45:50 +0100

    Correct file paths for both browser and node render

--- assets/js/add-reason.js
+++ assets/js/add-reason.js
@@ -1,5 +1,7 @@
 import nunjucks from 'nunjucks';
-const fields = require('../../dist/nunjucks/fields.njk');
+// todo: how on Earth do I define correctly this path for both dev and prod? Ben, heeelp... !
+const fields = require('../../dist/nunjucks/look-and-feel/components/fields.njk');
+const formElements = require('../../dist/nunjucks/formElements.njk');
 import $ from 'jquery';
 
 class AddReason {
@@ -9,6 +11,9 @@ class AddReason {
       const textbox = components.textbox;
       const renderedHTML = textbox('bla', 'bla', true);
       $('.grid-row').append(renderedHTML.val)
+      const moreso = formElements.getExported((err, comps) => {
+        console.info('an so it contnues')
+      })
     })
   }
   render() {
