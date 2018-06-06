21c303ec757641e6ef556bbab2aaa7e89ed93803
barbaracassani <barbara@kitsch-en.net>
2018-06-06 16:48:51 +0100

    Quick and dirty file acceptor

---
+++ test/file_acceptor/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+
+app.set('port', 3010);
+
+app.post('/upload/:filename', function (req, res) {
+  let filename = path.basename(req.params.filename);
+  filename = path.resolve(__dirname, filename);
+  const dst = fs.createWriteStream(filename);
+  req.pipe(dst);
+  dst.on('drain', function() {
+    console.log('drain', new Date());
+    req.resume();
+  });
+  req.on('error', (er) => console.error);
+  req.on('end', function () {
+    res.send(200);
+  });
+});
+
+http.createServer(app).listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
