f18cd9fe5f8dc79943a20c97b95ff22bacf95ccc
barbaracassani <barbara@kitsch-en.net>
2018-06-06 09:46:27 +0100

    Goes to the evidence upload page

--- steps/reasons-for-appealing/evidence-upload/EvidenceUpload.js
+++ steps/reasons-for-appealing/evidence-upload/EvidenceUpload.js
@@ -1,11 +1,30 @@
 const { Question, goTo } = require('@hmcts/one-per-page');
 const { form, text } = require('@hmcts/one-per-page/forms');
 const { answer } = require('@hmcts/one-per-page/checkYourAnswers');
+const Joi = require('joi');
 const paths = require('paths');
 
 class EvidenceUpload extends Question {
   static get path() {
     return paths.reasonsForAppealing.evidenceUpload;
+  }
+
+  get form() {
+    return form({
+      uploadEv: text.joi(
+        'some error',
+        Joi.any().required()
+      )
+    });
+  }
+
+
+  values() {
+    return {
+      reasonsForAppealing: {
+        otherReasons: this.fields.otherReasonForAppealing.value
+      }
+    };
   }
 
   next() {
