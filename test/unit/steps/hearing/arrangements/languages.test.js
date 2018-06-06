b0b9f3c747efc4a3f7784a863b11957f49383fdf
barbaracassani <barbara@kitsch-en.net>
2018-06-06 12:01:17 +0100

    unit tests

--- test/unit/steps/hearing/arrangements/languages.test.js
+++ test/unit/steps/hearing/arrangements/languages.test.js
@@ -1,8 +1,15 @@
 const { expect } = require('test/util/chai');
 const languages = require('steps/hearing/arrangements/languages');
+const signLanguages = require('steps/hearing/arrangements/signLanguages');
 
 describe('languages.js', () => {
   it('should return an array', () => {
     expect(languages).to.be.an('array');
+  });
+});
+
+describe('signLanguages.js', () => {
+  it('should return an array', () => {
+    expect(signLanguages).to.be.an('array');
   });
 });
