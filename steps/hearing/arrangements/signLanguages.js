a70837627e638a3b430948074b1957fbabbf88c6
barbaracassani <barbara@kitsch-en.net>
2018-06-07 16:05:56 +0100

    Updated list of sign languages

--- steps/hearing/arrangements/signLanguages.js
+++ steps/hearing/arrangements/signLanguages.js
@@ -1,15 +1,14 @@
 module.exports = [
   'American Sign Language (ASL)',
   'British Sign Language (BSL)',
-  'Deaf Blind ‑ Hands on Sign Language',
-  'International Sign Language',
-  'Lip Speaker Sign Language',
-  'Makaton Sign Language',
-  'Manual Alphabet',
-  'Note taker',
-  'Relay',
-  'SSE',
-  'Visual Frame',
-  'Sign Language (Others)',
-  'Sign Language Palantypist/Speech to TX'
+  'Hands on signing',
+  'International Sign (IS)',
+  'Lipspeaker',
+  'Makaton',
+  'Deafblind manual alphabet',
+  'Notetaker',
+  'Deaf Relay',
+  'Speech Supported English (SSE)',
+  'Visual frame signing',
+  'Palantypist / Speech to text'
 ];
