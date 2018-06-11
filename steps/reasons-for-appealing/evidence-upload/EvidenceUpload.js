e36e84d6a37c79bc893c1fbe7b0fcdbff09416df
barbaracassani <barbara@kitsch-en.net>
2018-06-11 15:49:56 +0100

    Added mkdir

--- steps/reasons-for-appealing/evidence-upload/EvidenceUpload.js
+++ steps/reasons-for-appealing/evidence-upload/EvidenceUpload.js
@@ -14,65 +14,81 @@ class EvidenceUpload extends Question {
     return paths.reasonsForAppealing.evidenceUpload;
   }
 
+  static makeDir(path, dirCallback) {
+    const p = pt.join(__dirname, path);
+    fs.stat(p, (fsError, stats) => {
+      if (fsError || !stats.isDirectory()) {
+        return fs.mkdir(p, dirCallback);
+      }
+      return dirCallback();
+    });
+  }
+
   static handleUpload(req, res, next) {
     const pathToUploadFolder = './../../../uploads';
     const logger = Logger.getLogger('EvidenceUpload.js');
 
-    if (req.method.toLowerCase() === 'post') {
-      const incoming = new formidable.IncomingForm({
-        uploadDir: pt.resolve(__dirname, pathToUploadFolder),
-        keepExtensions: true,
-        type: 'multipart'
-      });
-
-      incoming.once('error', er => {
-        logger.info('error while receiving the file from the client', er);
-      });
-
-      incoming.on('file', (field, file) => {
-        const pathToFile = `${pt.resolve(__dirname, pathToUploadFolder)}/${file.name}`;
-        fs.rename(file.path, pathToFile);
-      });
-
-      incoming.on('error', incomingError => {
-        logger.warn('an error has occured with form upload', incomingError);
-        req.resume();
-      });
-
-      incoming.on('aborted', () => {
-        logger.log('user aborted upload');
-      });
-
-      incoming.on('end', () => {
-        logger.log('-> upload done');
-      });
-
-      return incoming.parse(req, (uploadingError, fields, files) => {
-        if (uploadingError) {
-          return next(uploadingError);
-        }
-        const pathToFile = `${pt.resolve(__dirname, './../../../uploads')}/${files.uploadEv.name}`;
-
-        return request.post({
-          url: api.uploadEvidenceUrl,
-          formData: {
-            file: fs.createReadStream(pathToFile)
-          }
-        }, (forwardingError, resp, body) => {
-          if (!forwardingError) {
-            logger.info('No forwarding error, about to save data');
-            const b = JSON.parse(body);
-            req.body = {
-              uploadEv: b.documents[0].originalDocumentName,
-              link: b.documents[0]._links.self.href
-            };
-            return fs.unlink(pathToFile, next);
-          }
-          return next(forwardingError);
+    EvidenceUpload.makeDir(pathToUploadFolder, mkdirError => {
+      if (mkdirError) {
+        return next(mkdirError);
+      }
+      if (req.method.toLowerCase() === 'post') {
+        const incoming = new formidable.IncomingForm({
+          uploadDir: pt.resolve(__dirname, pathToUploadFolder),
+          keepExtensions: true,
+          type: 'multipart'
         });
-      });
-    }
-    return next();
+
+        incoming.once('error', er => {
+          logger.info('error while receiving the file from the client', er);
+        });
+
+        incoming.on('file', (field, file) => {
+          const pathToFile = `${pt.resolve(__dirname, pathToUploadFolder)}/${file.name}`;
+          fs.rename(file.path, pathToFile);
+        });
+
+        incoming.on('error', incomingError => {
+          logger.warn('an error has occured with form upload', incomingError);
+          req.resume();
+        });
+
+        incoming.on('aborted', () => {
+          logger.log('user aborted upload');
+        });
+
+        incoming.on('end', () => {
+          logger.log('-> upload done');
+        });
+
+        return incoming.parse(req, (uploadingError, fields, files) => {
+          if (uploadingError) {
+            return next(uploadingError);
+          }
+          const pathToFile = `${pt
+            .resolve(__dirname, './../../../uploads')}/${files.uploadEv.name}`;
+
+          return request.post({
+            url: api.uploadEvidenceUrl,
+            formData: {
+              file: fs.createReadStream(pathToFile)
+            }
+          }, (forwardingError, resp, body) => {
+            if (!forwardingError) {
+              logger.info('No forwarding error, about to save data');
+              const b = JSON.parse(body);
+              req.body = {
+                uploadEv: b.documents[0].originalDocumentName,
+                link: b.documents[0]._links.self.href
+              };
+              return fs.unlink(pathToFile, next);
+            }
+            return next(forwardingError);
+          });
+        });
+      }
+      return next();
+    });
   }
 
   get middleware() {
