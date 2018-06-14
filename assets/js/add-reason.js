16c875d9da43f25f65548954b7edc7eefffe4fb1
bendiggle <ben.diggle5@gmail.com>
2018-06-14 11:17:57 +0100

    Add error validation

--- assets/js/add-reason.js
+++ assets/js/add-reason.js
@@ -5,11 +5,8 @@ const fields = require('../../dist/nunjucks/look-and-feel/components/fields.njk'
 // const formElements = require('../../dist/nunjucks/formElements.njk');
 import $ from 'jquery';
 
-
-
-
 // get the results that have already been posted
-// post results - counter is shit
+// post results When click continue
 // Validation????
 
 class AddReason {
@@ -24,9 +21,12 @@ class AddReason {
     // you'll need a custom element to append the form to, this is just to play with it
     $('.add-another-add-link').before(`<div id="${this.formId}"></div>`);
 
+    this.textbox = '';
+
     this.addFields();
     this.addAnother();
-    this.addClickHandlers();
+    this.onSubmit();
+    // this.addClickHandlers();
   }
 
   addFields() {
@@ -37,7 +37,12 @@ class AddReason {
 
     fields.getExported((err, components) => {
       const textbox = components.textbox;
+
+      this.textbox = textbox;
+
       const textarea = components.textarea;
+
+      this.textarea = textarea;
       // todo: be a star and import the json tokens as well, instead of hardcoding the text. We may as well go the whole hog!
       const renderedTextbox = textbox({
         id: this.textboxId
@@ -52,30 +57,105 @@ class AddReason {
     });
   }
 
+  buildWhatYouDisagreeWithField(errors = [], value = '') {
+    return this.textbox({
+      id: this.textboxId,
+      errors,
+      value
+    }, 'What you disagree with');
+  }
+
+  buildWhyYouDisagreeField(errors, value) {
+    return this.textarea({
+      id: this.textareaId,
+      errors: errors || [],
+      value: value || ''
+    }, 'Why you disagree with it', null, false, 'You can write as much as you want');
+  }
+
+  onSubmit() {
+    $('input[type="submit"]').click(event => {
+      event.preventDefault();
+
+      const containers = $('.items-container');
+      let postCounter = 0;
+      $.each(containers, (index) => {
+        const values = this.buildAnswers(index);
+        $.ajax({
+          type: 'POST',
+          url: `/reason-for-appealing/item-${index}`,
+          data: values,
+          success: response => {
+            if (response.validationErrors) {
+              const whatYouDisagreeWith = response.validationErrors[0];
+              const whyYouDisagree = response.validationErrors[1];
+              const errorTextbox = this.buildWhatYouDisagreeWithField(whatYouDisagreeWith.errors, whatYouDisagreeWith.value);
+              const errorTextArea = this.buildWhyYouDisagreeField(whyYouDisagree.errors, whyYouDisagree.value);
+              $(`#items-${index}`).empty().append(errorTextbox.val).append(errorTextArea.val);
+              return false;
+            } else {
+              postCounter ++;
+              if ($(`#items-${index}`).children().hasClass('form-group-error')) {
+                $(`#items-${index} .form-group`)
+                  .removeClass('form-group-error')
+                  .children()
+                  .remove('.error-message');
+              }
+
+
+              console.log('added ' + index);
+
+              if (postCounter === containers.length) {
+                console.log('ACTUALLY FINISHED');
+              }
+
+              return true;
+              // CONTINUE LOOP
+            }
+            // build new form underneath
+            // refresh the page so you get the updated list
+          }
+        });
+      });
+
+      // GET ALL THE VALUES
+      // LOOP OVER AND POST ALL VALUES
+      // IF SUCCESSFUL THEN POST AGAIN TO PAGE
+      // ELSE DISPLAY VALIDATION
+
+    });
+  }
+
+  buildAnswers(index) {
+    const whatYouDisagreeWith = $(`#items-${index} #item\\.whatYouDisagreeWith`).val();
+    const whyYouDisagreeWith = $(`#items-${index} #item\\.reasonForAppealing`).val();
+    return {
+      'item.whatYouDisagreeWith': whatYouDisagreeWith,
+      'item.reasonForAppealing': whyYouDisagreeWith
+    };
+  }
+
   addAnother() {
     $('.add-another-add-link').click(event => {
       event.preventDefault();
-      $.ajax({
-        type: 'POST',
-        url: `/reason-for-appealing/item-${this.counter}`,
-        data: this.buildBodyJson(),
-        success: (a, b, c) => {
-          this.counter ++;
-          this.addFields();
-          console.log('here');
-          console.log(a);
-          console.log(b);
-          console.log(c);
-
-          // build new form underneath
-          // refresh the page so you get the updated list
-        },
-        error: (err) => {
-          console.log('meow')
-          console.log(err);
-          // display the errors on the ajax form!
-        }
-      });
+      console.log(this.counter);
+      this.counter ++;
+      this.addFields();
+      // $.ajax({
+      //   type: 'POST',
+      //   url: `/reason-for-appealing/item-${this.counter}`,
+      //   data: this.buildBodyJson(),
+      //   success: response => {
+      //
+      //     if (response.validationErrors) {
+      //       // display the errors
+      //     } else {
+      //
+      //     }
+      //     // build new form underneath
+      //     // refresh the page so you get the updated list
+      //   }
+      // });
     });
   }
 
@@ -83,20 +163,21 @@ class AddReason {
     let reasons;
 
     if ($('.noItems').length) {
+      console.log('NO Items')
       return [];
     } else {
-      return
+      return $('.add-another-list-item').text();
     }
 
-    return
-
-    const reasonsList = $('.add-another-list-item');
-    return $('.add-another-list-item');
+    // return
+    //
+    // const reasonsList = $('.add-another-list-item');
+    // return $('.add-another-list-item');
   }
 
   buildBodyJson() {
-    const whatYouDisagreeWith = $('#item\\.whatYouDisagreeWith').val();
-    const whyYouDisagreeWith = $('#item\\.reasonForAppealing').val();
+    const whatYouDisagreeWith = $(`#items-${this.counter} #item\\.whatYouDisagreeWith`).val();
+    const whyYouDisagreeWith = $(`#items-${this.counter} #item\\.reasonForAppealing`).val();
     return {
       'item.whatYouDisagreeWith': whatYouDisagreeWith,
       'item.reasonForAppealing': whyYouDisagreeWith
