635badeeab38f5f80342e11e5435c339fe608d43
bendiggle <ben.diggle5@gmail.com>
2018-06-15 13:52:41 +0100

    tidy up

--- assets/js/add-reason.js
+++ assets/js/add-reason.js
@@ -1,95 +1,112 @@
-const nunjucks = require('nunjucks');
-const request = require('superagent');
-// todo: how on Earth do I define correctly this path for both dev and prod? Ben, heeelp... !
-// ps: this works but doesn't strike me as very robust
-const fields = require('../../dist/nunjucks/look-and-feel/components/fields.njk');
-// const formElements = require('../../dist/nunjucks/formElements.njk');
 import $ from 'jquery';
+const fieldTemplates = require('../../dist/nunjucks/look-and-feel/components/fields.njk');
 
-// get the results that have already been posted
-// post results When click continue
-// Validation????
+// sort asset fields.njk
+// get content
+
 
 class AddReason {
 
   constructor() {
-    // todo increment the counter. Probably the simplest it's to read the dom of the list of the reasons already entered,
-    // instead of struggling to maintain an internal status.
     this.counter = 0;
     this.formId = 'dynamic-form';
     this.textboxId = 'item.whatYouDisagreeWith';
     this.textareaId = 'item.reasonForAppealing';
-    // you'll need a custom element to append the form to, this is just to play with it
-    $('.add-another-add-link').before(`<div id="${this.formId}"></div>`);
-
     this.textboxField = '';
     this.textareaField = '';
     this.items = [];
 
+    $('.add-another-add-link').before(`<div id="${this.formId}"></div>`);
+
+    this.removeDisplayList();
+    this.setUpFields();
+    this.getValues();
+  }
+
+  removeDisplayList() {
     if ($('.add-another-list').length) {
       $('.add-another-list').remove();
     }
-
-    this.setUpFields();
-
-    this.getValues();
-
-    // if (this.items.length > 0) {
-    //   this.buildForm();
-    // } else {
-      this.addFields();
-    // }
-
-    this.addAnother();
-    this.onSubmit();
   }
 
   buildForm() {
     const values = this.items.map((item, index) => {
-      return {
-        index,
+      const obj = {};
+      const fields = Object.keys(item.fields);
+      obj.index = index;
+      $.each(fields, (i, field) => {
+        obj[field] = {
+          value: item.fields[field].value
+        };
+      });
+      return obj;
+    });
 
+    $.each(values, (i, value) => {
+      if (i > 0) {
+        this.counter ++;
       }
-    })
+      this.addFields(value.whatYouDisagreeWith, value.reasonForAppealing);
+    });
   }
 
   getValues() {
     return $.ajax({
       type: 'GET',
-      url: `/reason-for-appealing`,
-      success: (response, b, c) => {
-        if (response.length > 0) {
-          this.items = response;
+      url: '/reason-for-appealing',
+      success: response => {
+        const fieldValues = Object.values(response);
+        if (fieldValues.length > 0) {
+          this.items = fieldValues;
+          this.buildForm();
+        } else {
+          this.addFields();
         }
+        this.addAnother();
+        this.onSubmit();
       }
-    })
+    });
   }
 
   setUpFields() {
-    fields.getExported((error, components) => {
+    fieldTemplates.getExported((error, components) => {
       this.textboxField = components.textbox;
       this.textareaField = components.textarea;
     });
   }
 
-  addFields() {
-    const whatYouDisagreeWithField = this.buildWhatYouDisagreeWithField();
-    const whyYouDisagreeField = this.buildWhyYouDisagreeField();
+  addFields(whatFieldDetails, reasonFieldDetails) {
+    const nullObj = {
+      errors: null,
+      value: null
+    };
+    const opts = {
+      whatYouDisagreeWith: whatFieldDetails || nullObj,
+      reasonForAppealing: reasonFieldDetails || nullObj
+    };
+    const whatYouDisagreeWithField = this.buildWhatYouDisagreeWithField(
+      [],
+      opts.whatYouDisagreeWith.value
+    );
+    const reasonForAppealingField = this.buildReasonForAppealingField(
+      [],
+      opts.reasonForAppealing.value
+    );
     $(`#${this.formId}`).append(`<div id="items-${this.counter}" class="items-container">`);
     $(`#items-${this.counter}`)
       .append(whatYouDisagreeWithField.val)
-      .append(whyYouDisagreeField.val);
+      .append(reasonForAppealingField.val);
   }
 
-  buildWhatYouDisagreeWithField(errors = [], value = '') {
+  buildWhatYouDisagreeWithField(errors, value) {
     return this.textboxField({
       id: this.textboxId,
-      errors,
-      value
+      errors: errors || [],
+      value: value || ''
     }, 'What you disagree with');
   }
 
-  buildWhyYouDisagreeField(errors, value) {
+  buildReasonForAppealingField(errors, value) {
     return this.textareaField({
       id: this.textareaId,
       errors: errors || [],
@@ -101,7 +118,6 @@ class AddReason {
     const that = this;
     $('form').submit(function(event) {
       event.preventDefault();
-
       const containers = $('.items-container');
       let answers = [];
 
@@ -109,10 +125,10 @@ class AddReason {
         answers.push(that.buildAnswers(index));
       });
 
-      const promiseSerial = funcs =>
+      const promiseSequence = funcs =>
         funcs.reduce((promise, func) =>
-            promise.then(result => func().then(Array.prototype.concat.bind(result))),
-          Promise.resolve([]));
+          promise.then(result => func().then(Array.prototype.concat.bind(result))),
+        Promise.resolve([]));
 
       const posts = answers.map((answer, index) => () => {
         return $.ajax({
@@ -120,7 +136,7 @@ class AddReason {
           url: `/reason-for-appealing/item-${index}`,
           data: answer,
           success: response => {
-            if (response.validationErrors) {
+            if (response.validationErrors.length > 0) {
               that.handleValidationError(index, response.validationErrors);
             } else {
               if ($(`#items-${index}`).children().hasClass('form-group-error')) {
@@ -134,13 +150,14 @@ class AddReason {
         });
       });
 
-      return promiseSerial(posts)
+      return promiseSequence(posts)
         .then(responses => {
-          const a = responses.filter(response => {
-            return response.validationErrors;
+          const fieldErrors = responses.filter(response => {
+            const errors = response.validationErrors;
+            return errors && errors.length > 0;
           });
 
-          if (a.length === 0) {
+          if (fieldErrors.length === 0) {
            this.submit();
           }
         });
@@ -148,21 +165,27 @@ class AddReason {
   }
 
   handleValidationError(index, validationErrors) {
-      const whatYouDisagreeWith = validationErrors[0];
-      const whyYouDisagree = validationErrors[1];
-      const errorTextbox = this.buildWhatYouDisagreeWithField(whatYouDisagreeWith.errors, whatYouDisagreeWith.value);
-      const errorTextArea = this.buildWhyYouDisagreeField(whyYouDisagree.errors, whyYouDisagree.value);
-      $(`#items-${index}`).empty()
-        .append(errorTextbox.val)
-        .append(errorTextArea.val);
+    const whatYouDisagreeWith = validationErrors[0];
+    const reasonForAppealing = validationErrors[1];
+    const errorTextbox = this.buildWhatYouDisagreeWithField(
+      whatYouDisagreeWith.errors,
+      whatYouDisagreeWith.value
+    );
+    const errorTextArea = this.buildReasonForAppealingField(
+      reasonForAppealing.errors,
+      reasonForAppealing.value
+    );
+    $(`#items-${index}`).empty()
+      .append(errorTextbox.val)
+      .append(errorTextArea.val);
   }
 
   buildAnswers(index) {
     const whatYouDisagreeWith = $(`#items-${index} #item\\.whatYouDisagreeWith`).val();
-    const whyYouDisagreeWith = $(`#items-${index} #item\\.reasonForAppealing`).val();
+    const reasonForAppealing = $(`#items-${index} #item\\.reasonForAppealing`).val();
     return {
       'item.whatYouDisagreeWith': whatYouDisagreeWith,
-      'item.reasonForAppealing': whyYouDisagreeWith
+      'item.reasonForAppealing': reasonForAppealing
     };
   }
 
@@ -183,4 +206,4 @@ class AddReason {
   }
 }
 
-export default AddReason
+export default AddReason;
