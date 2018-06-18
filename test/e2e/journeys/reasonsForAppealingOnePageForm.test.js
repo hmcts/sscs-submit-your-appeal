1b31d59322c18d1765519f472b157e2bc71a52f2
bendiggle <ben.diggle5@gmail.com>
2018-06-18 13:06:13 +0100

    e2e tests

--- test/e2e/journeys/reasonsForAppealingOnePageForm.test.js
+++ test/e2e/journeys/reasonsForAppealingOnePageForm.test.js
@@ -1,13 +1,10 @@
 const doYouWantTextMsgRemindersContent = require('steps/sms-notify/text-reminders/content.en.json');
 const selectors = require('steps/check-your-appeal/selectors');
-const DateUtils = require('utils/DateUtils');
-const moment = require('moment');
 const paths = require('paths');
 
 const doYouWantTextMsgReminders = doYouWantTextMsgRemindersContent.fields.doYouWantTextMsgReminders;
-const datesYouCantAttend = selectors.theHearing.datesYouCantAttend;
-const datesYouCantAttendHearingAnswer = `${datesYouCantAttend}  ${selectors.answer}`;
-const datesYouCantAttendHearingChange = `${datesYouCantAttend}  ${selectors.change}`;
+const reasonForAppealing = selectors.reasonsForAppealing.reasons;
+const reasonForAppealingChange = `${reasonForAppealing}-1  ${selectors.change}`;
 
 const whatYouDisagreeWithField = '#item\\.whatYouDisagreeWith';
 const reasonForAppealingField = '#item\\.reasonForAppealing';
@@ -59,7 +56,7 @@ Scenario('Adds reasons for appealing and sees them in check your answers', async
   });
 });
 
-Scenario('Selects a date when they cannot attend the hearing, then edits the date', async I => {
+Scenario('Enters a reason for appealing, then edits the reason', I => {
   I.enterDetailsFromStartToNINO();
   I.enterAppellantContactDetailsAndContinue();
   I.selectDoYouWantToReceiveTextMessageReminders(doYouWantTextMsgReminders.no);
@@ -87,19 +84,71 @@ Scenario('Selects a date when they cannot attend the hearing, then edits the dat
   });
 
   // Now Change the reason a different answer.
-  I.click('Change', datesYouCantAttendHearingChange);
+  I.click('Change', reasonForAppealingChange);
   I.seeCurrentUrlEquals(paths.reasonsForAppealing.reasonForAppealing);
-
-  twoReasons.forEach(reason => {
-    I.see(reason.whatYouDisagreeWith);
-    I.see(reason.reasonForAppealing);
-  });
+  I.waitForElement('#items-0');
 
   I.addAReasonForAppealing(
     `#items-0 ${whatYouDisagreeWithField}`,
     `#items-0 ${reasonForAppealingField}`,
     reasons[2]
   );
+  I.click('Continue');
+  I.click('Continue');
+  I.dontSee(reasons[0].whatYouDisagreeWith);
+  I.dontSee(reasons[0].reasonForAppealing);
+  I.see(reasons[2].whatYouDisagreeWith);
+  I.see(reasons[2].reasonForAppealing);
+});
+
+Scenario('Enters a reason for appealing, then removes the reason and sees errors', async I => {
+  I.enterDetailsFromStartToNINO();
+  I.enterAppellantContactDetailsAndContinue();
+  I.selectDoYouWantToReceiveTextMessageReminders(doYouWantTextMsgReminders.no);
+  I.selectDoYouHaveARepresentativeAndContinue(representative.fields.hasRepresentative.no);
+  I.addAReasonForAppealingAndThenClickAddAnother(
+    `#items-0 ${whatYouDisagreeWithField}`,
+    `#items-0 ${reasonForAppealingField}`,
+    reasons[0]
+  );
+  I.addAReasonForAppealing(
+    `#items-1 ${whatYouDisagreeWithField}`,
+    `#items-1 ${reasonForAppealingField}`,
+    reasons[1]
+  );
+  I.click('Continue');
+  I.enterAnythingElseAndContinue(testData.reasonsForAppealing.otherReasons);
+  I.readSendingEvidenceAndContinue();
+  I.enterDoYouWantToAttendTheHearing('No');
+  I.readYouHaveChosenNotToAttendTheHearingNoticeAndContinue();
+  I.confirmDetailsArePresent();
+
+  twoReasons.forEach(reason => {
+    I.see(reason.whatYouDisagreeWith);
+    I.see(reason.reasonForAppealing);
+  });
+
+  // Now Change the reason a different answer.
+  I.click('Change', reasonForAppealingChange);
+  I.seeCurrentUrlEquals(paths.reasonsForAppealing.reasonForAppealing);
+  I.waitForElement('#items-0');
+
+  I.addAReasonForAppealing(
+    `#items-0 ${whatYouDisagreeWithField}`,
+    `#items-0 ${reasonForAppealingField}`,
+    {
+      whatYouDisagreeWith: '',
+      reasonForAppealing: ''
+    }
+  );
+  I.click('Continue');
+  await I.hasErrorClass('#items-0');
+  I.addAReasonForAppealing(
+    `#items-0 ${whatYouDisagreeWithField}`,
+    `#items-0 ${reasonForAppealingField}`,
+    reasons[2]
+  );
+  I.click('Continue');
   I.click('Continue');
   I.dontSee(reasons[0].whatYouDisagreeWith);
   I.dontSee(reasons[0].reasonForAppealing);
