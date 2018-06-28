177aca9516cc3ffbeeb4156dcce7d448368ac4e4
Chris Grimble <chris@cg-it-solutions.com>
2018-06-28 16:10:39 +0100

    SSCS-3375 Change to use regional centre api

--- mocks/postcode_checker/index.js
+++ mocks/postcode_checker/index.js
@@ -9,49 +9,19 @@ const app = express();
 
 /* eslint-disable no-magic-numbers */
 app.set('port', 3011);
-app.get('/postcodes/:postcode', (req, res) => {
+app.get('/regionalcentre/:postcode', (req, res) => {
   const postcode = req.params.postcode;
   logger.log(`postcode request for ${postcode}`);
 
   const cannedRes = {
-    'EH8 8DX': {
-      country: {
-        gss_code: 'E92000001',
-        name: 'Scotland'
-      },
-      local_authority: {
-        name: 'Westminster',
-        gss_code: 'E09000033'
-      },
-      centre: {
-        type: 'Point',
-        coordinates: [
-          -0.141587558526369,
-          51.50100893654096
-        ]
-      }
-    },
-    default: {
-      country: {
-        gss_code: 'E92000001',
-        name: 'England'
-      },
-      local_authority: {
-        name: 'Westminster',
-        gss_code: 'E09000033'
-      },
-      centre: {
-        type: 'Point',
-        coordinates: [
-          -0.141587558526369,
-          51.50100893654096
-        ]
-      }
-    }
+    'EH8 8DX': { status: 200, body: { regionalcentre: 'Glasgow' } },
+    'ZX99 1AB': { status: 404, body: { } },
+    default: { status: 200, body: { regionalcentre: 'London' } }
   };
   const resJson = (postcode in cannedRes) ? cannedRes[postcode] : cannedRes.default;
 
-  res.send(JSON.stringify(resJson));
+  res.status(resJson.status);
+  res.json(resJson.body);
 });
 
 http.createServer(app).listen(app.get('port'), () => {
