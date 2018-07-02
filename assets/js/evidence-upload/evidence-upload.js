e636cb1bcc8cb0dbab26d6e63b0e160177d5be96
barbaracassani <barbara@kitsch-en.net>
2018-07-02 18:07:59 +0100

    upload file by ajax

--- assets/js/evidence-upload/evidence-upload.js
+++ assets/js/evidence-upload/evidence-upload.js
@@ -1,11 +1,13 @@
 import $ from 'jquery';
 import fieldTemplates from '@hmcts/look-and-feel/templates/look-and-feel/components/fields.njk';
 import errorSummary from '@hmcts/look-and-feel/templates/look-and-feel/components/errors.njk';
-
+// todo add accepts to the input
+// todo increase the counter
 class EvidenceUpload {
   constructor(elContainer) {
     this.elContainer = elContainer;
-    this.elId = 'file-upload-input';
+    this.formAction = '/evidence-upload/item-0';
+    this.elId = 'uploadEv';
     this.doTheUpload = this.doTheUpload.bind(this);
     fieldTemplates.getExported((error, components) => {
       if (components && components.fileupload) {
@@ -15,13 +17,31 @@ class EvidenceUpload {
     });
   }
   buildForm() {
-    return `<div id="upload-container">${this.fileupload}</div>`;
+    return `<div id="upload-container">
+    <form name=""></form>    
+    ${this.fileupload}</div>`;
   }
   hideUnnecessaryMarkup() {
     $('.add-another-add-link').hide();
   }
   doTheUpload() {
-    console.info('do the upload')
+    console.info('do the upload');
+    const formData = new FormData();
+    jQuery.each(jQuery('#' + this.elId)[0].files, function(i, file) {
+      console.info('filez ', file);
+      formData.append('file-' + i, file);
+    });
+    jQuery.ajax({
+      url: this.formAction,
+      data: formData,
+      cache: false,
+      contentType: false,
+      processData: false,
+      method: 'POST',
+      success: function(data){
+        alert(data);
+      }
+    });
   }
   attachEventListeners() {
     $('#' + this.elId).on('change', this.doTheUpload)
