ca0f0d2584296c6d252a950676c6e98e8d7939bb
barbaracassani <barbara@kitsch-en.net>
2018-07-05 11:05:17 +0100

    Integrated Chris' work

--- steps/reasons-for-appealing/evidence-upload/EvidenceUpload.js
+++ steps/reasons-for-appealing/evidence-upload/EvidenceUpload.js
@@ -10,127 +10,143 @@ const maxFileSize = config.get('features.evidenceUpload.maxFileSize');
 const Joi = require('joi');
 const paths = require('paths');
 const formidable = require('formidable');
-const pt = require('path');
-const fs = require('fs');
 const moment = require('moment');
+const stream = require('stream');
 const request = require('request');
-const { get } = require('lodash');
 const fileTypeWhitelist = require('steps/reasons-for-appealing/evidence-upload/fileTypeWhitelist');
 
 const maxFileSizeExceededError = 'MAX_FILESIZE_EXCEEDED_ERROR';
 const wrongFileTypeError = 'WRONG_FILE_TYPE_ERROR';
 const fileMissingError = 'FILE_MISSING_ERROR';
 
+
 class EvidenceUpload extends AddAnother {
   static get path() {
     return paths.reasonsForAppealing.evidenceUpload;
   }
 
-  static makeDir(path, dirCallback) {
-    const p = pt.join(__dirname, path);
-    fs.stat(p, (fsError, stats) => {
-      if (fsError || !stats.isDirectory()) {
-        return fs.mkdir(p, dirCallback);
-      }
-      return dirCallback();
-    });
-  }
-
   static handleUpload(req, res, next) {
-    const pathToUploadFolder = './../../../uploads';
     const logger = Logger.getLogger('EvidenceUpload.js');
 
     if (req.method.toLowerCase() === 'post') {
-      return EvidenceUpload.makeDir(pathToUploadFolder, mkdirError => {
-        if (mkdirError) {
-          return next(mkdirError);
+      const multiplier = 1024;
+      const incoming = new formidable.IncomingForm({
+        keepExtensions: true,
+        type: 'multipart',
+        maxFileSize: maxFileSize * multiplier * multiplier
+      });
+
+      incoming.once('error', er => {
+        logger.info('error while receiving the file from the client', er);
+      });
+
+      incoming.once('fileBegin', function fileBegin(field, file) {
+        if (file && file.name && !fileTypeWhitelist.find(el => el === file.type)) {
+          /* eslint-disable no-invalid-this */
+          return this.emit('error', wrongFileTypeError);
+          /* eslint-enable no-invalid-this */
         }
-        const multiplier = 1024;
-        const incoming = new formidable.IncomingForm({
-          uploadDir: pt.resolve(__dirname, pathToUploadFolder),
-          keepExtensions: true,
-          type: 'multipart',
-          maxFileSize: maxFileSize * multiplier * multiplier
-        });
+        return true;
+      });
 
-        incoming.once('error', er => {
-          logger.info('error while receiving the file from the client', er);
-        });
+      incoming.once('aborted', () => {
+        logger.log('user aborted upload');
+        return next(new Error());
+      });
 
-        incoming.once('fileBegin', function fileBegin(field, file) {
-          if (file && file.name && !fileTypeWhitelist.find(el => el === file.type)) {
-            /* eslint-disable no-invalid-this */
-            return this.emit('error', wrongFileTypeError);
-            /* eslint-enable no-invalid-this */
-          }
-          return true;
-        });
+      incoming.once('end', () => {
+        logger.log('-> upload done');
+      });
 
-        incoming.once('file', (field, file) => {
-          if (file.name && file.size) {
-            const pathToFile = `${pt.resolve(__dirname, pathToUploadFolder)}/${file.name}`;
-            fs.rename(file.path, pathToFile);
-          }
-        });
+      let uploadingFile = false;
+      incoming.onPart = part => {
+        if (!part.filename) {
+          // let formidable handle all non-file parts
+          incoming.handlePart(part);
+          return;
+        }
 
-
-        incoming.once('aborted', () => {
-          logger.log('user aborted upload');
-          return next(new Error());
-        });
-
-        incoming.once('end', () => {
-          logger.log('-> upload done');
-        });
-
-        return incoming.parse(req, (uploadingError, fields, files) => {
-          const unprocessableEntityStatus = 422;
-
-          if (uploadingError || !get(files, 'uploadEv.name')) {
-            /* eslint-disable operator-linebreak */
-            if (uploadingError &&
-              uploadingError.message &&
-              uploadingError.message.match(/maxFileSize exceeded/)) {
-              /* eslint-enable operator-linebreak */
-              // cater for the horrible formidable.js error
-              /* eslint-disable no-param-reassign */
-              uploadingError = maxFileSizeExceededError;
-              /* eslint-enable no-param-reassign */
+        const fileName = part.filename;
+        const fileData = new stream.PassThrough();
+        uploadingFile = true;
+        const apiRequest = request.post({
+          url: uploadEvidenceUrl,
+          formData: {
+            file: {
+              value: fileData,
+              options: {
+                filename: fileName,
+                contentType: part.mime,
+                knownLength: req.headers['content-length']
+              }
             }
-            // this is an obvious mistake but achieves our goal somehow.
-            // I'll have to come back to this.
-            res.status = unprocessableEntityStatus;
+          }
+        }, (forwardingError, resp, body) => {
+          if (!forwardingError) {
+            logger.info('No forwarding error, about to save data');
+            const b = JSON.parse(body);
             req.body = {
-              'item.uploadEv': uploadingError,
-              'item.link': ''
+              'item.uploadEv': b.documents[0].originalDocumentName,
+              'item.link': b.documents[0]._links.self.href
             };
+
             return next();
           }
-
-          const pathToFile = `${pt
-            .resolve(__dirname, pathToUploadFolder)}/${files.uploadEv.name}`;
-
-          return request.post({
-            url: uploadEvidenceUrl,
-            formData: {
-              file: fs.createReadStream(pathToFile)
-            }
-          }, (forwardingError, resp, body) => {
-            if (!forwardingError) {
-              logger.info('No forwarding error, about to save data');
-              const b = JSON.parse(body);
-              req.body = {
-                'item.uploadEv': b.documents[0].originalDocumentName,
-                'item.link': b.documents[0]._links.self.href
-              };
-              return fs.unlink(pathToFile, next);
-            }
-            return next(forwardingError);
-          });
+          return next(forwardingError);
         });
+
+        let fileSize = 0;
+        part.on('data', incomingData => {
+          fileSize += incomingData.length;
+          if (fileSize > maxFileSize * multiplier * multiplier) {
+            const errorMessage = `maxFileSize exceeded, received ${fileSize} bytes of file data`;
+            incoming._error(new Error(errorMessage));
+            apiRequest.abort();
+            return;
+          }
+          fileData.push(incomingData);
+        });
+
+        part.on('end', () => {
+          fileData.end();
+        });
+      };
+
+      incoming.on('error', uploadingError => {
+        /* eslint-disable operator-linebreak */
+        const unprocessableEntityStatus = 422;
+        if (uploadingError &&
+          uploadingError.message &&
+          uploadingError.message.match(/maxFileSize exceeded/)) {
+          /* eslint-enable operator-linebreak */
+          // cater for the horrible formidable.js error
+          /* eslint-disable no-param-reassign */
+          uploadingError = maxFileSizeExceededError;
+          /* eslint-enable no-param-reassign */
+        }
+        // this is an obvious mistake but achieves our goal somehow.
+        // I'll have to come back to this.
+        res.status = unprocessableEntityStatus;
+        req.body = {
+          'item.uploadEv': uploadingError
+        };
+        return next();
+      });
+
+      return incoming.parse(req, uploadingError => {
+        const unprocessableEntityStatus = 422;
+
+        if (!uploadingFile) {
+          // this is an obvious mistake but achieves our goal somehow.
+          // I'll have to come back to this.
+          res.status = unprocessableEntityStatus;
+          req.body = {
+            uploadEv: uploadingError
+          };
+          next();
+        }
       });
     }
-
     return next();
   }
 
