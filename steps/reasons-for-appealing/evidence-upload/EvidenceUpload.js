a2c4da523adcb43726612a6ecb34f0401efb9a2c
David Thompson <davethompson21@gmail.com>
2018-07-10 14:19:19 +0100

    Only attempting upload if POST is for a file

--- steps/reasons-for-appealing/evidence-upload/EvidenceUpload.js
+++ steps/reasons-for-appealing/evidence-upload/EvidenceUpload.js
@@ -28,7 +28,8 @@ class EvidenceUpload extends AddAnother {
   static handleUpload(req, res, next) {
     const logger = Logger.getLogger('EvidenceUpload.js');
 
-    if (req.method.toLowerCase() === 'post') {
+    const urlRegex = RegExp(`/${paths.reasonsForAppealing.evidenceUpload}/item-[0-9]*$`);
+    if (req.method.toLowerCase() === 'post' && urlRegex.test(req.originalUrl)) {
       const multiplier = 1024;
       const incoming = new formidable.IncomingForm({
         keepExtensions: true,
