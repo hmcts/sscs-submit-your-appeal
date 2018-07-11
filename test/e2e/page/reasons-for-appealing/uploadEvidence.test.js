16413bff35dd19d975078998d0d53cef086a18ae
David Thompson <davethompson21@gmail.com>
2018-07-11 14:01:34 +0100

    Fixing functional tests for upload evidence

--- test/e2e/page/reasons-for-appealing/uploadEvidence.test.js
+++ test/e2e/page/reasons-for-appealing/uploadEvidence.test.js
@@ -10,33 +10,23 @@ if (evidenceUploadEnabled) {
   Before(I => {
     I.createTheSession();
     I.amOnPage(paths.reasonsForAppealing.evidenceProvide);
+    I.selectAreYouProvidingEvidenceAndContinue(evidenceProvide.fields.evidenceProvide.yes);
   });
 
   Scenario('I can upload correctly a file', I => {
-    I.selectAreYouProvidingEvidenceAndContinue(evidenceProvide.fields.evidenceProvide.yes);
-    I.click('Continue');
     I.attachFile('#uploadEv', 'evidence.txt');
-    I.click('.button');
     I.dontSeeElement('.error-summary');
   });
+
   Scenario('I cannot upload the wrong type of file', I => {
-    I.selectAreYouProvidingEvidenceAndContinue(evidenceProvide.fields.evidenceProvide.yes);
     I.attachFile('#uploadEv', 'evidence.zip');
-    I.click('.button');
     I.seeElement('.error-summary');
     I.see(content.fields.uploadEv.error.wrongFileType);
   });
+
   Scenario('I cannot upload a very large file', I => {
-    I.selectAreYouProvidingEvidenceAndContinue(evidenceProvide.fields.evidenceProvide.yes);
     I.attachFile('#uploadEv', 'largefile.txt');
-    I.click('.button');
     I.seeElement('.error-summary');
     I.see(content.fields.uploadEv.error.maxFileSizeExceeded);
-  });
-  Scenario('I see an error if I submit the form without choosing a file', I => {
-    I.selectAreYouProvidingEvidenceAndContinue(evidenceProvide.fields.evidenceProvide.yes);
-    I.click('.button');
-    I.seeElement('.error-summary');
-    I.see(content.fields.uploadEv.error.required);
   });
 }
