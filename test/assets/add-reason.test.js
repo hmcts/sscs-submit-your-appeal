cad5e0890b57ab2b087df7f80d40fd5c89e5c033
barbaracassani <barbara@kitsch-en.net>
2018-07-13 14:07:02 +0100

    Test example

--- test/assets/add-reason.test.js
+++ test/assets/add-reason.test.js
@@ -2,17 +2,14 @@ const chai = require('chai');
 const expect = chai.expect;
 const jQuery = require('jquery');
 const { JSDOM } = require( 'jsdom' );
-const proxyquire = require('proxyquire');
-
 
 describe('add reason', () => {
-  let window;
-  let document;
   let AddReason;
   let addReason;
   let $;
-  beforeEach((done) => {
-    const jsdom = new JSDOM( '<div></div>' );
+
+  before((done) => {
+    const jsdom = new JSDOM('<body><div id="dynamic-form"><ul class="add-another-list"></ul>form goes here</div></body>');
 
     const { window } = jsdom;
     const { document } = window;
@@ -23,12 +20,22 @@ describe('add reason', () => {
     AddReason = require('assets/js/add-reason').default;
     addReason = new AddReason();
     done()
-
   });
+
   it('removes add-another on instantiation', () => {
     expect($('add-another-list').length).to.equal(0);
   });
-  it('builds a form on instantiation', () => {
 
+  it('# buildForm adds a dom node for each item', () => {
+    addReason.items = [{
+      'item.whatYouDisagreeWith': 'disagree',
+      'item.reasonForAppealing': 'reasons etc'
+    },
+      {
+        'item.whatYouDisagreeWith': 'disagree 2',
+        'item.reasonForAppealing': 'reasons etc 2'
+      }];
+    addReason.buildForm();
+    expect($('.items-container').length).to.equal(2);
   })
 });
