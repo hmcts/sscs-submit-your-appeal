b58aee75d676dc4ea586e82f029a6664e404d090
barbaracassani <barbara@kitsch-en.net>
2018-07-13 13:42:53 +0100

    works with add reason

--- test/assets/add-reason.test.js
+++ test/assets/add-reason.test.js
@@ -2,26 +2,27 @@ const chai = require('chai');
 const expect = chai.expect;
 const jQuery = require('jquery');
 const { JSDOM } = require( 'jsdom' );
+const proxyquire = require('proxyquire');
 
-const AddReason = require('assets/js/add-reason').default;
 
 describe('add reason', () => {
   let window;
   let document;
+  let AddReason;
   let addReason;
   let $;
   beforeEach((done) => {
     const jsdom = new JSDOM( '<div></div>' );
 
     const { window } = jsdom;
-    //window.document = window;
     const { document } = window;
     global.window = window;
     global.document = document;
 
     $ = global.jQuery = jQuery(window);
+    AddReason = require('assets/js/add-reason').default;
+    addReason = new AddReason();
     done()
-   // addReason = new AddReason();
 
   });
   it('removes add-another on instantiation', () => {
