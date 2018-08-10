1eaa831c055afed2932491724e080b505df4f7de
Barbara Cassani <barbaracassani@users.noreply.github.com>
2018-08-10 16:08:55 +0100

    Pathetic attempt to trigger a rebuild

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ app.use(helmet.referrerPolicy({ policy: 'origin' }));
 
 // Disallow search index indexing
 app.use((req, res, next) => {
-  // Setting headers stops pages being indexed even if indexed pages link to them.
+  // Setting headers stops pages being indexed even if indexed pages link to them
   res.setHeader('X-Robots-Tag', 'noindex');
   res.setHeader('X-Served-By', os.hostname());
   res.setHeader('Cache-Control', 'no-cache, max-age=0, must-revalidate, no-store');
