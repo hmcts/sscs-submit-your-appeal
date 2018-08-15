028578050b275f298f2c71e1edf9a4495eacb298
barbaracassani <barbara@kitsch-en.net>
2018-08-15 16:21:31 +0100

    Trying to enable smoke tests

--- test/e2e/smoke/appeal.test.js
+++ test/e2e/smoke/appeal.test.js
@@ -1,38 +1,44 @@
 const startAnAppealContent = require('landing-pages/start-an-appeal/content.en');
 const textRemindersContent = require('steps/sms-notify/text-reminders/content.en');
-const DateUtils = require('utils/DateUtils');
-const moment = require('moment');
+// const DateUtils = require('utils/DateUtils');
+// const moment = require('moment');
 const paths = require('paths');
 const mockData = require('test/e2e/data');
-const selectors = require('steps/check-your-appeal/selectors');
+/* eslint-disable max-len */
+// const selectors = require('steps/check-your-appeal/selectors');
 
 const appellant = mockData.appellant;
 const doYouWantTextMsgReminders = textRemindersContent.fields.doYouWantTextMsgReminders;
-const appellantPhoneNumberAnswer = `${selectors.appellant.phoneNumber} ${selectors.answer}`;
-const txtMsgRemindersMobAnswer = `${selectors.textMsgReminders.mobileNumber} ${selectors.answer}`;
+// const appellantPhoneNumberAnswer = `${selectors.appellant.phoneNumber} ${selectors.answer}`;
+// const txtMsgRemindersMobAnswer = `${selectors.textMsgReminders.mobileNumber} ${selectors.answer}`;
+
+const evidenceUploadEnabled = require('config').get('features.evidenceUpload.enabled');
 
 Feature('Full Journey');
 
-xScenario('Appellant full journey from /start-an-appeal to the /check-your-appeal page @smoke',
-  async I => {
-    const randomWeekDay = DateUtils.getDateInMilliseconds(
-      DateUtils.getRandomWeekDayFromDate(moment().utc().startOf('day').add(5, 'weeks'))
-    );
+if (!evidenceUploadEnabled) {
+  Scenario('Appellant full journey from /start-an-appeal to the /check-your-appeal page @smoke',
+    I => {
+    /*      const randomWeekDay = DateUtils.getDateInMilliseconds(
+        DateUtils.getRandomWeekDayFromDate(moment().utc().startOf('day').add(5, 'weeks'))
+      );*/
 
-    I.amOnPage(paths.landingPages.startAnAppeal);
-    I.click(startAnAppealContent.start);
-    I.enterDetailsFromStartToNINO();
-    I.enterAppellantContactDetailsWithMobileAndContinue(appellant.contactDetails.phoneNumber);
-    I.checkOptionAndContinue(doYouWantTextMsgReminders.yes);
-    I.checkOptionAndContinue('#useSameNumber-yes');
-    I.readSMSConfirmationAndContinue();
-    I.enterDetailsFromNoRepresentativeToUploadingEvidence();
-    await I.enterDetailsFromAttendingTheHearingDatePickerToEnd(randomWeekDay);
+      I.amOnPage(paths.landingPages.startAnAppeal);
+      I.click(startAnAppealContent.start);
+      I.enterDetailsFromStartToNINO();
+      I.enterAppellantContactDetailsWithMobileAndContinue(appellant.contactDetails.phoneNumber);
+      I.checkOptionAndContinue(doYouWantTextMsgReminders.yes);
+      I.checkOptionAndContinue('#useSameNumber-yes');
+      I.readSMSConfirmationAndContinue();
+      I.enterDetailsFromNoRepresentativeToUploadingEvidence();
+      /*    await I.enterDetailsFromAttendingTheHearingDatePickerToEnd(randomWeekDay);
     I.confirmDetailsArePresent();
     I.see(appellant.contactDetails.phoneNumber, appellantPhoneNumberAnswer);
     I.see(appellant.contactDetails.phoneNumber, txtMsgRemindersMobAnswer);
     I.seeCurrentUrlEquals(paths.checkYourAppeal);
     I.see('Personal Independence Payment (PIP)');
     I.see('Hearing arrangements');
-    I.see('Portuguese');
-  });
+    I.see('Portuguese');*/
+    });
+}
+/* eslint-enable max-len */
--- test/e2e/smoke/health.test.js
+++ test/e2e/smoke/health.test.js
@@ -2,7 +2,7 @@ const paths = require('paths');
 
 Feature('Health');
 
-xScenario('The API is up and responding to requests to /health @smoke', I => {
+Scenario('The API is up and responding to requests to /health @smoke', I => {
   I.amOnPage(paths.health);
   I.see('"status":"UP"');
 });
