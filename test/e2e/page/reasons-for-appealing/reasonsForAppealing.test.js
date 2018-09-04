4834d10f57f82f91587fb76cd57dcd9126619994
david.crespo <david.crespo@hmcts.net>
2018-09-04 16:36:23 +0100

    SSCS-3802 enable tests because they work fine, not sure why they were commented out

--- test/e2e/page/reasons-for-appealing/reasonsForAppealing.test.js
+++ test/e2e/page/reasons-for-appealing/reasonsForAppealing.test.js
@@ -2,7 +2,7 @@ const paths = require('paths');
 const content = require('steps/reasons-for-appealing/reason-for-appealing/content.en');
 const reasons = require('test/e2e/data').reasonsForAppealing.reasons;
 
-Feature('Reason For Appealing @batch-10');
+Feature('Reason For Appealing @batch-10 @crespo');
 
 Before(async I => {
   I.createTheSession();
@@ -13,12 +13,12 @@ After(I => {
   I.endTheSession();
 });
 
-xScenario('When I go to the page and there are no reasons I see the Add reason link', I => {
+Scenario('When I go to the page and there are no reasons I see the Add reason link', I => {
   I.see(content.noReasons);
   I.see(content.links.add);
 });
 
-xScenario('When I click the Add reason link I am taken to /reason-for-appealing/item-0', I => {
+Scenario('When I click the Add reason link I am taken to /reason-for-appealing/item-0', I => {
   I.see(content.noReasons);
   I.click(content.links.add);
   I.see(content.titleEdit);
@@ -26,31 +26,31 @@ xScenario('When I click the Add reason link I am taken to /reason-for-appealing/
   I.seeElement('textarea[name="item.reasonForAppealing"]');
 });
 
-xScenario('When I add a reason I see the reason in the list', I => {
+Scenario('When I add a reason I see the reason in the list', I => {
   I.enterReasonForAppealAndContinue(reasons[0], content.links.add);
   I.see(reasons[0].whatYouDisagreeWith);
 });
 
-xScenario('When I add a reason I see the add another reason link', I => {
+Scenario('When I add a reason I see the add another reason link', I => {
   I.enterReasonForAppealAndContinue(reasons[0], content.links.add);
   I.see(content.links.addAnother);
 });
 
-xScenario('When I add multiple reasons, I see them in the list', I => {
+Scenario('When I add multiple reasons, I see them in the list', I => {
   I.enterReasonForAppealAndContinue(reasons[0], content.links.add);
   I.enterReasonForAppealAndContinue(reasons[1], content.links.addAnother);
   I.see(reasons[0].whatYouDisagreeWith);
   I.see(reasons[1].whatYouDisagreeWith);
 });
 
-xScenario('When I add a reason and click the delete link, the reason is removed', I => {
+Scenario('When I add a reason and click the delete link, the reason is removed', I => {
   I.enterReasonForAppealAndContinue(reasons[0], content.links.add);
   I.see(reasons[0].whatYouDisagreeWith);
   I.click('Delete');
   I.dontSee(reasons[0].whatYouDisagreeWith);
 });
 
-xScenario('When adding a single reason, then remove it, I see Add reason', I => {
+Scenario('When adding a single reason, then remove it, I see Add reason', I => {
   I.enterReasonForAppealAndContinue(reasons[0], content.links.add);
   I.see(content.links.addAnother);
   I.click('Delete');
@@ -58,12 +58,12 @@ xScenario('When adding a single reason, then remove it, I see Add reason', I => 
   I.see(content.links.add);
 });
 
-xScenario('When I click Continue without adding a reason, I see errors', I => {
+Scenario('When I click Continue without adding a reason, I see errors', I => {
   I.click('Continue');
   I.see(content.listError);
 });
 
-xScenario('When I add a reason and the edit it, I see the new reason', I => {
+Scenario('When I add a reason and the edit it, I see the new reason', I => {
   I.enterReasonForAppealAndContinue(reasons[0], content.links.add);
   I.see(reasons[0].whatYouDisagreeWith);
   I.enterReasonForAppealAndContinue(reasons[1], 'Edit');
@@ -102,3 +102,13 @@ Scenario('When I use whitespace to pad out reason for appealing, I see errors', 
   I.click('Continue');
   I.see(content.fields.reasonForAppealing.error.notEnough);
 });
+
+Scenario('When I use whitespace to pad out reason for appealing, I see errors', I => {
+  I.click(content.links.add);
+  I.fillField('textarea[name="item.reasonForAppealing"]', reasons[4].reasonForAppealing);
+  I.fillField('input[name="item.whatYouDisagreeWith"]', reasons[0].whatYouDisagreeWith);
+  I.click('Continue');
+  I.see(content.fields.reasonForAppealing.error.notEnough);
+});
+
+
