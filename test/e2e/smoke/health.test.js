04539435fbaa811ab81505e140d0d1a14c120d52
david.crespo <david.crespo@hmcts.net>
2018-09-11 01:11:57 +0200

    Adjust verbose to right scripts and add retries to step

--- test/e2e/smoke/health.test.js
+++ test/e2e/smoke/health.test.js
@@ -4,5 +4,5 @@ Feature('Health');
 
 Scenario('The API is up, healthy and responding to requests to /health @smoke', I => {
   I.amOnPage(paths.health);
-  I.see('"status":"UP"');
+  I.retry(3).see('"status":"UP"');
 });
