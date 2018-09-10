37eb9650e2dca93c56b9a44ebfeb5616c431bf47
david.crespo <david.crespo@hmcts.net>
2018-09-10 18:39:44 +0200

    adjust locator to use strict locator

--- test/e2e/page-objects/start/benefit-type.js
+++ test/e2e/page-objects/start/benefit-type.js
@@ -1,7 +1,6 @@
 function enterBenefitTypeAndContinue(type) {
   const I = this;
-  I.waitForElement('#benefitType', 5);
-  I.fillField('#benefitType', type);
+  I.fillField({ id: 'benefitType' }, type);
   I.waitForElement('#benefitType__option--0', 5);
   I.click('#benefitType__option--0');
   I.click('Continue');
