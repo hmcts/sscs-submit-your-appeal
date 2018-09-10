cbcac8dc9a8e791493b2fc18a5cbe07554994299
david.crespo <david.crespo@hmcts.net>
2018-09-11 01:19:24 +0200

    add time between retries

--- test/e2e/smoke/health.test.js
+++ test/e2e/smoke/health.test.js
@@ -4,5 +4,8 @@ Feature('Health');
 
 Scenario('The API is up, healthy and responding to requests to /health @smoke', I => {
   I.amOnPage(paths.health);
-  I.retry(3).see('"status":"UP"');
+  I.retry({
+    retries: 3,
+    minTimeout: 3000
+  }).see('"status":"UP"');
 });
