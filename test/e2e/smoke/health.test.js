08c568c39db36af8002902edc071362af6fff462
david.crespo <david.crespo@hmcts.net>
2018-09-11 02:02:19 +0200

    retry checking api is up after 1 min

--- test/e2e/smoke/health.test.js
+++ test/e2e/smoke/health.test.js
@@ -5,7 +5,7 @@ Feature('Health');
 Scenario('The API is up, healthy and responding to requests to /health @smoke', I => {
   I.amOnPage(paths.health);
   I.retry({
-    retries: 3,
-    minTimeout: 3000
+    retries: 2,
+    minTimeout: 60000
   }).see('"status":"UP"');
 });
