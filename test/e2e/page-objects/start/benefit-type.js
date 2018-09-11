bc366ab38727c2cb9d9ce3c49982ee12d02209f0
david.crespo <david.crespo@hmcts.net>
2018-09-12 01:02:38 +0200

    use strict locator

--- test/e2e/page-objects/start/benefit-type.js
+++ test/e2e/page-objects/start/benefit-type.js
@@ -1,7 +1,7 @@
 function enterBenefitTypeAndContinue(type) {
   const I = this;
   I.fillField({ id: 'benefitType' }, type);
-  I.click({ id: 'benefitType__option--0'});
+  I.click({ id: 'benefitType__option--0' });
   I.click('Continue');
 }
 
