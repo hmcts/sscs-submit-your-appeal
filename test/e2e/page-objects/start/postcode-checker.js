ef7041ac2f5275cfd794df37255bdded11ba1036
david.crespo <david.crespo@hmcts.net>
2018-09-11 09:58:37 +0200

    add waitForElement because this one cannot be found easily

--- test/e2e/page-objects/start/postcode-checker.js
+++ test/e2e/page-objects/start/postcode-checker.js
@@ -1,8 +1,15 @@
 function enterPostcodeAndContinue(postcode) {
   const I = this;
 
-  I.fillField({ id: 'postcode' }, postcode);
+  I.waitForElement({
+    id: 'postcode'
+  }, 5);
+  I.fillField({
+    id: 'postcode'
+  }, postcode);
   I.click('Continue');
 }
 
-module.exports = { enterPostcodeAndContinue };
+module.exports = {
+  enterPostcodeAndContinue
+};
