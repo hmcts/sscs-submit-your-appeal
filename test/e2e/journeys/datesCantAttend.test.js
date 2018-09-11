dba215bd5fde2464dc39a8098d175adb484a5fc8
david.crespo <david.crespo@hmcts.net>
2018-09-12 01:08:15 +0200

    run only a minimum bunch of tests for tests

--- test/e2e/journeys/datesCantAttend.test.js
+++ test/e2e/journeys/datesCantAttend.test.js
@@ -32,21 +32,21 @@ Scenario('Provides date of when they cannot attend the hearing', async I => {
 }).retry(1);
 
 Scenario('Provides a date when they cannot attend the hearing then edits the date @journeys',
-async I => {
-  const randomWeekDayIn5Weeks = DateUtils.getRandomWeekDayFromDate(moment().add(5, 'weeks'));
-  const randomWeekDayIn6Weeks = DateUtils.getRandomWeekDayFromDate(moment().add(6, 'weeks'));
-  I.enterDetailsFromStartToNINO();
-  I.enterAppellantContactDetailsAndContinue();
-  I.selectDoYouWantToReceiveTextMessageReminders(doYouWantTextMsgReminders.no);
-  I.enterDetailsFromNoRepresentativeToUploadingEvidence();
-  await I.enterDetailsFromAttendingTheHearingToEnd(randomWeekDayIn5Weeks);
-  I.see(randomWeekDayIn5Weeks.format('DD MMMM YYYY'), datesYouCantAttendHearingAnswer);
+  async I => {
+    const randomWeekDayIn5Weeks = DateUtils.getRandomWeekDayFromDate(moment().add(5, 'weeks'));
+    const randomWeekDayIn6Weeks = DateUtils.getRandomWeekDayFromDate(moment().add(6, 'weeks'));
+    I.enterDetailsFromStartToNINO();
+    I.enterAppellantContactDetailsAndContinue();
+    I.selectDoYouWantToReceiveTextMessageReminders(doYouWantTextMsgReminders.no);
+    I.enterDetailsFromNoRepresentativeToUploadingEvidence();
+    await I.enterDetailsFromAttendingTheHearingToEnd(randomWeekDayIn5Weeks);
+    I.see(randomWeekDayIn5Weeks.format('DD MMMM YYYY'), datesYouCantAttendHearingAnswer);
 
-  // Now edit the single date from 10 to 11 weeks.
-  I.click('Change', datesYouCantAttendHearingChange);
-  I.seeCurrentUrlEquals(paths.hearing.hearingAvailability);
-  I.click('Continue');
-  I.enterDateCantAttendAndContinue(randomWeekDayIn6Weeks, 'Edit');
-  I.click('Continue');
-  I.see(randomWeekDayIn6Weeks.format('DD MMMM YYYY'), datesYouCantAttendHearingAnswer);
-}).retry(1);
+    // Now edit the single date from 10 to 11 weeks.
+    I.click('Change', datesYouCantAttendHearingChange);
+    I.seeCurrentUrlEquals(paths.hearing.hearingAvailability);
+    I.click('Continue');
+    I.enterDateCantAttendAndContinue(randomWeekDayIn6Weeks, 'Edit');
+    I.click('Continue');
+    I.see(randomWeekDayIn6Weeks.format('DD MMMM YYYY'), datesYouCantAttendHearingAnswer);
+  }).retry(1);
