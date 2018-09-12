6f376ad5c9b865d8e1de38c4856a1a81df85e287
david.crespo <david.crespo@hmcts.net>
2018-09-12 18:30:27 +0200

    fix esLint

--- test/e2e/page-objects/start/postcode-checker.js
+++ test/e2e/page-objects/start/postcode-checker.js
@@ -1,6 +1,6 @@
 function enterPostcodeAndContinue(postcode) {
   const I = this;
-  I.waitForElement({id: 'postcode'}, 5);
+  I.waitForElement({ id: 'postcode' }, 5);
   I.fillField({
     id: 'postcode'
   }, postcode);
