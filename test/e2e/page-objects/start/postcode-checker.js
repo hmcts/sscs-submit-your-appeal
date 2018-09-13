7c15a18f37ecda9419d0bf5ed4cecad0b4986c2f
david.crespo <david.crespo@hmcts.net>
2018-09-13 12:38:31 +0200

    fix lint

--- test/e2e/page-objects/start/postcode-checker.js
+++ test/e2e/page-objects/start/postcode-checker.js
@@ -1,6 +1,6 @@
 function enterPostcodeAndContinue(postcode) {
   const I = this;
-  
+
   I.retry({ retries: 3, minTimeout: 2000 }).fillField({ id: 'postcode' }, postcode);
   I.click('Continue');
 }
