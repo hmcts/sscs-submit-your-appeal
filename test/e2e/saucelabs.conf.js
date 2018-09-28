78d5da215a9fb9507b846cca2f71ec15178d0e62
Rob Doherty <robdohertykelly@gmail.com>
2018-09-28 12:13:53 +0100

    use logger in saucelabs.conf.js instead of console.log()

--- test/e2e/saucelabs.conf.js
+++ test/e2e/saucelabs.conf.js
@@ -1,9 +1,11 @@
-/* eslint-disable no-console, no-process-env */
+/* eslint-disable no-process-env */
 
 const config = require('config');
 const fileAcceptor = require('test/file_acceptor');
 const supportedBrowsers = require('./crossbrowser/supportedBrowsers.js');
+const { Logger } = require('@hmcts/nodejs-logging');
 
+const logger = Logger.getLogger('saucelabs.conf.js');
 const evidenceUploadEnabled = config.get('features.evidenceUpload.enabled');
 const tunnelName = process.env.SAUCE_TUNNEL_IDENTIFIER || config.get('saucelabs.tunnelId');
 
@@ -19,7 +21,7 @@ const getBrowserConfig = browserGroup => {
         desiredCapabilities: desiredCapability
       });
     } else {
-      console.error('ERROR: supportedBrowsers.js is empty or incorrectly defined');
+      logger.error('supportedBrowsers.js is empty or incorrectly defined');
     }
   }
   return browserConfig;
@@ -66,7 +68,7 @@ const setupConfig = {
   },
   teardownAll: done => {
     // Pause to allow SauceLabs to finish updating before Jenkins queries it for results
-    console.log('Wait for 30 seconds before Jenkins queries SauceLabs results...');
+    logger.info('Wait for 30 seconds before Jenkins queries SauceLabs results...');
     pauseFor(30);
     fileAcceptor.teardown(done);
   },
