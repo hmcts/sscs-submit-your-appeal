07ee901456bdcf5e984efb00eb5f429092a4b36d
Rob Doherty <robdohertykelly@gmail.com>
2018-10-12 17:24:30 +0100

    make uploadEvidencePage.js test page-object more robust by waiting for required elements

--- test/e2e/page-objects/upload-evidence/uploadEvidencePage.js
+++ test/e2e/page-objects/upload-evidence/uploadEvidencePage.js
@@ -1,7 +1,10 @@
 function uploadAPieceOfEvidence() {
   const I = this;
+  I.waitForVisible('label[for="uploadEv"]');
   I.attachFile('#uploadEv', 'evidence.txt');
-  I.click('.button');
+  I.waitForVisible('span[data-index="items-0"]');
+  I.waitForVisible('.add-another-delete-link');
+  I.click('Continue');
 }
 
 module.exports = {
