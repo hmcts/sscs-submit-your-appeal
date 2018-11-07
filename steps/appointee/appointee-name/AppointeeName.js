45fec90d17f6be749c6d75a27c866d9326854c78
Guilherme Zühlke O'Connor <guioconnor@gmail.com>
2018-11-07 16:57:32 +0000

    AppointeeName step

---
+++ steps/appointee/appointee-name/AppointeeName.js
@@ -0,0 +1,69 @@
+const { Question, goTo } = require('@hmcts/one-per-page');
+const { form, text } = require('@hmcts/one-per-page/forms');
+const { answer } = require('@hmcts/one-per-page/checkYourAnswers');
+const { whitelist, firstName, lastName } = require('utils/regex');
+const sections = require('steps/check-your-appeal/sections');
+const Joi = require('joi');
+const paths = require('paths');
+
+class AppointeeName extends Question {
+  static get path() {
+    return paths.appointee.enterAppointeeName;
+  }
+
+  get form() {
+    const fields = this.content.fields;
+    return form({
+      title: text.joi(
+        fields.title.error.required,
+        Joi.string().required()
+      ).joi(
+        fields.title.error.invalid,
+        Joi.string().regex(whitelist)
+      ),
+      firstName: text.joi(
+        fields.firstName.error.required,
+        Joi.string().required()
+      ).joi(
+        fields.firstName.error.invalid,
+        Joi.string().trim().regex(firstName)
+      ),
+      lastName: text.joi(
+        fields.lastName.error.required,
+        Joi.string().required()
+      ).joi(
+        fields.lastName.error.invalid,
+        Joi.string().trim().regex(lastName)
+      )
+    });
+  }
+
+  answers() {
+    const title = this.fields.title.value;
+    const first = this.fields.firstName.value;
+    const last = this.fields.lastName.value;
+    return [
+      answer(this, {
+        question: this.content.cya.appointeeName.question,
+        section: sections.appointeeDetails,
+        answer: `${title} ${first} ${last}`
+      })
+    ];
+  }
+
+  values() {
+    return {
+      appointee: {
+        title: this.fields.title.value,
+        firstName: this.fields.firstName.value,
+        lastName: this.fields.lastName.value
+      }
+    };
+  }
+
+  next() {
+    return goTo(this.journey.steps.AppointeeDOB);
+  }
+}
+
+module.exports = AppointeeName;
