2ea81165989460ce1939955c0fc7fcf990e199dd
Toni Sanchez <toninos@gmail.com>
2019-01-23 14:53:50 +0000

    Titles validation (#686)

---
+++ utils/titlesList.js
@@ -0,0 +1,32 @@
+const titleList = [
+  {
+    label: 'Mr',
+    value: 'Mr.'
+  }, {
+    label: 'Mrs',
+    value: 'Mrs.'
+  }, {
+    label: 'Miss',
+    value: 'Miss'
+  }, {
+    label: 'Ms',
+    value: 'Ms.'
+  }, {
+    label: 'Mr & Mrs',
+    value: 'Mr & Mrs'
+  }, {
+    label: 'Sir',
+    value: 'S/M'
+  }, {
+    label: 'Madam',
+    value: 'S/M'
+  }, {
+    label: 'Doctor',
+    value: 'Dr.'
+  }, {
+    label: 'Reverend',
+    value: 'Rev.'
+  }
+];
+
+module.exports = titleList;
