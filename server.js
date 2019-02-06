2d76666a836723d2a1b952a9d349a082e6c4bfa2
Chris Moreton <christopher.moreton@hmcts.net>
2019-02-06 16:24:56 +0000

    Enable shared app insights

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
+const appInsights = require('applicationinsights');
 const logger = require('logger');
 const app = require('app.js');
 const config = require('config');
 
+const logPath = 'server.js';
+const env = process.env.NODE_ENV || 'development';
+const roleName = 'sya';
+
 app.listen(config.node.port);
 
-const logPath = 'server.js';
+logger.trace(`NODE_ENV is ${env}`, logPath);
+
+if (env !== 'development') {
+  logger.trace(`appInsights role name is ${roleName}`, logPath);
+  appInsights.setup();
+  const client = appInsights.defaultClient;
+  client.context.tags[appInsights.defaultClient.context.keys.cloudRole] = roleName;
+}
 
 logger.trace(`SYA server listening on port: ${config.node.port}`, logPath);
