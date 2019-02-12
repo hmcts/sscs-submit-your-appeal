241902cde11e923a27e8921f7a2240af4665136d
Guilherme O'Connor <guilherme@fundapps.co>
2019-02-12 12:20:33 +0000

    Added regex validation to phone number joi rule

--- utils/customJoiSchemas.js
+++ utils/customJoiSchemas.js
@@ -1,5 +1,6 @@
 const Joi = require('joi');
 const { parsePhoneNumberFromString } = require('libphonenumber-js');
+const { phoneNumber } = require('utils/regex');
 
 const customJoi = Joi.extend(joi => {
   return {
@@ -24,13 +25,14 @@ const customJoi = Joi.extend(joi => {
         validate(params, value, state, options) {
           const parsedPhoneNumber = parsePhoneNumberFromString(value, 'GB');
           let isValidPhone = false;
+          const matchesRegex = phoneNumber.test(value);
           // If no phone-like structure comes of the parsing, a non-object is returned
           // so we need to explicitely check for the presence of the `isvalid` method
           if (parsedPhoneNumber && parsedPhoneNumber.isValid) {
             isValidPhone = parsedPhoneNumber.isValid();
           }
 
-          if (!isValidPhone) {
+          if (!isValidPhone || !matchesRegex) {
             return this.createError('string.validatePhone', { v: value }, state, options);
           }
 
