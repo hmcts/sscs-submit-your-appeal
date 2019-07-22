f29b0e7fc6b549dd7a2c94981524009fef86ed61
Izel Aciman <izelaciman@gmail.com>
2019-07-22 13:05:41 +0100

    Seperate webconfig from start.

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const https = require('https');
 const fs = require('graceful-fs');
 const webpack = require('webpack');
 const webpackDevConfig = require('./webpack/webpack.dev.js');
-const webpackProdConfig = require('./webpack/webpack.prod.js');
 const webpackMiddleware = require('webpack-dev-middleware');
 
 const logPath = 'server.js';
@@ -25,18 +24,7 @@ if (process.env.NODE_ENV === 'development') {
     });
   });
 } else {
-  const compiler = webpack(webpackProdConfig);
-  compiler.run((error, stats) => {
-    if (error) {
-      logger.trace.error(error);
-    }
-    if (stats) {
-      const time = stats.endTime - stats.startTime;
-      logger.trace(`Webpack build complete in ${time}ms. Hash ${stats.hash}`);
-      app.locals.webpackHash = stats.hash;
-      app.listen(config.node.port, () => {
-        logger.trace(`SYA server listening on port: ${config.node.port}`, logPath);
-      });
-    }
+  app.listen(config.node.port, () => {
+    logger.trace(`SYA server listening on port: ${config.node.port}`, logPath);
   });
 }
