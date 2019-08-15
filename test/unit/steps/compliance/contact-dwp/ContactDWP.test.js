8edb60678125236063d035605b9d2f9b40521e35
Toni sanchez <toninos@gmail.com>
2019-08-15 15:41:26 +0100

    adding unit tests to HaveContactedDWP and ContactDWP pages

--- test/unit/steps/compliance/contact-dwp/ContactDWP.test.js
+++ test/unit/steps/compliance/contact-dwp/ContactDWP.test.js
@@ -1,11 +1,21 @@
 const { expect } = require('test/util/chai');
 const ContactDWP = require('steps/compliance/contact-dwp/ContactDWP');
 const paths = require('paths');
+const config = require('config');
 
 describe('ContactDWP.js', () => {
   describe('get path()', () => {
     it('returns path /contact-dwp', () => {
       expect(ContactDWP.path).to.equal(paths.compliance.contactDWP);
+    });
+
+    it('get allowUC from config', () => {
+      const contactDWP = new ContactDWP({
+        journey: {
+          steps: {}
+        }
+      });
+      expect(contactDWP.allowUC).to.equal(config.get('features.allowUC.enabled') === 'true');
     });
   });
 });
