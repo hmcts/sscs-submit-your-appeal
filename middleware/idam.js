69cd7c22a6dd060d5c3d0ac0912d7c476977e8d9
david.crespo <david.crespo@hmcts.net>
2019-09-24 12:09:28 +0100

    add logging to debug issue with clearing cookies

--- middleware/idam.js
+++ middleware/idam.js
@@ -60,18 +60,18 @@ const methods = {
   },
   protect: (...args) => middleware.protect(idamArgs, ...args),
   logout: (req, res, next) => {
-    logger.console("*** logout ***", 1);
+    logger.console('*** logout ***', 1);
     const args = setArgsFromRequest(req);
-    logger.console("*** logout.args ***" + args, 1);
-    logger.console("*** logout.middleware.logout...", 1);
+    logger.console(`*** logout.args ***${args}`, 1);
+    logger.console('*** logout.middleware.logout...', 1);
     middleware.logout(args)(req, res, next);
     if (req.cookies) {
-      logger.console("*** logout.cookies: " + req.cookies, 1);
-      logger.console("*** logout.clearCookie: " + req.cookies['__auth-token'], 1);
+      logger.console(`*** logout.cookies: ${req.cookies}`, 1);
+      logger.console(`*** logout.clearCookie: ${req.cookies['__auth-token']}`, 1);
     }
     res.clearCookie('__auth-token');
-    logger.console("*** all done ***", 1);
-    logger.console("*** logout.cookies: " + req.cookies, 1);
+    logger.console('*** all done ***', 1);
+    logger.console(`*** logout.cookies: ${req.cookies}`, 1);
   },
   userDetails: () => middleware.userDetails(idamArgs)
 };
