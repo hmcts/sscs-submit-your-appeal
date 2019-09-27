bff7b2510c69f01e89447195fb2d2330c453d5b5
David Crespo <david.crespo@hmcts.net>
2019-09-27 10:22:24 +0100

    Bugfix/sscs 6648 save come back later (#843)
    * remove aut token from cookie more explicity
    
    * remove cookies explicity
    
    * fix unit test
    
    * add logging to debug issue with clearing cookies
    
    * add logging to debug issue with clearing cookies
    
    * fix more esLint issues
    
    * add next to continue
    
    * add more logs
    
    * clear logger
    
    * refactor the fix
    
    * fix esLint
    
    * move clear cookie to static method in the signOut class
    
    * disable sonar just for testing purpose
    
    * fix esLint issue
    
    * try req.hostname to get the domain this time
    
    * remove logger because it is not used
    
    * clear session and state cookies and add log to debug error
    
    * remove logs
    
    * add unit test for the clearCookies method
    
    * add semicolon
    
    * enable sonar again for the steps

--- steps/idam/sign-out/SignOut.js
+++ steps/idam/sign-out/SignOut.js
@@ -23,12 +23,30 @@ class SignOut extends ExitPoint {
     }
     return content.body.para2.replace('[mrn-date]', mrnDate);
   }
+
+  static clearCookies(req, res, next) {
+    res.clearCookie('__auth-token', {
+      path: '/',
+      domain: req.hostname,
+      httpOnly: true,
+      secure: true
+    });
+    res.clearCookie('__state', {
+      path: '/',
+      domain: req.hostname,
+      httpOnly: true,
+      secure: true
+    });
+    next();
+  }
+
   get middleware() {
     return [
       idam.authenticate,
       this.journey.collectSteps,
       ...super.middleware,
-      idam.logout
+      idam.logout,
+      SignOut.clearCookies
     ];
   }
 }
--- test/unit/steps/idam/SignOut.test.js
+++ test/unit/steps/idam/SignOut.test.js
@@ -18,6 +18,46 @@ describe('SignOut.js', () => {
     it('returns path /exit', () => {
       expect(SignOut.path).to.equal(paths.idam.signOut);
     });
+
+    it('clears cookies', () => {
+      // mocks and spies
+      const res = {
+        clearCookie: (cookieName, options) => {
+          return {
+            name: cookieName,
+            ops: options
+          };
+        }
+      };
+
+      const req = {
+        hostname: 'hmcts.net'
+      };
+
+      const resMock = sinon.mock(res);
+      resMock.expects('clearCookie').once().withArgs('__auth-token', {
+        path: '/',
+        domain: 'hmcts.net',
+        httpOnly: true,
+        secure: true
+      });
+      resMock.expects('clearCookie').once().withArgs('__state', {
+        path: '/',
+        domain: 'hmcts.net',
+        httpOnly: true,
+        secure: true
+      });
+
+      const next = sinon.spy();
+
+      // method under test
+      SignOut.clearCookies(req, res, next);
+
+      // expectations and verifications
+      expect(next).to.have.been.calledOnce;
+      resMock.verify();
+    });
+
     it('expects middleware', () => {
       const authenticateMock = sinon.stub(idam, 'authenticate');
       expect(signOut.middleware[0]).to.equal(authenticateMock);
