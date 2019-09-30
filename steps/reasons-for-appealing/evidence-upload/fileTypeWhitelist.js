b7c220c412b5374432effc745f45f5fa1581dbb9
Izel Aciman <izelaciman@gmail.com>
2019-09-30 13:16:53 +0100

    Update fileupload whitelist.

--- steps/reasons-for-appealing/evidence-upload/fileTypeWhitelist.js
+++ steps/reasons-for-appealing/evidence-upload/fileTypeWhitelist.js
@@ -1,29 +1,14 @@
 /*
-BMP
-DOC
-DOCX
-DOT
-DOTX
-JPEG
-JPG
-PDF
-PNG
-POT
-POTX
-PPA
-PPS
-PPSX
-PPT
-PPTX
-TIF
-TIFF
-TXT
-XLA
-XLS
-XLSB
-XLSX
-XLT
-XLTX
+jpg
+jpeg
+bmp
+tif
+tiff
+png
+pdf
+txt
+doc
+docx
 */
 module.exports = [
   '.jpg',
@@ -35,36 +20,12 @@ module.exports = [
   '.pdf',
   '.txt',
   '.doc',
-  '.dot',
   '.docx',
-  '.dotx',
-  '.xls',
-  '.xlt',
-  '.xla',
-  '.xlsx',
-  '.xltx',
-  '.xlsb',
-  '.ppt',
-  '.pot',
-  '.pps',
-  '.ppa',
-  '.pptx',
-  '.potx',
-  '.ppsx',
   'image/jpeg',
   'application/pdf',
   'image/tiff',
   'image/png',
   'image/bmp',
   'text/plain',
-  'application/msword',
-  'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-  'application/vnd.openxmlformats-officedocument.wordprocessingml.template',
-  'application/vnd.ms-excel',
-  'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-  'application/vnd.openxmlformats-officedocument.spreadsheetml.template',
-  'application/vnd.ms-powerpoint',
-  'application/vnd.openxmlformats-officedocument.presentationml.presentation',
-  'application/vnd.openxmlformats-officedocument.presentationml.template',
-  'application/vnd.openxmlformats-officedocument.presentationml.slideshow'
+  'application/msword'
 ];
