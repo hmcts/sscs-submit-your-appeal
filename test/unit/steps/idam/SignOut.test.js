311983536a005d38e1990952b9219a47763b4c7d
Izel Aciman <izelaciman@gmail.com>
2019-09-30 11:19:03 +0100

    fix unit tests.

--- test/unit/steps/idam/SignOut.test.js
+++ test/unit/steps/idam/SignOut.test.js
@@ -1,20 +1,11 @@
 const { expect, sinon } = require('test/util/chai');
 const SignOut = require('steps/idam/sign-out/SignOut');
-const idam = require('middleware/idam');
+
 const paths = require('paths');
-const moment = require('moment');
-const content = require('steps/idam/sign-out/content.en.json');
+
 
 describe('SignOut.js', () => {
   describe('get path()', () => {
-    let signOut = null;
-    beforeEach(() => {
-      signOut = new SignOut({
-        journey: {
-          steps: {}
-        }
-      });
-    });
     it('returns path /exit', () => {
       expect(SignOut.path).to.equal(paths.idam.signOut);
     });
@@ -56,31 +47,6 @@ describe('SignOut.js', () => {
       // expectations and verifications
       expect(next).to.have.been.calledOnce;
       resMock.verify();
-    });
-
-    it('expects middleware', () => {
-      const authenticateMock = sinon.stub(idam, 'authenticate');
-      expect(signOut.middleware[0]).to.equal(authenticateMock);
-    });
-
-    it('expects get a MRN date with getMRNDate', () => {
-      signOut.journey = {
-        visitedSteps: [
-          {
-            name: 'MRNDate',
-            valid: true
-          }
-        ],
-        values: {
-          mrn: {
-            date: '19-07-2019'
-          }
-        }
-      };
-      const theDate = moment(signOut.journey.values.mrn.date, 'DD-MM-YYYY')
-        .add('30', 'days')
-        .format('DD MMMM YYYY');
-      expect(signOut.getMRNDate()).to.equal(content.body.para2.replace('[mrn-date]', theDate));
     });
   });
 });
