9d402c74bc96e362b06ff535bf58c3367c8b6a07
david.crespo <david.crespo@hmcts.net>
2019-10-24 11:46:21 +0100

    fix checkstyle

--- test/unit/steps/idam/SignInBack.test.js
+++ test/unit/steps/idam/SignInBack.test.js
@@ -1,27 +1,25 @@
 const {
-    expect
+  expect
 } = require('test/util/chai');
 const SignInBack = require('steps/idam/sign-in-back/SignInBack');
 const paths = require('paths');
 
 describe.only('SignInBack.js', () => {
-    let signInBack = null;
+  let signInBack = null;
 
-    beforeEach(() => {
-        signInBack = new SignInBack({
-            journey: {
-                steps: {
-                    IdamRedirect: paths.start.idamRedirect
-                }
-            }
-        });
+  beforeEach(() => {
+    signInBack = new SignInBack({
+      journey: {
+        steps: {
+          IdamRedirect: paths.start.idamRedirect
+        }
+      }
     });
+  });
 
-    describe('get path()', () => {
-        it('returns path /sign-in-back', () => {
-            expect(signInBack.path).to.equal(paths.idam.signInBack);
-        });
+  describe('get path()', () => {
+    it('returns path /sign-in-back', () => {
+      expect(signInBack.path).to.equal(paths.idam.signInBack);
     });
-
-
+  });
 });
