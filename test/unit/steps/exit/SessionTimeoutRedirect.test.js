bd56fc8420c6e7c56bac0b64031b9aba08a2a4cd
david.crespo <david.crespo@hmcts.net>
2019-12-19 20:05:30 +0100

    fix lint issue

--- test/unit/steps/exit/SessionTimeoutRedirect.test.js
+++ test/unit/steps/exit/SessionTimeoutRedirect.test.js
@@ -6,8 +6,6 @@ const paths = require('paths');
 const steps = require('steps');
 const itParam = require('mocha-param');
 
-describe.only('SessionTimeoutRedirect.js', () => {});
-
 describe('path()', () => {
   it('returns path /session-timeout-redirect', () => {
     expect(SessionTimeoutRedirect.path).to.equal(paths.session.sessionTimeoutRedirect);
@@ -16,7 +14,7 @@ describe('path()', () => {
 
 function generateDifferentScenarios() {
   const signOutStep = steps.filter(step => step.name === 'SignOut').pop();
-  const sessionTimeoutStep = steps.filter(step => step.name === 'SessionTimeout').pop();
+  // const sessionTimeoutStep = steps.filter(step => step.name === 'SessionTimeout').pop();
   const scenarioWithLoggedUser = {
     req: {
       idam: {
@@ -33,18 +31,18 @@ function generateDifferentScenarios() {
     }
   };
 
-  const reqWithNoLoggedUser = {
-    journey: {
-      steps: {
-        SessionTimeoutStep: sessionTimeoutStep
-      }
-    }
-  };
+  // const reqWithNoLoggedUser = {
+  //   journey: {
+  //     steps: {
+  //       SessionTimeoutStep: sessionTimeoutStep
+  //     }
+  //   }
+  // };
   return [scenarioWithLoggedUser];
 }
 
 describe.only('next()', () => {
-  itParam('redirect to ', generateDifferentScenarios(), (scenario) => {
+  itParam('redirect to ', generateDifferentScenarios(), scenario => {
     const sessionTimeoutRedirect = new SessionTimeoutRedirect(scenario.req);
     expect(sessionTimeoutRedirect.next().step.path).to.equal(scenario.expected.path);
   });
