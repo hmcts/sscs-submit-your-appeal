c0234e56fc4ab662c3fa6407408433011bf591fa
david.crespo <david.crespo@hmcts.net>
2019-12-19 19:23:44 +0100

    fix lint issues

--- test/unit/steps/exit/SessionTimeoutRedirect.test.js
+++ test/unit/steps/exit/SessionTimeoutRedirect.test.js
@@ -3,11 +3,33 @@ const {
 } = require('test/util/chai');
 const SessionTimeoutRedirect = require('steps/exit-points/SessionTimeoutRedirect');
 const paths = require('paths');
+const steps = require('steps');
 
 describe.only('SessionTimeoutRedirect.js', () => {
-  describe('get path()', () => {
+  describe('path()', () => {
     it('returns path /session-timeout-redirect', () => {
       expect(SessionTimeoutRedirect.path).to.equal(paths.session.sessionTimeoutRedirect);
+    });
+  });
+
+  describe('next()', () => {
+    it.only('redirect to /sign-out if user is logged in', () => {
+      const signOutStep = steps.filter(step => step.name === 'SignOut').pop();
+      const sessionTimeoutStep = steps.filter(step => step.name === 'SessionTimeout').pop();
+      const req = {
+        idam: {
+          userId: '1'
+        },
+        journey: {
+          steps: {
+            SignOut: signOutStep,
+            SessionTimeoutStep: sessionTimeoutStep
+          }
+        }
+      };
+      
+      let sessionTimeoutRedirect = new SessionTimeoutRedirect(req);
+      expect(sessionTimeoutRedirect.next().step.path).to.equal(paths.idam.signOut);
     });
   });
 });
