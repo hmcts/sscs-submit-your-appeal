f4f9bc1828c8764492b06fcb59745f95e761413f
david.crespo <david.crespo@hmcts.net>
2020-01-27 22:17:58 +0000

    improve page object independence to wait for clickable button

--- test/e2e/smoke/appeal.test.js
+++ test/e2e/smoke/appeal.test.js
@@ -15,15 +15,4 @@ Scenario('Appellant full journey from /start-an-appeal to the /check-your-appeal
     I.checkOptionAndContinue(doYouWantTextMsgReminders.yes);
     I.checkOptionAndContinue('#useSameNumber-yes');
     I.readSMSConfirmationAndContinue();
-  });
-
-Scenario('debug issue on PR', I => {
-  I.amOnPage(paths.session.root);
-  I.enterBenefitTypeAndContinue('PIP');
-  I.enterPostcodeAndContinue('WV11 2HE');
-  I.seeCurrentUrlEquals(paths.start.independence);
-  I.waitForClickable({
-    css: 'input[type=submit][value=Continue]'
-  });
-  I.click('Continue');
-}).tag('@debug');
+  }).tag('@smoke');
