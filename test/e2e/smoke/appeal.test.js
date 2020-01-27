82daef78938561d40908ab595821eb769c63618d
david.crespo <david.crespo@hmcts.net>
2020-01-27 21:30:39 +0000

    debug functional test failure

--- test/e2e/smoke/appeal.test.js
+++ test/e2e/smoke/appeal.test.js
@@ -7,7 +7,7 @@ const doYouWantTextMsgReminders = textRemindersContent.fields.doYouWantTextMsgRe
 
 Feature('Full Journey');
 
-Scenario('Appellant full journey from /start-an-appeal to the /check-your-appeal page @smoke',
+Scenario('Appellant full journey from /start-an-appeal to the /check-your-appeal page',
   I => {
     I.amOnPage(paths.session.root);
     I.enterDetailsFromStartToNINO();
@@ -15,4 +15,15 @@ Scenario('Appellant full journey from /start-an-appeal to the /check-your-appeal
     I.checkOptionAndContinue(doYouWantTextMsgReminders.yes);
     I.checkOptionAndContinue('#useSameNumber-yes');
     I.readSMSConfirmationAndContinue();
-  }).retry(2);
+  });
+
+Scenario('debug issue on PR', I => {
+  I.amOnPage(paths.session.root);
+  I.enterBenefitTypeAndContinue('PIP');
+  I.enterPostcodeAndContinue('WV11 2HE');
+  I.seeCurrentUrlEquals(paths.start.independence);
+  I.waitForClickable({
+    css: 'input[type=submit][value=Continue]'
+  });
+  I.click('Continue')
+}).tag('@debug');
