1861821525e25c95c9463093d1e8525cf04c6f70
srochani <sunil.rochani@hotmail.com>
2020-07-08 15:37:52 +0100

    SSCS-7067: SYA – Display message to user if case is already created

--- test/unit/steps/errors/DuplicateError.test.js
+++ test/unit/steps/errors/DuplicateError.test.js
@@ -2,11 +2,12 @@ const DuplicateError = require('steps/errors/duplicate-error/DuplicateError');
 const { stub } = require('sinon');
 const DateUtils = require('utils/DateUtils');
 const { expect } = require('test/util/chai');
+const sinon = require('sinon');
 
 describe('DuplicateError.js', () => {
-  const req = stub();
+  let req = stub();
   let duplicateErrorClass = null;
-  const res = stub();
+  let res = stub();
   res.send = stub();
 
   beforeEach(() => {
@@ -22,6 +23,12 @@ describe('DuplicateError.js', () => {
       }
     });
   });
+
+  afterEach(() => {
+    req = {};
+    res = {};
+  });
+
   describe('get path()', () => {
     it('returns path /duplicate-case-error', () => {
       expect(DuplicateError.path).to.equal('/duplicate-case-error');
@@ -29,8 +36,11 @@ describe('DuplicateError.js', () => {
   });
 
   describe('handler', () => {
+    req.method = 'GET';
+    const duplicateErrorMrnDate = sinon.stub().returns('20 June 2020');
     it('calls res.send()', () => {
       duplicateErrorClass.handler(req, res);
+      duplicateErrorMrnDate().should.eql('20 June 2020');
     });
   });
 });
