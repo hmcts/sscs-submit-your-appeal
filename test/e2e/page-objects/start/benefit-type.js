aa8a1ea8ddb518d651205992b4b7f20bc58aae46
LucaDelBuonoHMCTS <luca.delbuono@hmcts.com>
2020-07-15 09:58:31 +0100

    Add Welsh e2e (WIP)

--- test/e2e/page-objects/start/benefit-type.js
+++ test/e2e/page-objects/start/benefit-type.js
@@ -1,7 +1,7 @@
 function enterBenefitTypeAndContinue(commonContent, type) {
   const I = this;
   I.retry({ retries: 3, minTimeout: 2000 }).fillField({ id: 'benefitType' }, type);
-  I.click({ id: 'benefitType__option--0' });
+  I.retry({ retries: 2, minTimeout: 2000 }).click('#benefitType__option--0');
   I.click(commonContent.continue);
 }
 
