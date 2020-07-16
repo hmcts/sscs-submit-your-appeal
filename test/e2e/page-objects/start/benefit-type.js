769990f341e7cdf30d31433d418b70ff0ad2d4ef
PhaniP <phani.perla@hmcts.net>
2020-07-16 09:15:37 +0100

    updated click method

--- test/e2e/page-objects/start/benefit-type.js
+++ test/e2e/page-objects/start/benefit-type.js
@@ -1,8 +1,8 @@
-function enterBenefitTypeAndContinue(commonContent, type) {
+function enterBenefitTypeAndContinue(type) {
   const I = this;
   I.retry({ retries: 3, minTimeout: 2000 }).fillField({ id: 'benefitType' }, type);
-  I.retry({ retries: 2, minTimeout: 2000 }).click('#benefitType__option--0');
-  I.click(commonContent.continue);
+  I.click({ id: 'benefitType__option--0' });
+  I.click('Continue');
 }
 
 module.exports = { enterBenefitTypeAndContinue };
