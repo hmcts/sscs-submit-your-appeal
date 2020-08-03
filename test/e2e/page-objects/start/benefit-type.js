602e408acc813cc60a75b8da10e0f1930898bc72
PhaniP <phani.perla@hmcts.net>
2020-08-03 09:48:35 +0100

    Added Functional Tag

--- test/e2e/page-objects/start/benefit-type.js
+++ test/e2e/page-objects/start/benefit-type.js
@@ -2,7 +2,7 @@ function enterBenefitTypeAndContinue(commonContent, type) {
   const I = this;
   I.retry({ retries: 3, minTimeout: 2000 }).fillField({ id: 'benefitType' }, type);
   I.click('#benefitType__option--0');
-  I.click(commonContent.continue);
+  I.click({xpath:'//input[@class="govuk-button" and @type="submit"]'});
 }
 
 module.exports = { enterBenefitTypeAndContinue };
