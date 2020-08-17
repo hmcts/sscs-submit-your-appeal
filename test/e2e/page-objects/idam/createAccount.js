fb53a14e8770c26ebc2f29f43cbbcecd80f1e5cc
rentaluser <rentaluser@rentalusers-MacBook-Pro.local>
2020-08-17 22:42:34 +0100

    scroll action on page step

--- test/e2e/page-objects/idam/createAccount.js
+++ test/e2e/page-objects/idam/createAccount.js
@@ -2,6 +2,7 @@ function selectIfYouWantToCreateAccount(commonContent, option) {
   const I = this;
 
   I.checkOption(option);
+  I.scrollIntoView(commonContent.continue, 5, 5);
   I.click(commonContent.continue);
 }
 
