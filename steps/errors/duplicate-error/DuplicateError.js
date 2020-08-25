46d6d6be53f5b9f17ff09cddc6371f08f56dbe4d
srochani <sunil.rochani@hotmail.com>
2020-08-25 10:25:38 +0100

    SSCS-7965: SYA – Message if case is already created isn’t working (#964)
    Co-authored-by: sunil <sunil.rochani@hmcts.net>

--- steps/errors/duplicate-error/DuplicateError.js
+++ steps/errors/duplicate-error/DuplicateError.js
@@ -17,7 +17,7 @@ class DuplicateError extends Page {
 
   get duplicateErrorMrnDate() {
     const d = this.req.session.MRNDate.mrnDate;
-    return DateUtils.createMoment(d.day, DateUtils.getMonthValue(d), d.year).format('DD MMM YYYY');
+    return DateUtils.createMoment(d.day, d.month, d.year).format('DD MMM YYYY');
   }
 }
 
