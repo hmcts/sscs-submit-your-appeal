9941253ad750abb530775d4e4aaa0e936c9b3c0f
sunilrochani <sunil.rochani@hotmail.com>
2020-09-02 08:52:41 +0100

    Fixed security scan issue

--- security.sh
+++ security.sh
@@ -15,8 +15,5 @@ while !(curl -s http://0.0.0.0:1001) > /dev/null
   zap-cli --zap-url http://0.0.0.0 -p 1001 report -o activescan.html -f html
   echo 'Changing owner from $(id -u):$(id -g) to $(id -u):$(id -u)'
   chown -R $(id -u):$(id -u) activescan.html
-  curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
   cp *.html functional-output/
-  zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l Medium --exit-code False
-
-
+  zap-cli -p 1001 alerts -l Medium
