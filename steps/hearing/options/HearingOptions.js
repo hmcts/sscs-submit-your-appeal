d5ab93a76a81467d6ead1436a8fc1b388050205b
Rasika.Abeyrathna <Rasika.Abeyrathna@hmcts.net>
2020-09-29 15:11:09 +0100

    SSCS-7814:SYA - Select all the suitable options for you to take part in the hearing

--- steps/hearing/options/HearingOptions.js
+++ steps/hearing/options/HearingOptions.js
@@ -4,6 +4,8 @@ const { form, object, text } = require('@hmcts/one-per-page/forms');
 const { answer } = require('@hmcts/one-per-page/checkYourAnswers');
 const paths = require('paths');
 const validOptions = require('steps/hearing/options/options');
+const emailOptions = require('utils/emailOptions');
+const customJoi = require('utils/customJoiSchemas');
 const { errorFor } = require('@hmcts/one-per-page/src/forms/validator');
 const Joi = require('joi');
 const {
@@ -18,21 +20,44 @@ class HearingOptions extends SaveToDraftStore {
 
   get form() {
     return form({
-      selectOptions: object({
-        option: text.joi(
-          this.content.fields.options.error.required,
-          Joi.string().valid([validOptions.telephone, validOptions.video, validOptions.faceToFace]).required()
-        ),
-        telephone: text,
-        email: text
-      }).check(
-        errorFor('telephone', this.content.fields.options.telephone.error.required),
-        value => emptyTelephoneValidation(value)
-      ).check(
-        errorFor('email', this.content.fields.options.video.error.required),
-        value => emptyEmailValidation(value)
-      )
-    });
+        selectOptions: object({
+          option: text.joi(
+            this.content.fields.options.error.required,
+            Joi.string().valid([validOptions.telephone, validOptions.video, validOptions.faceToFace]).required()
+          ),
+          telephone: text.joi(
+              this.content.fields.options.telephone.error.invalid,
+              customJoi.string().trim().validatePhone()
+          ),
+          email: text.joi(
+              this.content.fields.options.video.error.invalid,
+              Joi.string().trim().email(emailOptions).allow('')
+          )
+        }).check(
+          errorFor('telephone', this.content.fields.options.telephone.error.required),
+          value => emptyTelephoneValidation(value)
+        ).check(
+          errorFor('email', this.content.fields.options.video.error.required),
+          value => emptyEmailValidation(value)
+        )
+      });
+
+    // return form({
+    //   selectOptions: object({
+    //     option: text.joi(
+    //       this.content.fields.options.error.required,
+    //       Joi.string().valid([validOptions.telephone, validOptions.video, validOptions.faceToFace]).required()
+    //     ),
+    //     telephone: text,
+    //     email: text
+    //   }).check(
+    //     errorFor('telephone', this.content.fields.options.telephone.error.required),
+    //     value => emptyTelephoneValidation(value)
+    //   ).check(
+    //     errorFor('email', this.content.fields.options.video.error.required),
+    //     value => emptyEmailValidation(value)
+    //   )
+    // });
   }
 
   get telephone() {
