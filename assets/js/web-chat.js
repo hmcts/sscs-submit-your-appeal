9d9c298347a125e767301ba2f37cd45620c43473
Rasika.Abeyrathna <Rasika.Abeyrathna@hmcts.net>
2020-10-09 17:08:07 +0100

    Integration test of final version of webchat client

--- assets/js/web-chat.js
+++ assets/js/web-chat.js
@@ -3,27 +3,27 @@ const webChat = document.querySelector('web-chat');
 const message = document.querySelector('#metrics');
 
 export class WebChat {
-    init() {
-        if (button !== null && webChat !== null) {
-            button.addEventListener('click', () => {
-                webChat.classList.remove('hidden');
-            });
+  init() {
+    if (button !== null && webChat !== null) {
+      button.addEventListener('click', () => {
+        webChat.classList.remove('hidden');
+      });
 
-            webChat.addEventListener('hide', () => {
-                webChat.classList.add('hidden');
-            });
+      webChat.addEventListener('hide', () => {
+        webChat.classList.add('hidden');
+      });
 
-            webChat.addEventListener('metrics', metrics => {
-                const metricsDetail = metrics.detail;
-                const status = metricsDetail.ContactCentreState;
-                if (status === 'Open') {
-                    const ewt = metricsDetail.ewt;
-                    const agentCount = metricsDetail.agentCount;
-                    message.innerHTML = `Retrieved metrics: EWT = ${ewt}, available agents = ${agentCount}`;
-                } else {
-                    button.replaceWith(`Contact centre is ${status} now`);
-                }
-            });
+      webChat.addEventListener('metrics', metrics => {
+        const metricsDetail = metrics.detail;
+        const status = metricsDetail.ContactCentreState;
+        if (status === 'Open') {
+          const ewt = metricsDetail.ewt;
+          const agentCount = metricsDetail.agentCount;
+          message.innerHTML = `Retrieved metrics: EWT = ${ewt}, available agents = ${agentCount}`;
+        } else {
+          button.replaceWith(`Contact centre is ${status} now`);
         }
+      });
     }
+  }
 }
