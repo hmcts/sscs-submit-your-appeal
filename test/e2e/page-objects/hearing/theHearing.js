da598a2421e7ecd2e319f47046c3a2c064c35742
gokul-sol <gokul.sridharan@solirius.com>
2020-11-06 09:09:06 +0000

    Reduce wait time

--- test/e2e/page-objects/hearing/theHearing.js
+++ test/e2e/page-objects/hearing/theHearing.js
@@ -1,7 +1,7 @@
 function enterDoYouWantToAttendTheHearing(commonContent, option) {
   const I = this;
 
-  I.wait(10);
+  I.wait(5);
   I.checkOption(option);
   I.scrollPageToBottom();
   I.click(commonContent.continue);
