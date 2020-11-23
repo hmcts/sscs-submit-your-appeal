fd40a7b57f8515403264a29b63cd3457f549c49a
rasikasri <44141067+rasikasri@users.noreply.github.com>
2020-11-23 17:42:32 +0000

    SSCS-8432: Hearing Options should not appear when we say NO to attend hearing question FT (#1042)

--- test/e2e/e2e-sya/e2e.uc.cy.test.js
+++ test/e2e/e2e-sya/e2e.uc.cy.test.js
@@ -25,6 +25,7 @@ Scenario(`${language.toUpperCase()} - UC E2E SYA Journey @functional`, I => {
   I.readSMSConfirmationAndContinue(commonContent);
   I.enterDetailsFromNoRepresentativeToUploadingEvidence(commonContent);
   I.enterDoYouWantToAttendTheHearing(commonContent, '#attendHearing-yes');
+  I.selectTelephoneHearingOptionsAndContinue(commonContent);
   I.selectDoYouNeedSupportAndContinue(commonContent, '#arrangements-no');
   I.selectHearingAvailabilityAndContinue(commonContent, '#scheduleHearing-no');
   if (actUrl === aatUrl) I.completePcq();
