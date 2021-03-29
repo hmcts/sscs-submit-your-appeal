12e6c42f5798ee56d2b96c32897b0538b2c349f1
Rasika.Abeyrathna <Rasika.Abeyrathna@hmcts.net>
2021-03-29 10:17:27 +0100

    SSCS-8888: DLA (SYA) - End to End Automated Tests

---
+++ test/e2e/e2e-sya/e2e.dla.en.test.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-process-env */
+
+const language = 'en';
+
+const content = require('commonContent');
+const testData = require(`test/e2e/data.${language}`);
+
+const testDataEn = require('test/e2e/data.en');
+
+Feature(`${language.toUpperCase()} - DLA E2E SYA - Full Journey`);
+
+Scenario(`${language.toUpperCase()} - DLA E2E SYA PCQ Journey @functional`, I => {
+  const benefitCode = testDataEn.benefitTypes[3].code;
+  const office = testDataEn.benefitTypes[3].office;
+  const commonContent = content[language];
+  I.createTheSession(language);
+  I.wait(2);
+  I.enterCaseDetailsFromStartToNINO(commonContent, language, benefitCode, office);
+  I.enterAppellantContactDetailsWithMobileAndContinue(commonContent, language, '07411222222');
+  I.checkOptionAndContinue(commonContent, '#doYouWantTextMsgReminders-yes');
+  I.checkOptionAndContinue(commonContent, '#useSameNumber-yes');
+  I.readSMSConfirmationAndContinue(commonContent);
+  I.enterDetailsFromNoRepresentativeToUploadingEvidence(commonContent);
+  I.enterDoYouWantToAttendTheHearing(commonContent, '#attendHearing-yes');
+  I.selectTelephoneHearingOptionsAndContinue(commonContent);
+  I.selectDoYouNeedSupportAndContinue(commonContent, '#arrangements-no');
+  I.selectHearingAvailabilityAndContinue(commonContent, '#scheduleHearing-no');
+  I.completeAllPcq();
+  I.checkYourAppealToConfirmationPage(language, testData.signAndSubmit.signer);
+  I.endTheSession();
+}).retry(1);
