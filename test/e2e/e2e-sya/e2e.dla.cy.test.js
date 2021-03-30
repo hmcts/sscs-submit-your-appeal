2fab15afc3e2d1d5d5882d5ec03859be548a1aad
rasikasri <44141067+rasikasri@users.noreply.github.com>
2021-03-30 14:43:55 +0100

    Update e2e.dla.cy.test.js

--- test/e2e/e2e-sya/e2e.dla.cy.test.js
+++ test/e2e/e2e-sya/e2e.dla.cy.test.js
@@ -9,7 +9,7 @@ const testDataEn = require('test/e2e/data.en');
 
 Feature(`${language.toUpperCase()} - DLA E2E SYA - Full Journey`);
 
-Scenario(`${language.toUpperCase()} - DLA E2E SYA Journey @functional`, I => {
+Scenario(`${language.toUpperCase()} - DLA E2E SYA Journey @fullfunctional`, I => {
   const benefitCode = testDataEn.benefitTypes[3].code;
   const office = testDataEn.benefitTypes[3].office;
   const commonContent = content[language];
