0db4a4d8f0410771af2c48d5503bcafff44bd89b
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2021-04-29 12:02:00 +0100

    increase retry to 2

--- test/e2e/journeys/manualPostcodeLookup.en.test.js
+++ test/e2e/journeys/manualPostcodeLookup.en.test.js
@@ -23,4 +23,4 @@ Scenario(`${language.toUpperCase()} - Appellant enters contact details Manually`
   I.enterDetailsFromNoRepresentativeToEnd(commonContent);
   I.skipPcq();
   I.confirmDetailsArePresent(language);
-}).retry(1);
+}).retry(2);
