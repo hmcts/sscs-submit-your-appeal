f43bf66cd09563043bcbe4fff15e9b3e675fe229
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2021-05-07 16:13:26 +0100

    SSCS-8903 - Attendance Allowance (SYA) - End to End Automated Tests

--- test/e2e/e2e-sya/e2e.ca.en.test.js
+++ test/e2e/e2e-sya/e2e.ca.en.test.js
@@ -2,29 +2,16 @@
 
 const language = 'en';
 
-const content = require('commonContent');
 const testData = require(`test/e2e/data.${language}`);
-
 const testDataEn = require('test/e2e/data.en');
+const e2eBenefit = require('test/e2e/e2e-sya/e2e-benefit');
+
+const benefitCode = testDataEn.benefitTypes[4].code;
+const office = testDataEn.benefitTypes[4].office;
+const hasDwpIssuingOffice = testDataEn.benefitTypes[4].hasDwpIssuingOffice;
 
 Feature(`${language.toUpperCase()} - CA E2E SYA - Full Journey`);
 
-Scenario(`${language.toUpperCase()} - CA E2E SYA PCQ Journey @fullfunctional`, I => {
-  const searchName = testDataEn.benefitTypes[4].searchName;
-  const commonContent = content[language];
-  I.createTheSession(language);
-  I.wait(2);
-  I.enterDetailsFromStartToNINOWithSearchName(commonContent, language, searchName);
-  I.enterAppellantContactDetailsWithMobileAndContinue(commonContent, language, '07411222222');
-  I.checkOptionAndContinue(commonContent, '#doYouWantTextMsgReminders-yes');
-  I.checkOptionAndContinue(commonContent, '#useSameNumber-yes');
-  I.readSMSConfirmationAndContinue(commonContent);
-  I.enterDetailsFromNoRepresentativeToUploadingEvidence(commonContent);
-  I.enterDoYouWantToAttendTheHearing(commonContent, '#attendHearing-yes');
-  I.selectTelephoneHearingOptionsAndContinue(commonContent);
-  I.selectDoYouNeedSupportAndContinue(commonContent, '#arrangements-no');
-  I.selectHearingAvailabilityAndContinue(commonContent, '#scheduleHearing-no');
-  I.completeAllPcq();
-  I.checkYourAppealToConfirmationPage(language, testData.signAndSubmit.signer);
-  I.endTheSession();
+Scenario(`${language.toUpperCase()} - ${benefitCode} E2E SYA Journey @functional`, I => {
+  e2eBenefit.e2eBenefit(I, benefitCode, office, testData.signAndSubmit.signer, language, hasDwpIssuingOffice);
 }).retry(1);
--- test/e2e/e2e-sya/e2e.dla.cy.test.js
+++ test/e2e/e2e-sya/e2e.dla.cy.test.js
@@ -1,31 +1,16 @@
 /* eslint-disable no-process-env */
 
 const language = 'cy';
-
-const content = require('commonContent');
 const testData = require(`test/e2e/data.${language}`);
-
 const testDataEn = require('test/e2e/data.en');
+const e2eBenefit = require('test/e2e/e2e-sya/e2e-benefit');
+
+const benefitCode = testDataEn.benefitTypes[3].code;
+const office = testDataEn.benefitTypes[3].office;
+const hasDwpIssuingOffice = testDataEn.benefitTypes[3].hasDwpIssuingOffice;
 
 Feature(`${language.toUpperCase()} - DLA E2E SYA - Full Journey`);
 
 Scenario(`${language.toUpperCase()} - DLA E2E SYA Journey @fullfunctional`, I => {
-  const benefitCode = testDataEn.benefitTypes[3].code;
-  const office = testDataEn.benefitTypes[3].office;
-  const commonContent = content[language];
-  I.createTheSession(language);
-  I.wait(2);
-  I.enterCaseDetailsFromStartToNINO(commonContent, language, benefitCode, office);
-  I.enterAppellantContactDetailsWithMobileAndContinue(commonContent, language, '07411222222');
-  I.checkOptionAndContinue(commonContent, '#doYouWantTextMsgReminders-yes');
-  I.checkOptionAndContinue(commonContent, '#useSameNumber-yes');
-  I.readSMSConfirmationAndContinue(commonContent);
-  I.enterDetailsFromNoRepresentativeToUploadingEvidence(commonContent);
-  I.enterDoYouWantToAttendTheHearing(commonContent, '#attendHearing-yes');
-  I.selectTelephoneHearingOptionsAndContinue(commonContent);
-  I.selectDoYouNeedSupportAndContinue(commonContent, '#arrangements-no');
-  I.selectHearingAvailabilityAndContinue(commonContent, '#scheduleHearing-no');
-  I.completeAllPcqCY();
-  I.checkYourAppealToConfirmationPage(language, testData.signAndSubmit.signer);
-  I.endTheSession();
+  e2eBenefit.e2eBenefit(I, benefitCode, office, testData.signAndSubmit.signer, language, hasDwpIssuingOffice);
 }).retry(1);
