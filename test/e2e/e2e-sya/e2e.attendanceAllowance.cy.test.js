ace51859b3c327ff2ac84a54bbe2ad8bc347c29a
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2021-05-11 16:38:50 +0100

    change to hasDwpIssuingOffice to true for welsh attendance allowance

--- test/e2e/e2e-sya/e2e.attendanceAllowance.cy.test.js
+++ test/e2e/e2e-sya/e2e.attendanceAllowance.cy.test.js
@@ -6,8 +6,8 @@ const testDataEn = require('test/e2e/data.en');
 const e2eBenefit = require('test/e2e/e2e-sya/e2e-benefit');
 
 const benefitCode = testDataEn.benefitTypes[5].codeWelsh;
-const hasDwpIssuingOffice = testDataEn.benefitTypes[5].hasDwpIssuingOffice;
 const office = testDataEn.benefitTypes[5].office;
+const hasDwpIssuingOffice = true;
 
 Feature(`${language.toUpperCase()} - Attendance Allowance E2E SYA - Full Journey`);
 Scenario(`${language.toUpperCase()} - ${benefitCode} E2E SYA Journey @functional @e2e`, I => {
