717b38b65d61f3f67726e486f7284741a04440bf
Ryan Da Costa <ryan.dacosta1@hmcts.net>
2021-05-12 10:37:57 +0100

    retry 20 aa welsh

--- test/e2e/e2e-sya/e2e.attendanceAllowance.cy.test.js
+++ test/e2e/e2e-sya/e2e.attendanceAllowance.cy.test.js
@@ -12,4 +12,4 @@ const office = testDataEn.benefitTypes[5].office;
 Feature(`${language.toUpperCase()} - Attendance Allowance E2E SYA - Full Journey`);
 Scenario(`${language.toUpperCase()} - ${benefitCode} E2E SYA Journey @fullfunctional @e2e`, I => {
   e2eBenefit.e2eBenefit(I, benefitCode, office, signer, language, hasDwpIssuingOffice);
-}).retry(1);
+}).retry(20);
