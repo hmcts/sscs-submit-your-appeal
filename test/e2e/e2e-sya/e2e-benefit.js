6db4c31b68b3b36c15bfb295ed437be1d46fcab0
Ryan Da Costa <369407+ryandac@users.noreply.github.com>
2021-05-13 12:08:52 +0100

    SSCS-8903 - Attendance Allowance (SYA) - End to End Automated Tests (#1149)

--- test/e2e/e2e-sya/e2e-benefit.js
+++ test/e2e/e2e-sya/e2e-benefit.js
@@ -1,19 +1,10 @@
-/* eslint-disable no-process-env */
-
-const language = 'en';
-
 const content = require('commonContent');
-const testData = require(`test/e2e/data.${language}`);
 
-Feature(`${language.toUpperCase()} - PIP E2E SYA - Full Journey`);
-
-Scenario(`${language.toUpperCase()} - PIP E2E SYA Journey @functional @crossbrowser @e2e`, I => {
+function e2eBenefit(I, benefitCode, office, signer, language, hasDwpIssuingOffice) {
   const commonContent = content[language];
-
   I.createTheSession(language);
-
   I.wait(2);
-  I.enterDetailsFromStartToNINO(commonContent, language);
+  I.enterCaseDetailsFromStartToNINO(commonContent, language, benefitCode, office, hasDwpIssuingOffice);
   I.enterAppellantContactDetailsWithMobileAndContinue(commonContent, language, '07411222222');
   I.checkOptionAndContinue(commonContent, '#doYouWantTextMsgReminders-yes');
   I.checkOptionAndContinue(commonContent, '#useSameNumber-yes');
@@ -23,7 +14,16 @@ Scenario(`${language.toUpperCase()} - PIP E2E SYA Journey @functional @crossbrow
   I.selectTelephoneHearingOptionsAndContinue(commonContent);
   I.selectDoYouNeedSupportAndContinue(commonContent, '#arrangements-no');
   I.selectHearingAvailabilityAndContinue(commonContent, '#scheduleHearing-no');
-  I.skipPcq();
-  I.checkYourAppealToConfirmationPage(language, testData.signAndSubmit.signer);
+  if (language === 'en') {
+    I.completeAllPcq();
+  } else {
+    I.completeAllPcqCY();
+  }
+  I.checkYourAppealToConfirmationPage(language, signer);
   I.endTheSession();
-}).retry(2);
+}
+
+
+module.exports = {
+  e2eBenefit
+};
---
+++ test/e2e/e2e-sya/e2e.attendanceAllowance.en.test.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-process-env */
+
+const language = 'en';
+const signer = require(`test/e2e/data.${language}`).signAndSubmit.signer;
+const testDataEn = require('test/e2e/data.en');
+const e2eBenefit = require('test/e2e/e2e-sya/e2e-benefit');
+
+const benefitCode = testDataEn.benefitTypes[5].code;
+const office = testDataEn.benefitTypes[5].office;
+const hasDwpIssuingOffice = testDataEn.benefitTypes[5].hasDwpIssuingOffice;
+
+Feature(`${language.toUpperCase()} - Attendance Allowance E2E SYA - Full Journey`);
+
+Scenario(`${language.toUpperCase()} - ${benefitCode} E2E SYA Journey @fullfunctional @e2e`, I => {
+  e2eBenefit.e2eBenefit(I, benefitCode, office, signer, language, hasDwpIssuingOffice);
+}).retry(1);
