e04abcd471541f02a8a94e5978fb3fbc34d44e3b
Chris Davidson <christopher.davidson@btinternet.com>
2021-06-16 10:30:09 +0100

    sscs 8960 changed number

--- test/e2e/e2e-sya/e2e.maternityAllowance.en.test.js
+++ test/e2e/e2e-sya/e2e.maternityAllowance.en.test.js
@@ -5,9 +5,9 @@ const signer = require(`test/e2e/data.${language}`).signAndSubmit.signer;
 const testDataEn = require('test/e2e/data.en');
 const e2eBenefit = require('test/e2e/e2e-sya/e2e-benefit');
 
-const benefitCode = testDataEn.benefitTypes[9].code;
-const office = testDataEn.benefitTypes[9].office;
-const hasDwpIssuingOffice = testDataEn.benefitTypes[9].hasDwpIssuingOffice;
+const benefitCode = testDataEn.benefitTypes[8].code;
+const office = testDataEn.benefitTypes[8].office;
+const hasDwpIssuingOffice = testDataEn.benefitTypes[8].hasDwpIssuingOffice;
 
 Feature(`${language.toUpperCase()} - Industrial Injuries Disablement Benefit E2E SYA - Full Journey`);
 
