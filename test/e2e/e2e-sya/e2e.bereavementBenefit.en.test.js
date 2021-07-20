6576134802e8c091dfba4519a7a9e9da767754f6
Daniel Bell <daniel.bell@solirius.com>
2021-07-20 14:14:48 +0100

    Change tag name case

--- test/e2e/e2e-sya/e2e.bereavementBenefit.en.test.js
+++ test/e2e/e2e-sya/e2e.bereavementBenefit.en.test.js
@@ -11,6 +11,6 @@ const hasDwpIssuingOffice = testDataEn.benefitTypes[6].hasDwpIssuingOffice;
 
 Feature(`${language.toUpperCase()} - Bereavement Benefit E2E SYA - Full Journey`);
 
-Scenario(`${language.toUpperCase()} - ${benefitCode} E2E SYA Journey @fullfunctional @e2e`, I => {
+Scenario(`${language.toUpperCase()} - ${benefitCode} E2E SYA Journey @fullFunctional @e2e`, I => {
   e2eBenefit.e2eBenefit(I, benefitCode, office, signer, language, hasDwpIssuingOffice);
 }).retry(1);
--- test/e2e/e2e-sya/e2e.incomeSupport.cy.test.js
+++ test/e2e/e2e-sya/e2e.incomeSupport.cy.test.js
@@ -10,6 +10,6 @@ const hasDwpIssuingOffice = testDataEn.benefitTypes[11].hasDwpIssuingOffice;
 const office = testDataEn.benefitTypes[11].office;
 
 Feature(`${language.toUpperCase()} - Income Support E2E SYA - Full Journey`);
-Scenario(`${language.toUpperCase()} - ${benefitCode} E2E SYA Journey @fullfunctional @e2e`, I => {
+Scenario(`${language.toUpperCase()} - ${benefitCode} E2E SYA Journey @fullFunctional @e2e`, I => {
   e2eBenefit.e2eBenefit(I, benefitCode, office, signer, language, hasDwpIssuingOffice);
 }).retry(1);
