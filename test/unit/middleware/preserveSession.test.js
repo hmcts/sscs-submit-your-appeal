f827787966b94b1550a8370235bb4bdf78f35884
sunilrochani <sunil.rochani@hotmail.com>
2021-07-27 11:57:10 +0100

    SSCS 9422: SYA code coverage randomly dropped below 80%

--- test/unit/middleware/preserveSession.test.js
+++ test/unit/middleware/preserveSession.test.js
@@ -1,12 +1,12 @@
 const modulePath = 'middleware/preserveSession';
-const preserveSession = require(modulePath);
+const preserveSessionTest = require(modulePath);
 const { sinon, expect } = require('@hmcts/one-per-page-test-suite');
 
 describe(modulePath, () => {
   it('sets session to temporary req.sess', () => {
     const next = sinon.stub();
     const req = { session: { key: 'value' } };
-    preserveSession(req, {}, next);
+    preserveSessionTest(req, {}, next);
     expect(next.calledOnce).to.eql(true);
     expect(req.sess).to.eql(req.session);
   });
