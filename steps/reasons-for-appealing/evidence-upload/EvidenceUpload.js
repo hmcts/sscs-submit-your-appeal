07fd79778db1b90d79bc1a5244cecd87083b7c57
Chris Davidson <christopher.davidson@btinternet.com>
2021-10-22 16:07:51 +0100

    sscs 8711 fix deleting problem

--- steps/reasons-for-appealing/evidence-upload/EvidenceUpload.js
+++ steps/reasons-for-appealing/evidence-upload/EvidenceUpload.js
@@ -193,20 +193,21 @@ class EvidenceUpload extends SaveToDraftStoreAddAnother {
         logger.trace('No forwarding error, about to save data', logPath);
         const b = JSON.parse(body);
         if (b && b.documents) {
-          req.body = {
-            'item.uploadEv': b.documents[0].originalDocumentName,
-            'item.link': b.documents[0]._links.self.href,
-            'item.hashToken': b.documents[0].hashToken,
-            'item.size': size
-          };
-        } else {
-          console.log('Evidence upload document conversion error');
-          req.body = {
-            'item.uploadEv': technicalProblemError,
-            'item.link': '',
-            'item.hashToken': '',
-            'item.size': 0
-          };
+          if (b.documents[0].hashToken) {
+            req.body = {
+              'item.uploadEv': b.documents[0].originalDocumentName,
+              'item.link': b.documents[0]._links.self.href,
+              'item.hashToken': b.documents[0].hashToken,
+              'item.size': size
+            };
+          } else {
+            req.body = {
+              'item.uploadEv': b.documents[0].originalDocumentName,
+              'item.link': b.documents[0]._links.self.href,
+              'item.hashToken': '',
+              'item.size': size
+            };
+          }
         }
         return fs.unlink(pathToFile, next);
       }
@@ -280,12 +281,20 @@ class EvidenceUpload extends SaveToDraftStoreAddAnother {
 
   values() {
     const evidences = this.fields.items.value.map(file => {
-      return {
-        url: file.link,
-        fileName: file.uploadEv,
-        hashToken: file.hashToken,
-        uploadedDate: moment().format('YYYY-MM-DD')
-      };
+      if (file.hashToken) {
+        return {
+          url: file.link,
+          fileName: file.uploadEv,
+          hashToken: file.hashToken,
+          uploadedDate: moment().format('YYYY-MM-DD')
+        };
+      } else {
+        return {
+          url: file.link,
+          fileName: file.uploadEv,
+          uploadedDate: moment().format('YYYY-MM-DD')
+        };
+      }
     });
     return {
       reasonsForAppealing: {
