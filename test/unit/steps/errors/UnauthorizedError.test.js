d23a179522dd0590472d709be3d0674d61a684e0
Mohammed Qureshi <mohammed.qureshi@HMCTS.NET>
2022-01-17 11:18:20 +0000

    remove unused res method

--- test/unit/steps/errors/UnauthorizedError.test.js
+++ test/unit/steps/errors/UnauthorizedError.test.js
@@ -2,36 +2,32 @@ const UnauthorizedError = require('steps/errors/unauthorized/UnauthorizedError')
 const { expect } = require('test/util/chai');
 const paths = require('paths');
 const { stub } = require('sinon');
-const sinon = require('sinon');
 
 describe('UnauthorizedError.js', () => {
-  let req = {}
-  let res = {}
+  let req = {};
+  let res = {};
   let unauthorizedErrorClass = null;
 
   beforeEach(() => {
-    req = {}
+    req = {};
     res = {
       render: stub()
-    }
+    };
 
     unauthorizedErrorClass = new UnauthorizedError({
       journey: {
         steps: {
           UnauthorizedError: paths.errors.UnauthorizedErrorClass
-        },
-        res: {
-          send: stub()
         }
       }
-    })
-  })
+    });
+  });
 
   describe('get path()', () => {
     it('returns path /unauthorized-case-error', () => {
       expect(UnauthorizedError.path).to.equal('/unauthorized-case-error');
-    })
-  })
+    });
+  });
 
   describe('handler when method is GET', () => {
     it('calls res.render', () => {
@@ -39,15 +35,13 @@ describe('UnauthorizedError.js', () => {
       unauthorizedErrorClass.handler(req, res);
       expect(res.render).to.have.been.calledOnce;
     });
-  })
+  });
 
-    describe('handler when method is POST', () => {
-      it('not calls res.send()', () => {
-        req.method = 'POST';
-        unauthorizedErrorClass.handler(req, res);
-        expect(res.render).to.have.not.been.called;
-      })
-    })
-
-
-  })
+  describe('handler when method is POST', () => {
+    it('not calls res.send()', () => {
+      req.method = 'POST';
+      unauthorizedErrorClass.handler(req, res);
+      expect(res.render).to.have.not.been.called;
+    });
+  });
+});
