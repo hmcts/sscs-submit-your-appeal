57fed7a18f64d4c91c899c814c124a8f0573fdcd
Jarek Pierchala <jaroslaw.pierchala@hmcts.net>
2023-02-23 16:44:45 +0000

    changed the property for ignore plugin

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -21,7 +21,7 @@ module.exports = {
   ],
   plugins: [
     new webpack.IgnorePlugin({
-        RegExp: /^\.\/locale$/,
+        resourceRegExp: /^\.\/locale$/,
         contextRegExp: /moment$/,
     }),
     new CleanWebpackPlugin(),
