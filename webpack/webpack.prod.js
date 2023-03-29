42484041bb2b0e87c2ad894552dfc3234d237f1e
Mohammed Ali <mohammed.ali@hmcts.net>
2023-03-29 10:53:15 +0100

    SSCS-11479 - updated webpack.prod.js file

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,7 +1,7 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const TerserJSPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
