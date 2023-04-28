876da32d921c676a9e46611b7383f69fa6a7d901
addison-jephtah <108927648+addison-jephtah@users.noreply.github.com>
2023-04-28 15:46:17 +0100

    Revert "Yarn upgrade (#1379)"
    This reverts commit ab2ad6c5a0eae149ab177c45b935fd3fafdb6c1f.

--- test/e2e/codecept.conf.js
+++ test/e2e/codecept.conf.js
@@ -3,7 +3,6 @@ const config = require('config');
 const fileAcceptor = require('test/file_acceptor');
 const fs = require('fs');
 const testUser = require('../util/IdamUser');
-const urls = require('../../urls');
 
 const evidenceUploadEnabled = config.get('features.evidenceUpload.enabled');
 
@@ -28,7 +27,7 @@ exports.config = {
   },
   helpers: {
     Puppeteer: {
-      url: process.env.TEST_URL || `${urls.formDownload.benefitAppeal}`,
+      url: process.env.TEST_URL || config.get('e2e.frontendUrl'),
       waitForTimeout: parseInt(config.get('e2e.waitForTimeout')),
       waitForAction: parseInt(config.get('e2e.waitForAction')),
       waitForNavigation: 'load',
@@ -49,7 +48,7 @@ exports.config = {
     },
     MyHelper: {
       require: './helpers/helper.js',
-      url: process.env.TEST_URL || `${urls.formDownload.benefitAppeal}`
+      url: config.get('e2e.frontendUrl')
     },
     REST: {
     }
