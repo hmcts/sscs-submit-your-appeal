be1a9baffbedb3daa0f545c19dd6ec17b74c73f1
Jephtah Addison <jephtah.addison@hmcts.net>
2023-06-02 12:21:11 +0100

    demo fix

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,13 +2,13 @@ provider "azurerm" {
   features {}
 }
 
-data "azurerm_key_vault" "sscs_key_vault" {
-  name                = local.vaultName
-  resource_group_name = local.vaultName
+locals {
+  azureVaultName = "sscs-${var.env}"
 }
 
-locals {
-  vaultName = "${var.product}-${var.env}"
+data "azurerm_key_vault" "sscs_key_vault" {
+  name                = local.azureVaultName
+  resource_group_name = local.azureVaultName
 }
 
 data "azurerm_subnet" "core_infra_redis_subnet" {
@@ -18,10 +18,11 @@ data "azurerm_subnet" "core_infra_redis_subnet" {
 }
 
 module "redis-cache" {
-  source      = "git@github.com:hmcts/cnp-module-redis?ref=master"
-  product     = "${var.product}-redis"
-  location    = var.location
-  env         = var.env
+  source   = "git@github.com:hmcts/cnp-module-redis?ref=master"
+  product  = "${var.product}-redis"
+  location = var.location
+  env      = var.env
+
   subnetid    = data.azurerm_subnet.core_infra_redis_subnet.id
   common_tags = var.common_tags
 }
@@ -30,10 +31,20 @@ resource "azurerm_key_vault_secret" "redis_access_key" {
   name         = "${var.product}-redis-access-key"
   value        = module.redis-cache.access_key
   key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
+
+  content_type = "secret"
+  tags = merge(var.common_tags, {
+    "source" : "redis ${module.redis-cache.host_name}"
+  })
 }
 
 resource "azurerm_key_vault_secret" "redis_connection_string" {
   name         = "${var.product}-redis-connection-string"
   value        = "redis://ignore:${urlencode(module.redis-cache.access_key)}@${module.redis-cache.host_name}:${module.redis-cache.redis_port}?tls=true"
   key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
+
+  content_type = "secret"
+  tags = merge(var.common_tags, {
+    "source" : "redis ${module.redis-cache.host_name}"
+  })
 }
