07dc696559116af32f7481d0305c454ebbc9ecf1
paul-pearson <paul.pearson@version1.com>
2023-06-22 16:40:13 +0100

    Try and run without Redis
    Remove redis from healthcheck, and configure
    one-per-page to use in memory store, for testing
    purposes.

--- services/healthcheck.js
+++ services/healthcheck.js
@@ -1,6 +1,6 @@
 const healthcheck = require('@hmcts/nodejs-healthcheck');
 const os = require('os');
-const ioRedis = require('ioredis');
+// const ioRedis = require('ioredis');
 const config = require('config');
 
 const outputs = require('@hmcts/nodejs-healthcheck/healthcheck/outputs');
@@ -9,30 +9,30 @@ const { OK } = require('http-status-codes');
 const logger = require('logger');
 
 
-const ioRedisClient = ioRedis.createClient(
-  config.redis.url,
-  { enableOfflineQueue: false }
-);
+// const ioRedisClient = ioRedis.createClient(
+//   config.redis.url,
+//   { enableOfflineQueue: false }
+// );
 
-ioRedisClient.on('connect', () => {
-  logger.trace('Redis is connected :', 'health_check_error');
-});
+// ioRedisClient.on('connect', () => {
+//   logger.trace('Redis is connected :', 'health_check_error');
+// });
+//
+// ioRedisClient.on('ready', () => {
+//   logger.trace('Redis is ready:', 'health_check_error');
+// });
+//
+// ioRedisClient.on('reconnecting', () => {
+//   logger.trace('Redis is reconnecting: ', 'health_check_error');
+// });
 
-ioRedisClient.on('ready', () => {
-  logger.trace('Redis is ready:', 'health_check_error');
-});
-
-ioRedisClient.on('reconnecting', () => {
-  logger.trace('Redis is reconnecting: ', 'health_check_error');
-});
-
-ioRedisClient.on('error', error => {
-  logger.trace(`Health check failed on redis: ${error}`, 'health_check_error');
-});
-
-ioRedisClient.on('end', () => {
-  logger.trace('Redis connection is ended :', 'health_check_error');
-});
+// ioRedisClient.on('error', error => {
+//   logger.trace(`Health check failed on redis: ${error}`, 'health_check_error');
+// });
+//
+// ioRedisClient.on('end', () => {
+//   logger.trace('Redis connection is ended :', 'health_check_error');
+// });
 
 const healthOptions = message => {
   return {
@@ -50,21 +50,21 @@ const healthOptions = message => {
 const setup = app => {
   healthcheck.addTo(app, {
     checks: {
-      redis: healthcheck.raw(() => ioRedisClient.ping().then(_ => healthcheck.status(_ === 'PONG'))
-        .catch(error => {
-          logger.trace(`Health check failed on redis: ${error}`, 'health_check_error');
-          return outputs.down(error);
-        })),
+      // redis: healthcheck.raw(() => ioRedisClient.ping().then(_ => healthcheck.status(_ === 'PONG'))
+      //   .catch(error => {
+      //     logger.trace(`Health check failed on redis: ${error}`, 'health_check_error');
+      //     return outputs.down(error);
+      //   })),
       'submit-your-appeal-api': healthcheck.web(`${config.api.url}/health`,
         healthOptions('Health check failed on submit-your-appeal-api:')
       )
     },
     readinessChecks: {
-      redis: healthcheck.raw(() => ioRedisClient.ping().then(_ => healthcheck.status(_ === 'PONG'))
-        .catch(error => {
-          logger.trace(`Readiness check failed on redis: ${error}`, 'Readiness_check_error');
-          return outputs.down(error);
-        })),
+      // redis: healthcheck.raw(() => ioRedisClient.ping().then(_ => healthcheck.status(_ === 'PONG'))
+      //   .catch(error => {
+      //     logger.trace(`Readiness check failed on redis: ${error}`, 'Readiness_check_error');
+      //     return outputs.down(error);
+      //   })),
       'submit-your-appeal-api': healthcheck.web(`${config.api.url}/health/readiness`,
         healthOptions('Readiness check failed on submit-your-appeal-api:')
       )
