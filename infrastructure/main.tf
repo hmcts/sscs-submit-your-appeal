d623e9f75f15ebe9473acb027df84ad001a866fd
jarekPierchala <118526007+jarekPierchala@users.noreply.github.com>
2023-06-29 12:48:05 +0100

    Demo fix (#1389)
    * fix for pipeline failure by correcting Dockerfile
    
    * fix for pipeline failure
    
    * fix for Dockerfile to deploy to demo for testing
    
    * fix for Dockerfile to deploy to demo for testing
    
    * fix for Dockerfile to deploy to demo for testing
    
    * fix for Dockerfile to deploy to demo for testing
    
    * Bumping chart version/ fixing aliases
    
    * fix for Dockerfile to deploy to demo for testing
    
    * fix for Dockerfile to deploy to demo for testing
    
    * fix for Dockerfile to deploy to demo for testing
    
    * added logger for redis
    
    * added logger for redis
    
    * added logger for redis
    
    * added logger for redis
    
    * added logger for redis
    
    * added logger for redis
    
    * added logger for redis
    
    * added logger for redis
    
    * demo fix to ensure SYA is deployed
    
    * demo fix to ensure SYA is deployed
    
    * demo fix to ensure SYA is deployed
    
    * demo fix to ensure SYA is deployed
    
    * demo fix to ensure SYA is deployed
    
    * demo fix to ensure SYA is deployed
    
    * demo fix to ensure SYA is deployed
    
    * demo fix
    
    * demo fix
    
    * demo fix
    
    * demo fix
    
    * demo fix
    
    * demo fix
    
    * demo fix
    
    * demo fix
    
    * demo fix
    
    * demo fix
    
    * logs added
    
    * demo fix
    
    * demo fix
    
    * demo fix
    
    * demo fix
    
    * demo fix
    
    * demo fix
    
    * demo fix
    
    * demo fix
    
    * demo fix
    
    * demo fix
    
    * demo fix
    
    * demo fix
    
    * demo fix
    
    * demo fix
    
    * demo fix
    
    * redis update to 6
    
    * demo fix for logging redis connectivity
    
    * demo fix for logging redis connectivity
    
    * Update main.tf
    
    * Update main.tf
    
    * demo fix for logging redis connectivity
    
    * demo fix for logging redis connectivity
    
    * demo fix for logging redis connectivity
    
    * demo fix for logging redis connectivity
    
    * demo fix for logging redis connectivity
    
    * demo fix for logging redis connectivity
    
    * suppress CVE
    
    * suppress CVE
    
    * logging redis connection string
    
    * Update main.tf
    
    * Try and run without Redis
    
    Remove redis from healthcheck, and configure
    one-per-page to use in memory store, for testing
    purposes.
    
    * Update known issues
    
    * connecting to redis using redis library and connection string
    
    * Update healthcheck.js
    
    * Update main.tf
    
    * Use redis rather than InMemory data store
    
    * removing plain connection string
    
    * Use redis rather than InMemory data store
    
    * removing plain connection string
    
    * Correct redis connection config
    
    * Log redis url to check it is as expected
    
    * Add colon to connection string
    
    and remove /0
    
    * Remove "ignore" and tls from redis url
    
    * Correct redis url
    
    * updating redis connection string to include default
    
    * updating redis connection string to use rediss instead
    
    * Correct redis url after updating secret manually
    
    * Temporarily remove redis from healthcheck
    
    * removing logs from healthcheck.js, BenefitType.js and appConfigurations.js
    
    * removing debug comments from healthcheck.js
    
    * removing readiness check and tidying up config
    
    * removing ioredis dependency
    
    * enabling redis readinessCheck in healthcheck.js
    
    * temp redis instance for prod
    
    * suppress CVE as no patch is available
    
    * Update main.tf
    
    * disable smoke tests
    
    * disable smoke tests
    
    * disabel func tests
    
    ---------
    
    Co-authored-by: Jephtah Addison <jephtah.addison@hmcts.net>
    Co-authored-by: hmcts-jenkins-j-to-z <61242337+hmcts-jenkins-j-to-z[bot]@users.noreply.github.com>
    Co-authored-by: paul-pearson <paul.pearson@version1.com>

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -18,14 +18,16 @@ data "azurerm_subnet" "core_infra_redis_subnet" {
 }
 
 module "redis-cache" {
-  source        = "git@github.com:hmcts/cnp-module-redis?ref=master"
-  product       = var.product
-  location      = var.location
-  common_tags   = var.common_tags
-  env           = var.env
-  name          = "${var.product}-v6"
-  redis_version = "6"
-  subnetid      = data.azurerm_subnet.core_infra_redis_subnet.id
+  source                        = "git@github.com:hmcts/cnp-module-redis?ref=master"
+  product                       = var.product
+  location                      = var.location
+  common_tags                   = var.common_tags
+  env                           = var.env
+  name                          = "${var.product}-redis-v6"
+  redis_version                 = "6"
+  business_area                 = "cft"
+  public_network_access_enabled = false
+  private_endpoint_enabled      = true
 }
 
 resource "azurerm_key_vault_secret" "redis_access_key" {
