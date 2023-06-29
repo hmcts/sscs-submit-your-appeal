cc20b5abb7c2ddee79a9b7e7113aed3b0d10d102
jarekPierchala <jaroslaw.pierchala@hmcts.net>
2023-06-29 11:23:33 +0100

    disable smoke tests

--- bin/runsmoketest.sh
+++ bin/runsmoketest.sh
@@ -14,5 +14,5 @@ trap shutdownDocker INT TERM QUIT EXIT
 docker-compose --version
 
 
-docker-compose -f docker-compose.smoke-tests.yml run syatests
-docker-compose -f docker-compose.smoke-tests.yml down
+#docker-compose -f docker-compose.smoke-tests.yml run syatests
+#docker-compose -f docker-compose.smoke-tests.yml down
