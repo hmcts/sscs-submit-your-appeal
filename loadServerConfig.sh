ca3e498f22b34d6eb39ac7561bd1e3fb7ac55371
eliandro goncalves <eliandro.goncalves@version1.com>
2023-07-20 16:20:36 +0100

    fix docker build

--- loadServerConfig.sh
+++ loadServerConfig.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Set environment variable from Azure secret vault
 # Parameters: <Environment variable name> <Vault Name> <Secret Name>
@@ -8,10 +8,6 @@ loadSecret () {
 
 echo "Fetching secrets..."
 
-#loadSecret "DOCMOSIS_TEMPLATES_ENDPOINT_AUTH" "dg-docassembly-aat" "docmosis-templates-auth"
-#loadSecret "DOCMOSIS_ACCESS_KEY" "dg-docassembly-aat" "docmosis-access-key"
-#loadSecret "AZURE_SERVICE_BUS_CONNECTION_STRING" "sscs-aat" "sscs-servicebus-connection-string-tf"
-#loadSecret "LAUNCH_DARKLY_SDK_KEY" "wa-aat" "ld-secret"
 loadSecret "SERVER_KEY" "sscs-aat" "server-key"
 loadSecret "SERVER_CERTIFICATE" "sscs-aat" "server-certificate"
 
