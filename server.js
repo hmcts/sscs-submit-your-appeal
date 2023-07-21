d33a0373e76d84dc6212258525b64e9b2adf0272
eliandro goncalves <eliandro.goncalves@version1.com>
2023-07-21 11:41:10 +0100

    Add secrets from azure KV to app

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ if (process.env.NODE_ENV === 'development') {
     app.locals.webpackHash = stats.hash;
     https.createServer({
       key: config.serverKeysConfiguration.serverKey, // eslint-disable-line
-      cert: fs.readFileSync('keys/server.cert') // eslint-disable-line
+      cert: config.serverKeysConfiguration.serverCertificate // eslint-disable-line
     }, app).listen(config.node.port, () => {
       logger.trace(`SYA server listening on port: ${config.node.port}`, logPath);
     });
