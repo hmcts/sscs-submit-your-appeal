b2633d83c0bedfcff333cd46a2261b2117b8ec46
hashimalisolirius <HashimAli@HMCTS.NET>
2023-09-05 09:57:36 +0100

    SSCSSCI-9 Generate server key and certificate from mountSecrets.js

--- server.js
+++ server.js
@@ -8,10 +8,11 @@ const app = require('app.js');
 const logger = require('logger');
 const https = require('https');
 
-const fs = require('graceful-fs');
 const webpack = require('webpack');
 const webpackDevConfig = require('./webpack/webpack.dev.js');
 const webpackMiddleware = require('webpack-dev-middleware');
+const fs = require('fs');
+const mountSecrets = require('./services/mountSecrets');
 
 const logPath = 'server.js';
 
@@ -19,12 +20,12 @@ if (process.env.NODE_ENV === 'development') {
   const compiler = webpack(webpackDevConfig);
   const wp = webpackMiddleware(compiler, { publicPath: webpackDevConfig.output.publicPath });
   app.use(wp);
-
+  mountSecrets();
   wp.waitUntilValid(stats => {
     app.locals.webpackHash = stats.hash;
     https.createServer({
-      key: fs.readFileSync('keys/server.key'), // eslint-disable-line
-      cert: fs.readFileSync('keys/server.cert') // eslint-disable-line
+      key: fs.readFileSync('secrets/sscs-aat/server-key'), // eslint-disable-line
+      cert: fs.readFileSync('secrets/sscs-aat/server-certificate') // eslint-disable-line
     }, app).listen(config.node.port, () => {
       logger.trace(`SYA server listening on port: ${config.node.port}`, logPath);
     });
