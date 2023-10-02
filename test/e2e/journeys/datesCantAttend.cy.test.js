080ee3ab9caad735f826e53459419a9ddc57d45f
taleb <tech@benouaer.com>
2023-10-02 23:35:40 +0100

    SSCSCI-18: omit skipPcq step to fix e2e

--- test/e2e/journeys/datesCantAttend.cy.test.js
+++ test/e2e/journeys/datesCantAttend.cy.test.js
@@ -47,7 +47,7 @@ Scenario(`${language.toUpperCase()} - Provides a date when they cannot attend th
   I.enterDetailsFromNoRepresentativeToNoUploadingEvidence(commonContent);
   await I.enterDetailsFromAttendingTheHearingToEnd(commonContent, language, randomWeekDayIn5Weeks);
   I.forceClick(commonContent.continue);
-  I.skipPcqCY();
+  // I.skipPcqCY();
   I.wait(1);
   console.log(`language assigned is ${moment.locale()}`);
   console.log(`Generated date is ############# ${randomWeekDayIn5Weeks}`);
