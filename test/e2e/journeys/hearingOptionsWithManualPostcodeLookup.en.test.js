c2f0fa883cdeb6f01891a7db862f573e313035b8
taleb <tech@benouaer.com>
2023-10-26 18:25:37 +0100

    SSCSCI-9: skip PCQ- welsh if it's down

--- test/e2e/journeys/hearingOptionsWithManualPostcodeLookup.en.test.js
+++ test/e2e/journeys/hearingOptionsWithManualPostcodeLookup.en.test.js
@@ -5,7 +5,7 @@ const commonContent = require('commonContent')[language];
 const paths = require('paths');
 const testData = require(`test/e2e/data.${language}`);
 
-Feature(`${language.toUpperCase()} - Hearing options test for type Telephone @pcqTest @functional`);
+Feature(`${language.toUpperCase()} - Hearing options test for type Telephone @functional`);
 
 Before(({ I }) => {
   I.createTheSession(language);
