d7689839df16a824a93cac75f30d1e17e0e887a0
taleb <tech@benouaer.com>
2023-11-12 21:41:13 +0000

    SSCSCI-9: go back to original secret mount code

--- server.js
+++ server.js
@@ -1,21 +1,40 @@
 const config = require('@hmcts/properties-volume').addTo(require('config'));
 const setupSecrets = require('services/setupSecrets');
-const startLocalDevServer = require('services/start-local-dev-server');
 
 // Setup secrets before loading the app
 setupSecrets();
 
 const app = require('app.js');
 const logger = require('logger');
+const https = require('https');
+
+const webpack = require('webpack');
+const webpackDevConfig = require('./webpack/webpack.dev.js');
+const webpackMiddleware = require('webpack-dev-middleware');
+const mountSecrets = require('./services/start-local-dev-server');
 
 const logPath = 'server.js';
 
 if (process.env.NODE_ENV === 'development') {
-  startLocalDevServer()
-    .then(() => logger.trace('SYA local development server started'));
+  const compiler = webpack(webpackDevConfig);
+  const wp = webpackMiddleware(compiler, { publicPath: webpackDevConfig.output.publicPath });
+  app.use(wp);
+  mountSecrets().then(res => {
+    wp.waitUntilValid(stats => {
+      app.locals.webpackHash = stats.hash;
+      https.createServer({
+        key: res.serverKey,
+        cert: res.serverCertificate
+      }, app).listen(config.node.port, () => {
+        logger.trace(`SYA server listening on port: ${config.node.port}`, logPath);
+      });
+    });
+  });
 } else {
   app
     .listen(config.node.port,
-      () => logger.trace(`SYA server listening on port: ${config.node.port}`, logPath));
+      () => {
+        logger.trace(`SYA server listening on port: ${config.node.port}`, logPath);
+      });
 }
 app.timeout = 240000;
--- services/start-local-dev-server.js
+++ services/start-local-dev-server.js
@@ -1,12 +1,5 @@
 const { AzureCliCredential } = require('@azure/identity');
 const { SecretClient } = require('@azure/keyvault-secrets');
-const app = require('../app');
-const config = require('config');
-const https = require('https');
-const logger = require('../logger');
-const webpack = require('webpack');
-const webpackDevConfig = require('../webpack/webpack.dev');
-const webpackMiddleware = require('webpack-dev-middleware');
 
 async function mount(vaultName, secret) {
   try {
@@ -27,26 +20,10 @@ async function mount(vaultName, secret) {
   }
 }
 
-async function fetchSecrets() {
+async function run() {
   const serverKey = await mount('sscs-aat', 'server-key');
   const serverCertificate = await mount('sscs-aat', 'server-certificate');
   return { serverKey, serverCertificate };
 }
 
-const startLocalDevServer = async() => {
-  const compiler = webpack(webpackDevConfig);
-  const wp = webpackMiddleware(compiler, { publicPath: webpackDevConfig.output.publicPath });
-  app.use(wp);
-  const { serverKey, serverCertificate } = await fetchSecrets();
-  wp.waitUntilValid(stats => {
-    app.locals.webpackHash = stats.hash;
-    https.createServer({
-      key: serverKey,
-      cert: serverCertificate
-    }, app).listen(config.node.port, () => {
-      logger.trace(`SYA server listening on port: ${config.node.port}`);
-    });
-  });
-};
-
-module.exports = startLocalDevServer;
+module.exports = run;
