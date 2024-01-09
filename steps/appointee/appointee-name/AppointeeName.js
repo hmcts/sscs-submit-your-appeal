2eee3d67ba4a759fb62955d1afda3bc52e5f571e
NHAL22 <113360585+NHAL22@users.noreply.github.com>
2024-01-09 09:45:54 +0000

    Latest: SSCS-10112 - Judicial- CCD- Non-standard captialised names are format… (#1429)

--- steps/appointee/appointee-name/AppointeeName.js
+++ steps/appointee/appointee-name/AppointeeName.js
@@ -8,7 +8,6 @@ const Joi = require('joi');
 const paths = require('paths');
 const titlesList = require('../../../utils/titlesList');
 const { decode } = require('utils/stringUtils');
-const parseFullName = require('parse-full-name').parseFullName;
 
 class AppointeeName extends SaveToDraftStore {
   static get path() {
@@ -51,12 +50,11 @@ class AppointeeName extends SaveToDraftStore {
     const title = this.fields.title.value;
     const first = this.fields.firstName.value;
     const last = this.fields.lastName.value;
-    const fullName = parseFullName(`${first} ${last}`, 'all', 1, 0, 0);
     return [
       answer(this, {
         question: this.content.cya.appointeeName.question,
         section: sections.appointeeDetails,
-        answer: decode(`${title} ${fullName.first} ${fullName.last}`)
+        answer: decode(`${title} ${first} ${last}`)
       })
     ];
   }
@@ -65,12 +63,11 @@ class AppointeeName extends SaveToDraftStore {
     const title = this.fields.title.value;
     const first = this.fields.firstName.value;
     const last = this.fields.lastName.value;
-    const fullName = parseFullName(`${first} ${last}`, 'all', 1, 0, 0);
     return {
       appointee: {
         title: decode(title),
-        firstName: decode(fullName.first),
-        lastName: decode(fullName.last)
+        firstName: decode(first),
+        lastName: decode(last)
       }
     };
   }
