5bf6af501a15e059c3e64a586a88c0e248e780f4
Gemma Talbot <gemma.talbot@solirius.com>
2024-03-11 11:09:37 +0000

    revert en sign in test

--- test/e2e/journeys/citizenSignIn.en.test.js
+++ test/e2e/journeys/citizenSignIn.en.test.js
@@ -1,29 +1,23 @@
 /* eslint-disable no-process-env */
-/* eslint init-declarations: ["error", "never"]*/
 
 const language = 'en';
 const commonContent = require('commonContent')[language];
 const moment = require('moment');
 const testData = require(`test/e2e/data.${language}`);
-const testUser = require('../../util/IdamUser');
 
 Feature(`${language.toUpperCase()} - Citizen, Sign in scenarios for SYA`);
 
-let userEmail;
-
 Before(({ I }) => {
   I.createTheSession(language);
-  userEmail = testUser.createUser();
 });
 
 After(({ I }) => {
   I.endTheSession();
-  testUser.deleteUser(userEmail);
 });
 
 Scenario(`${language.toUpperCase()} - Sign in as a new user and verify draft appeals page @functional`, async({ I }) => {
   await moment().locale(language);
-  await I.enterDetailsFromStartToDraftAppeals(commonContent, language, userEmail);
+  await I.enterDetailsFromStartToDraftAppeals(commonContent, language, process.env.USEREMAIL_1);
   await I.enterAppellantContactDetailsWithMobileAndContinueAfterSignIn(commonContent, language, '07411222222');
   await I.checkOptionAndContinueAfterSignIn(commonContent, '#doYouWantTextMsgReminders-no');
   await I.checkOptionAndContinueAfterSignIn(commonContent, '#hasRepresentative-no');
