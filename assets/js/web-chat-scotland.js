238e486e1c3ad79fe65768027d971cf57f938ebc
hashimalisolirius <140517911+hashimalisolirius@users.noreply.github.com>
2024-07-18 11:07:10 +0100

    update webchat channel (#1495)

--- assets/js/web-chat-scotland.js
+++ assets/js/web-chat-scotland.js
@@ -3,7 +3,7 @@
             window.__8x8Chat =
                 {uuid: "script_1553624711656480310cfd53.70557078",
                     tenant: "aG1jdHN4MTAx",
-                    channel: "Scotland_Tribunal_SSCS_Webchat",
+                    channel: "Scotland_Tribunal_SSCS_Webchat LIVE",
                     channelUuid: "aUEWe4EVSOuCIvsuANo64Q",
                     domain: "https://vcc-eu4.8x8.com",
                     path: "/.",
