b3db91fca4ad4197aa6015338b03a51d1daae620
Katalin Kovacs <katalin.kovacs@solirius.com>
2024-08-23 22:09:39 +0100

    Removed unneeded lines

--- webpack/app.js
+++ webpack/app.js
@@ -6,7 +6,6 @@ const sass = path.resolve(__dirname, '../assets/scss');
 const images = path.resolve(__dirname, '../assets/images');
 const locales = path.resolve(__dirname, '../assets/locale');
 const views = path.resolve(__dirname, '../views/components');
-const webpackTemplates = path.resolve(__dirname, '../views/webpack');
 
 const copyImages = new CopyWebpackPlugin({
   patterns: [{ from: images, to: '../dist/images' }]
@@ -20,11 +19,7 @@ const copyViews = new CopyWebpackPlugin({
   patterns: [{ from: views, to: '../dist/nunjucks/components' }]
 });
 
-const copyWebpackTemplates = new CopyWebpackPlugin({
-  patterns: [{ from: webpackTemplates, to: '../dist/nunjucks/webpack' }]
-});
-
 module.exports = {
   paths: { sass },
-  plugins: [copyImages, copyLocales, copyViews, copyWebpackTemplates]
+  plugins: [copyImages, copyLocales, copyViews]
 };
