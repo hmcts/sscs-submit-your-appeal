7fd3170b5ef45ee146890e0e6fe8ea9fec4ba2d8
nilay <nilay.patel@hmcts.net>
2024-09-19 11:35:03 +0100

    test fix

--- test/e2e/page-objects/start/benefit-type.js
+++ test/e2e/page-objects/start/benefit-type.js
@@ -6,8 +6,8 @@ async function enterBenefitTypeAndContinue(page, language, commonContent, type) 
   const benefitContent = language === 'en' ? benefitContentEn : benefitContentCy;
 
   await expect(page.getByText(benefitContent.title).first()).toBeVisible({ timeout: 45000 });
-  await page.fill('#benefitType', type);
-  await page.click('#benefitType__option--0');
+  await page.fill('#benefitType', type.split(' ')[0]);
+  await page.getByText(type).first().click()
   await page.getByText(commonContent.continue).first().click();
 }
 
