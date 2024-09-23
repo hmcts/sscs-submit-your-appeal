0fe4df157235c45fe16751f172ea4782394ac9c9
nilay <nilay.patel@hmcts.net>
2024-09-23 15:41:28 +0100

    addressing code review

--- utils/benefitTypeUtils.js
+++ utils/benefitTypeUtils.js
@@ -1,8 +1,8 @@
 const benefitTypes = require('steps/start/benefit-type/types');
+const _ = require('lodash');
 
 const isIba = req =>
-  (Object.hasOwn(req.session || {}, 'BenefitType') && req.session.BenefitType.benefitType === benefitTypes.infectedBloodAppeal) ||
-  (req.hostname && req.hostname.includes('iba-')) ||
-  (Object.hasOwn(req.query || {}, 'forceIba'));
+  req.session?.BenefitType?.benefitType === benefitTypes.infectedBloodAppeal ||
+  req.hostname?.includes('iba-') || !_.isUndefined(req.query?.forceIba);
 
 module.exports = { isIba };
