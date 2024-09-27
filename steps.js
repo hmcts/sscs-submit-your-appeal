17709976e15200e5916912ffe2bd029aa06e6ddf
Timi Olubusi <timiolubusi@icloud.com>
2024-09-27 14:46:04 +0100

    Edit the existing files for new IBCA Ref screen
    Editing the Appellant DOB screen to go to the IBCA ref is its IBA case
    
    Added new screen to path and steps

--- steps.js
+++ steps.js
@@ -34,6 +34,7 @@ const AppellantContactDetails = require('steps/identity/appellant-contact-detail
 const AppellantName = require('steps/identity/appellant-name/AppellantName');
 const AppellantDOB = require('steps/identity/appellant-dob/AppellantDOB');
 const AppellantNINO = require('steps/identity/appellant-nino/AppellantNINO');
+const AppellantIBCARef = require('steps/identity/appellant-ibca-ref/AppellantIBCARef');
 const AppealFormDownload = require('steps/appeal-form-download/AppealFormDownload');
 const AppointeeName = require('steps/appointee/appointee-name/AppointeeName');
 const AppointeeDOB = require('steps/appointee/appointee-dob/AppointeeDOB');
@@ -120,6 +121,7 @@ const identity = [
   AppellantDOB,
   AppellantName,
   AppellantNINO,
+  AppellantIBCARef,
   Appointee,
   AppealFormDownload
 ];
