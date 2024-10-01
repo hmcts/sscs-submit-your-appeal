c84a07070e4cf642b2be04ed80d7aee0371f81ab
Timi Olubusi <timiolubusi@icloud.com>
2024-10-01 16:45:33 +0100

    Refactoring code and cleaning up test

--- test/unit/steps/identity/appellant-ibca-ref/appellantIBCARef.test.js
+++ test/unit/steps/identity/appellant-ibca-ref/appellantIBCARef.test.js
@@ -80,7 +80,7 @@ describe('AppellantIBCARef.js', () => {
     });
 
     it('should return correct is appointee title', () => {
-        appellantIBCARef.journey.req.session.Appointee.isAppointee = 'yes';
+      appellantIBCARef.journey.req.session.Appointee.isAppointee = 'yes';
       expect(appellantIBCARef.title).to.equal(IS_APPOINTEE_TITLE);
     });
 
@@ -89,7 +89,7 @@ describe('AppellantIBCARef.js', () => {
     });
 
     it('should return correct is appointee subtitle', () => {
-        appellantIBCARef.journey.req.session.Appointee.isAppointee = 'yes';
+      appellantIBCARef.journey.req.session.Appointee.isAppointee = 'yes';
       expect(appellantIBCARef.subtitle).to.equal(IS_APPOINTEE_SUBTITLE);
     });
   });
