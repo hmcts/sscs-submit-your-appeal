bba8ca54c1a32ac8909efc38d740108ea0eaa836
nilay <nilay.patel@hmcts.net>
2024-10-07 12:27:37 +0100

    changing iba checker to allow for new prod domain

--- utils/benefitTypeUtils.js
+++ utils/benefitTypeUtils.js
@@ -3,7 +3,7 @@ const benefitTypes = require('steps/start/benefit-type/types');
 const isIba = req =>
   req.session?.BenefitType?.benefitType === benefitTypes.infectedBloodAppeal ||
   req.hostname?.includes('iba') ||
-  req.hostname?.includes('infected-blood-appeal') ||
+  req.hostname?.includes('infected-blood') ||
   process.env.IS_IBA === 'true';
 
 module.exports = { isIba };
