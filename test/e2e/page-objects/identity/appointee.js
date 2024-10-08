7b37657643910055faca4f4fc7745284cb892833
Timi Olubusi <timiolubusi@icloud.com>
2024-10-08 17:14:19 +0100

    Fix for failing smoke test that checks content
    Added a dynamic variable for IBA specific wording

--- test/e2e/page-objects/identity/appointee.js
+++ test/e2e/page-objects/identity/appointee.js
@@ -5,7 +5,7 @@ function selectAreYouAnAppointeeAndContinue(language, commonContent, option) {
   const I = this;
   const appointeeContent = language === 'en' ? appointeeContentEn : appointeeContentCy;
 
-  I.waitForText(appointeeContent.fields.isAppointee.yes);
+  I.waitForText(appointeeContent.fields.isAppointee.yes.replace('{{appointedBy}}', 'DWP'));
   I.checkOption(option);
   I.click(commonContent.continue);
 }
