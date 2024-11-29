3c13fac54e8b342932754ecf4c20abe1377fc9cd
gokul-sol <gokul.sridharan@HMCTS.NET>
2024-11-29 11:37:47 +0000

    remove comments

--- test/util/randomData.js
+++ test/util/randomData.js
@@ -3,16 +3,12 @@ const generateValidNINumber = () => {
   // but for simplicity, we will just allow any two letters)
   const firstLetters = ['AB'];
 
-  // Pick a random starting combination (you could add more logic here to restrict starting letters)
   const firstPart = firstLetters[Math.floor(Math.random() * firstLetters.length)];
 
-  // Generate six random digits and ensure it has 6 digits (with leading zeros if necessary)
   const digits = Math.floor(Math.random() * 1000000).toString().padStart(6, '0');
 
-  // List of valid ending letters (excluding 'O', 'I', 'Q')
   const validLastLetters = ['A', 'B', 'C', 'D'];
-
-  // Pick a random ending letter
+  
   const lastLetter = validLastLetters[Math.floor(Math.random() * validLastLetters.length)];
 
   // Combine parts into a valid NI number
