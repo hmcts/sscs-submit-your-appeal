d26d93c004df0d355770bc0d88ff37e411c658b8
gokul <69240498+gokul-sol@users.noreply.github.com>
2024-12-02 11:42:43 +0000

    Fix nino test data issue for UI tests (#1569)
    * add random ni generator for tests
    
    * remove comments
    
    * remove nino check on confirmation
    
    ---------
    
    Co-authored-by: gokul-sol <gokul.sridharan@HMCTS.NET>

---
+++ test/util/randomData.js
@@ -0,0 +1,18 @@
+const generateValidNINumber = () => {
+  // List of valid first two letter combinations (the starting letters follow some patterns,
+  // but for simplicity, we will just allow any two letters)
+  const firstLetters = ['AB'];
+
+  const firstPart = firstLetters[Math.floor(Math.random() * firstLetters.length)];
+
+  const digits = Math.floor(Math.random() * 1000000).toString().padStart(6, '0');
+
+  const validLastLetters = ['A', 'B', 'C', 'D'];
+  
+  const lastLetter = validLastLetters[Math.floor(Math.random() * validLastLetters.length)];
+
+  // Combine parts into a valid NI number
+  return `${firstPart}${digits}${lastLetter}`;
+};
+
+module.exports = { generateValidNINumber };
