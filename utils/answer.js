e4a7db4a06978fffe29ee86bcedcd012e6fa0b35
Timi Olubusi <timiolubusi@icloud.com>
2025-01-07 17:19:55 +0000

    Updating the answers to also have Welsh option available
    This will allow to a default to English so normal implementation works but also have Welsh option and without major change on implementation

--- utils/answer.js
+++ utils/answer.js
@@ -1,8 +1,20 @@
-const answer = {
-  YES: 'yes',
-  NO: 'no',
-  NOT_PROVIDED: 'Not provided',
-  NOT_REQUIRED: 'Not required'
+const translationAnswers = {
+  en: {
+    YES: 'yes',
+    NO: 'no',
+    NOT_PROVIDED: 'Not provided',
+    NOT_REQUIRED: 'Not required'
+  },
+  cy: {
+    YES: 'Ydw',
+    NO: 'Nac ydw',
+    NOT_PROVIDED: 'Heb ei ddarparu',
+    NOT_REQUIRED: 'Dim ei angen'
+  }
 };
+
+const answer = translationAnswers.en;
+answer.en = translationAnswers.en;
+answer.cy = translationAnswers.cy;
 
 module.exports = answer;
