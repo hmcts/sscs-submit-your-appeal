605b700a8c2c01d9f59e5b952616ba30b2766067
Timi Olubusi <timiolubusi@icloud.com>
2025-01-31 15:38:11 +0000

    Fixing testds again

--- test/e2e/e2e-sya/e2e.cy.test.js
+++ test/e2e/e2e-sya/e2e.cy.test.js
@@ -5,90 +5,35 @@ const language = 'cy';
 const content = require('commonContent');
 const testData = require(`test/e2e/data.${language}`);
 const { createTheSession } = require('../page-objects/session/createSession');
-const {
-  enterDetailsFromStartToNINO,
-  enterDetailsFromNoRepresentativeToNoUploadingEvidence,
-  checkYourAppealToConfirmationPage
-} = require('../page-objects/cya/checkYourAppeal');
-const {
-  enterAppellantContactDetailsWithMobileAndContinue
-} = require('../page-objects/identity/appellantDetails');
+const { enterDetailsFromStartToNINO, enterDetailsFromNoRepresentativeToNoUploadingEvidence,
+  checkYourAppealToConfirmationPage } = require('../page-objects/cya/checkYourAppeal');
+const { enterAppellantContactDetailsWithMobileAndContinue } = require('../page-objects/identity/appellantDetails');
 const { checkOptionAndContinue } = require('../page-objects/controls/option');
-const {
-  readSMSConfirmationAndContinue
-} = require('../page-objects/sms-notify/smsConfirmation');
-const {
-  enterDoYouWantToAttendTheHearing
-} = require('../page-objects/hearing/theHearing');
-const {
-  selectTelephoneHearingOptionsAndContinue
-} = require('../page-objects/hearing/options');
-const {
-  selectDoYouNeedSupportAndContinue
-} = require('../page-objects/hearing/support');
-const {
-  selectHearingAvailabilityAndContinue
-} = require('../page-objects/hearing/availability');
+const { readSMSConfirmationAndContinue } = require('../page-objects/sms-notify/smsConfirmation');
+const { enterDoYouWantToAttendTheHearing } = require('../page-objects/hearing/theHearing');
+const { selectTelephoneHearingOptionsAndContinue } = require('../page-objects/hearing/options');
+const { selectDoYouNeedSupportAndContinue } = require('../page-objects/hearing/support');
+const { selectHearingAvailabilityAndContinue } = require('../page-objects/hearing/availability');
 const { skipPcqCY } = require('../page-objects/pcq/pcq');
 const { endTheSession } = require('../page-objects/session/endSession');
 
 test.describe(`${language.toUpperCase()} - PIP E2E SYA - Full Journey`, () => {
-  test(
-    `${language.toUpperCase()} - PIP E2E SYA Journey`,
-    { tag: ['@functional', '@e2e'] },
-    async({ page }) => {
-      const commonContent = content[language];
+  test(`${language.toUpperCase()} - PIP E2E SYA Journey`, { tag: ['@functional', '@e2e'] }, async({ page }) => {
+    const commonContent = content[language];
 
-      await createTheSession(page, language);
-      await enterDetailsFromStartToNINO(page, commonContent, language);
-      await enterAppellantContactDetailsWithMobileAndContinue(
-        page,
-        commonContent,
-        language,
-        '07411222222'
-      );
-      await checkOptionAndContinue(
-        page,
-        commonContent,
-        '#doYouWantTextMsgReminders-yes'
-      );
-      await checkOptionAndContinue(page, commonContent, '#useSameNumber-yes');
-      await readSMSConfirmationAndContinue(page, commonContent);
-      await enterDetailsFromNoRepresentativeToNoUploadingEvidence(
-        page,
-        language,
-        commonContent
-      );
-      await enterDoYouWantToAttendTheHearing(
-        page,
-        language,
-        commonContent,
-        '#attendHearing-yes'
-      );
-      await selectTelephoneHearingOptionsAndContinue(
-        page,
-        language,
-        commonContent
-      );
-      await selectDoYouNeedSupportAndContinue(
-        page,
-        language,
-        commonContent,
-        '#arrangements-no'
-      );
-      await selectHearingAvailabilityAndContinue(
-        page,
-        language,
-        commonContent,
-        '#scheduleHearing-no'
-      );
-      await skipPcqCY(page);
-      await checkYourAppealToConfirmationPage(
-        page,
-        language,
-        testData.signAndSubmit.signer
-      );
-      await endTheSession(page);
-    }
-  );
+    await createTheSession(page, language);
+    await enterDetailsFromStartToNINO(page, commonContent, language);
+    await enterAppellantContactDetailsWithMobileAndContinue(page, commonContent, language, '07411222222');
+    await checkOptionAndContinue(page, commonContent, '#doYouWantTextMsgReminders');
+    await checkOptionAndContinue(page, commonContent, '#useSameNumber-yes');
+    await readSMSConfirmationAndContinue(page, commonContent);
+    await enterDetailsFromNoRepresentativeToNoUploadingEvidence(page, language, commonContent);
+    await enterDoYouWantToAttendTheHearing(page, language, commonContent, '#attendHearing');
+    await selectTelephoneHearingOptionsAndContinue(page, language, commonContent);
+    await selectDoYouNeedSupportAndContinue(page, language, commonContent, '#arrangements-2');
+    await selectHearingAvailabilityAndContinue(page, language, commonContent, '#scheduleHearing-2');
+    await skipPcqCY(page);
+    await checkYourAppealToConfirmationPage(page, language, testData.signAndSubmit.signer);
+    await endTheSession(page);
+  });
 });
--- test/e2e/journeys/noMRN.en.test.js
+++ test/e2e/journeys/noMRN.en.test.js
@@ -2,50 +2,34 @@ const { test, expect } = require('@playwright/test');
 
 const language = 'en';
 const commonContent = require('commonContent')[language];
-const contactDWPContent = require(
-  `steps/compliance/contact-dwp/content.${language}`
-);
+const contactDWPContent = require(`steps/compliance/contact-dwp/content.${language}`);
 
 const DateUtils = require('utils/DateUtils');
 const testData = require('test/e2e/data.en');
 const moment = require('moment');
 const config = require('config');
-const { checkOptionAndContinue } = require('../page-objects/controls/option');
 const {
-  selectHaveYouGotAMRNAndContinue
-} = require('../page-objects/compliance/haveAMRN');
-const {
-  selectIfYouWantToCreateAccount
-} = require('../page-objects/idam/createAccount');
-const {
-  continueFromIndependance
-} = require('../page-objects/start/independence');
-const {
-  enterPostcodeAndContinue
-} = require('../page-objects/start/postcode-checker');
-const {
-  enterBenefitTypeAndContinue
-} = require('../page-objects/start/benefit-type');
+  checkOptionAndContinue
+} = require('../page-objects/controls/option');
+const { selectHaveYouGotAMRNAndContinue } = require('../page-objects/compliance/haveAMRN');
+const { selectIfYouWantToCreateAccount } = require('../page-objects/idam/createAccount');
+const { continueFromIndependance } = require('../page-objects/start/independence');
+const { enterPostcodeAndContinue } = require('../page-objects/start/postcode-checker');
+const { enterBenefitTypeAndContinue } = require('../page-objects/start/benefit-type');
 const {
   confirmDetailsArePresent,
   enterDetailsFromAttendingTheHearingDatePickerToEnd,
   enterDetailsFromNoRepresentativeToUploadingEvidence
 } = require('../page-objects/cya/checkYourAppeal');
-const {
-  selectDoYouWantToReceiveTextMessageReminders
-} = require('../page-objects/sms-notify/textReminders');
+const { selectDoYouWantToReceiveTextMessageReminders } = require('../page-objects/sms-notify/textReminders');
 const {
   enterAppellantContactDetailsAndContinue,
   enterAppellantNINOAndContinue,
   enterAppellantDOBAndContinue,
   enterAppellantNameAndContinue
 } = require('../page-objects/identity/appellantDetails');
-const {
-  continueFromStillCanAppeal
-} = require('../page-objects/compliance/stillCanAppeal');
-const {
-  enterReasonForNoMRNAndContinue
-} = require('../page-objects/compliance/noMRN');
+const { continueFromStillCanAppeal } = require('../page-objects/compliance/stillCanAppeal');
+const { enterReasonForNoMRNAndContinue } = require('../page-objects/compliance/noMRN');
 const { endTheSession } = require('../page-objects/session/endSession');
 const { createTheSession } = require('../page-objects/session/createSession');
 
@@ -53,139 +37,52 @@ const allowSaveAndReturnEnabled = config.get('features.allowSaveAndReturn.enable
 
 const appellant = testData.appellant;
 
-test.describe(
-  `${language.toUpperCase()} - Appellant does not have a MRN`,
-  { tag: '@batch-04' },
-  () => {
-    test.beforeEach('Create session and user', async({ page }) => {
-      await createTheSession(page, language);
-    });
+test.describe(`${language.toUpperCase()} - Appellant does not have a MRN`, { tag: '@batch-04' }, () => {
+  test.beforeEach('Create session and user', async({ page }) => {
+    await createTheSession(page, language);
+  });
 
-    test.afterEach('End session and delete user', async({ page }) => {
-      await endTheSession(page);
-    });
+  test.afterEach('End session and delete user', async({ page }) => {
+    await endTheSession(page);
+  });
 
-    test(`${language.toUpperCase()} - Appellant has contacted DWP`, async({
-      page
-    }) => {
-      const randomWeekDay = DateUtils.getDateInMilliseconds(
-        DateUtils.getRandomWeekDayFromDate(
-          moment().utc().startOf('day').add(5, 'weeks')
-        )
-      );
+  test(`${language.toUpperCase()} - Appellant has contacted DWP`, async({ page }) => {
+    const randomWeekDay = DateUtils.getDateInMilliseconds(
+      DateUtils.getRandomWeekDayFromDate(moment().utc().startOf('day').add(5, 'weeks'))
+    );
 
-      const hasMRN = false;
+    const hasMRN = false;
 
-      await enterBenefitTypeAndContinue(
-        page,
-        language,
-        commonContent,
-        testData.benefitType.code
-      );
-      await enterPostcodeAndContinue(
-        page,
-        language,
-        commonContent,
-        appellant.contactDetails.postCode
-      );
-      await checkOptionAndContinue(page, commonContent, '#isAppointee-no');
-      await continueFromIndependance(page, commonContent);
-      await checkOptionAndContinue(page, commonContent, '#haveAMRN-no');
-      await checkOptionAndContinue(
-        page,
-        commonContent,
-        '#haveContactedDWP-yes'
-      );
-      await enterReasonForNoMRNAndContinue(
-        page,
-        language,
-        commonContent,
-        testData.mrn.reasonForNoMRN
-      );
-      await continueFromStillCanAppeal(page, language);
-      await enterAppellantNameAndContinue(
-        page,
-        language,
-        commonContent,
-        appellant.title,
-        appellant.firstName,
-        appellant.lastName
-      );
-      await enterAppellantDOBAndContinue(
-        page,
-        language,
-        commonContent,
-        appellant.dob.day,
-        appellant.dob.month,
-        appellant.dob.year
-      );
-      await enterAppellantNINOAndContinue(
-        page,
-        language,
-        commonContent,
-        appellant.nino
-      );
-      await enterAppellantContactDetailsAndContinue(
-        page,
-        commonContent,
-        language
-      );
-      await selectDoYouWantToReceiveTextMessageReminders(
-        page,
-        commonContent,
-        '#doYouWantTextMsgReminders-no'
-      );
-      await enterDetailsFromNoRepresentativeToUploadingEvidence(
-        page,
-        language,
-        commonContent
-      );
-      await enterDetailsFromAttendingTheHearingDatePickerToEnd(
-        page,
-        commonContent,
-        language,
-        randomWeekDay
-      );
-      await confirmDetailsArePresent(page, language, hasMRN);
-    });
+    await enterBenefitTypeAndContinue(page, language, commonContent, testData.benefitType.code);
+    await enterPostcodeAndContinue(page, language, commonContent, appellant.contactDetails.postCode);
+    await checkOptionAndContinue(page, commonContent, '#isAppointee-no');
+    await continueFromIndependance(page, commonContent);
+    await checkOptionAndContinue(page, commonContent, '#haveAMRN-2');
+    await checkOptionAndContinue(page, commonContent, '#haveContactedDWP-yes');
+    await enterReasonForNoMRNAndContinue(page, language, commonContent, testData.mrn.reasonForNoMRN);
+    await continueFromStillCanAppeal(page, language);
+    await enterAppellantNameAndContinue(page, language, commonContent, appellant.title, appellant.firstName, appellant.lastName);
+    await enterAppellantDOBAndContinue(page, language, commonContent, appellant.dob.day, appellant.dob.month, appellant.dob.year);
+    await enterAppellantNINOAndContinue(page, language, commonContent, appellant.nino);
+    await enterAppellantContactDetailsAndContinue(page, commonContent, language);
+    await selectDoYouWantToReceiveTextMessageReminders(page, commonContent, '#doYouWantTextMsgReminders-2');
+    await enterDetailsFromNoRepresentativeToUploadingEvidence(page, language, commonContent);
+    await enterDetailsFromAttendingTheHearingDatePickerToEnd(page, commonContent, language, randomWeekDay);
+    await confirmDetailsArePresent(page, language, hasMRN);
+  });
 
-    test(`${language.toUpperCase()} - Appellant has not contacted DWP and exits the service`, async({
-      page
-    }) => {
-      await enterBenefitTypeAndContinue(
-        page,
-        language,
-        commonContent,
-        testData.benefitType.code
-      );
-      await enterPostcodeAndContinue(
-        page,
-        language,
-        commonContent,
-        appellant.contactDetails.postCode
-      );
-      await checkOptionAndContinue(page, commonContent, '#isAppointee-no');
-      await continueFromIndependance(page, commonContent);
-      if (allowSaveAndReturnEnabled) {
-        await selectIfYouWantToCreateAccount(
-          page,
-          language,
-          commonContent,
-          '#createAccount-no'
-        );
-      }
-      await selectHaveYouGotAMRNAndContinue(
-        page,
-        language,
-        commonContent,
-        '#haveAMRN-no'
-      );
-      await checkOptionAndContinue(page, commonContent, '#haveContactedDWP-no');
-      await expect(
-        page.getByText(contactDWPContent.title).first()
-      ).toBeVisible();
-      await page.getByText(contactDWPContent.govuk).first().click();
-      await page.waitForURL('https://www.gov.uk/');
-    });
-  }
-);
+  test(`${language.toUpperCase()} - Appellant has not contacted DWP and exits the service`, async({ page }) => {
+    await enterBenefitTypeAndContinue(page, language, commonContent, testData.benefitType.code);
+    await enterPostcodeAndContinue(page, language, commonContent, appellant.contactDetails.postCode);
+    await checkOptionAndContinue(page, commonContent, '#isAppointee-no');
+    await continueFromIndependance(page, commonContent);
+    if (allowSaveAndReturnEnabled) {
+      await selectIfYouWantToCreateAccount(page, language, commonContent, '#createAccount-2');
+    }
+    await selectHaveYouGotAMRNAndContinue(page, language, commonContent, '#haveAMRN-2');
+    await checkOptionAndContinue(page, commonContent, '#haveContactedDWP-no');
+    await expect(page.getByText(contactDWPContent.title).first()).toBeVisible();
+    await page.getByText(contactDWPContent.govuk).first().click();
+    await page.waitForURL('https://www.gov.uk/');
+  });
+});
