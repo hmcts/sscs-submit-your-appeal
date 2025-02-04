3b187ab9a376f6abb2ebc84dee0ab78deebca37d
rtiru-hmcts2 <rohith.tiru2@hmcts.net>
2025-02-04 11:04:15 +0000

    converted tabs to spaces.

--- test/e2e/e2e-playwright-structure-bp/pages/appellant-in-mainland-uk.page.ts
+++ test/e2e/e2e-playwright-structure-bp/pages/appellant-in-mainland-uk.page.ts
@@ -1,25 +1,25 @@
 import { BasePage } from './ibca-base.page';
 
 export class AppellantInMainlandUkPage extends BasePage {
-	defaultPageContent: any = {
-		"heading": "Do you live in England, Scotland or Wales?",
-		"bodyContents": []
-	};
+  defaultPageContent: any = {
+    "heading": "Do you live in England, Scotland or Wales?",
+    "bodyContents": []
+  };
 
-	async chooseLanguage(language: string) {
-		const supportedLanguages: any = {
-			'english': 'English only',
-			'englishAndWelsh': 'English and Welsh'
-		}
-		await this.goto('language-preference');
-		await this.page.getByText(supportedLanguages[language]).click();
-		await this.submitPage();
-	}
+  async chooseLanguage(language: string) {
+    const supportedLanguages: any = {
+      'english': 'English only',
+      'englishAndWelsh': 'English and Welsh'
+    }
+    await this.goto('language-preference');
+    await this.page.getByText(supportedLanguages[language]).click();
+    await this.submitPage();
+  }
 
-	async isInCountry(option: boolean) {
-		(option) ?
-			await this.page.getByText('Yes').click() :
-			await this.page.getByText('No').click();
-		await this.submitPage();
-	}
+  async isInCountry(option: boolean) {
+    (option) ?
+      await this.page.getByText('Yes').click() :
+      await this.page.getByText('No').click();
+    await this.submitPage();
+  }
 }
