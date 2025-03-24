fe7ab7ce92df9e1582417f18a10d8871601b8f60
rtiru-hmcts2 <157617586+rtiru-hmcts2@users.noreply.github.com>
2025-03-24 16:02:28 +0000

    Iba changes to master (#1620)
    * initial iba changes.
    
    * SSCSCI-1512: SYA - MVP - IBCA E2E Journey To Create a Case - With Login in English.
    
      As a QA I need a test that would run an execution scenario of creating an IBCA Case from a Citizens point of view.
        The variations of this test would have to be with
          With a valid MRN.
          With an invalid MRN (Older that one month)
          With and invalid MRN Older than 13 months.

---
+++ test/e2e/e2e-playwright-structure-bp/pages/index.ts
@@ -0,0 +1,47 @@
+import { AppellantInMainlandUkPage } from "./appellant-in-mainland-uk.page";
+import { AppellantTextRemindersPage } from "./appellant-text-reminders.page";
+import { IbcaReferenceLateDatePage, IbcaReferenceLateDateReasonPage } from "./check-mrn-date.page";
+import { CheckYourAppealPage } from "./check-your-appeal.page";
+import { ConfirmationPage } from "./confirmation.page";
+import { CreateAccountPage } from "./create-account.page";
+import { DraftAppealsPage } from "./draft-appeals.page";
+import { AppellantContactDetailsPage } from "./enter-appellant-contact-details.page";
+import { AppellantDobPage } from "./enter-appellant-dob.page";
+import { IbcaReferenceNumberPage } from "./enter-appellant-ibca-reference.page";
+import { EnterAppellantNamePage } from "./enter-appellant-name.page";
+import { EnterAppellantRolePage } from "./enter-appellant-role.page";
+import { EvidenceProvidePage } from "./evidence-provide.page";
+import { IndependencePage } from "./independence.page";
+import { LanguagePreferencePage } from "./language-preference.page";
+import { IbcaReferenceDatePage } from "./mrn-date.page";
+import { OtherReasonsForAppealPage } from "./other-reason-for-appealing.page";
+import { ReasonsForAppealPage } from "./reasons-for-appealing.page";
+import { RepresentativePage } from "./representative.page";
+import { ReviewDecisionNotice } from "./review-decision-notice.page";
+import { TheHearingPage } from "./the-hearing.page";
+
+
+export {
+  LanguagePreferencePage,
+  AppellantInMainlandUkPage,
+  IndependencePage,
+  CreateAccountPage,
+  ReviewDecisionNotice,
+  IbcaReferenceNumberPage,
+  IbcaReferenceDatePage,
+  IbcaReferenceLateDatePage,
+  IbcaReferenceLateDateReasonPage,
+  EnterAppellantRolePage,
+  EnterAppellantNamePage,
+  AppellantContactDetailsPage,
+  AppellantDobPage,
+  AppellantTextRemindersPage,
+  RepresentativePage,
+  ReasonsForAppealPage,
+  OtherReasonsForAppealPage,
+  EvidenceProvidePage,
+  TheHearingPage,
+  CheckYourAppealPage,
+  ConfirmationPage,
+  DraftAppealsPage,
+}
