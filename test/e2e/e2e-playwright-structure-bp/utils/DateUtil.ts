1ed483da817ea3e2509c45f9d0c311d10023579c
Nilay <nilay.patel@solirius.com>
2025-04-01 13:01:27 +0100

    ibca-test-fixes (#1631)
    * fixing issues
    
    * fix
    
    * language fix
    
    * date fix

--- test/e2e/e2e-playwright-structure-bp/utils/DateUtil.ts
+++ test/e2e/e2e-playwright-structure-bp/utils/DateUtil.ts
@@ -1,5 +1,5 @@
 export function getDayMonthYear(dateObj: Date) {
-  const day = dateObj.getUTCDate() - 1; // predate by 1 day
+  const day = dateObj.getUTCDate();
   const month = dateObj.getUTCMonth() + 1; // months from 1-12
   const year = dateObj.getUTCFullYear();
   return [day, month, year];
