2a8bbfdda619c253aeef7c178f0eacef29ecc49d
Joshua Gray <joshgray91@gmail.com>
2025-04-28 23:01:39 +0100

    Implement english NI content

--- steps/identity/appellant-contact-details/AppellantContactDetails.js
+++ steps/identity/appellant-contact-details/AppellantContactDetails.js
@@ -20,6 +20,8 @@ const PCL = require('components/postcodeLookup/controller');
 const { isIba } = require('utils/benefitTypeUtils');
 const i18next = require('i18next');
 
+const allowNI = config.get('features.allowNI.enabled');
+
 const usePostcodeChecker = config.get('postcodeChecker.enabled');
 const url = config.postcodeLookup.url;
 const token = config.postcodeLookup.token;
@@ -77,6 +79,9 @@ class AppellantContactDetails extends SaveToDraftStore {
   get form() {
     const fields = this.content.fields;
     const prefix = this.contentPrefix();
+    const ibaPostcodeMessage = allowNI ?
+      'invalidPostcodeIbaNI' :
+      'invalidPostcodeIba';
 
     return this.pcl.schemaBuilder([
       { name: this.pcl.fieldMap.postcodeLookup },
@@ -127,7 +132,7 @@ class AppellantContactDetails extends SaveToDraftStore {
           )
           .joi(
             fields.postCode.error[prefix][
-              isIba(this.req) ? 'invalidPostcodeIba' : 'invalidPostcode'
+              isIba(this.req) ? ibaPostcodeMessage : 'invalidPostcode'
             ],
             customJoi
               .string()
--- steps/identity/appellant-in-mainland-uk/AppellantInMainlandUk.js
+++ steps/identity/appellant-in-mainland-uk/AppellantInMainlandUk.js
@@ -11,6 +11,10 @@ const { titleise } = require('utils/stringUtils');
 const { branch } = require('@hmcts/one-per-page');
 const { isIba } = require('utils/benefitTypeUtils');
 
+const config = require('config');
+
+const allowNI = config.get('features.allowNI.enabled');
+
 class AppellantInMainlandUk extends SaveToDraftStore {
   static get path() {
     return paths.identity.enterAppellantInMainlandUk;
@@ -27,7 +31,9 @@ class AppellantInMainlandUk extends SaveToDraftStore {
   get form() {
     return form({
       inMainlandUk: text.joi(
-        this.content.fields.inMainlandUk.errors.required,
+        allowNI ?
+          this.content.fields.inMainlandUk.errors.requiredNI :
+          this.content.fields.inMainlandUk.errors.required,
         Joi.string().valid([userAnswer.YES, userAnswer.NO]).required()
       )
     });
@@ -37,7 +43,8 @@ class AppellantInMainlandUk extends SaveToDraftStore {
     const content = require(`./content.${i18next.language}`);
 
     return answer(this, {
-      question: this.content.cya.inMainlandUk.question,
+      question: allowNI ? this.content.cya.inMainlandUk.questionNI :
+      this.content.cya.inMainlandUk.question,
       section: sections.appellantDetails,
       answer: titleise(content.cya.inMainlandUk[this.fields.inMainlandUk.value])
     });
