6e5db5be789dea0e9fc5cf5fb6d28cfd08607a6f
Gemma Talbot <gemma.talbot@solirius.com>
2025-09-02 18:08:14 +0100

    add log

--- services/setupSecrets.js
+++ services/setupSecrets.js
@@ -1,12 +1,14 @@
 const config = require('@hmcts/properties-volume').addTo(require('config'));
 const { get, set } = require('lodash');
+const logger = require('logger');
 
 const setSecret = (secretPath, configPath) => {
   // Only overwrite the value if the secretPath is defined
   if (config.has(secretPath)) {
     set(config, configPath, get(config, secretPath));
+    logger.trace('Setting secret with path: ' + secretPath);
   } else {
-    logger.warn('Cannot find secret with path: ' + secretPath);
+    logger.trace('Cannot find secret with path: ' + secretPath);
         }
 };
 
