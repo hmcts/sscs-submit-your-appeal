fc6961a66b23156a19e997e72d6d078cf76db440
Gemma Talbot <gemma.talbot@solirius.com>
2025-09-04 10:52:34 +0100

    add kerv webchat

--- services/setupSecrets.js
+++ services/setupSecrets.js
@@ -12,16 +12,16 @@ const setupSecrets = () => {
   if (config.has('secrets.sscs')) {
     setSecret('secrets.sscs.sscs-redis-connection-string', 'redis.url');
     setSecret('secrets.sscs.sscs-redis-access-key', 'redis.secret');
-    setSecret(
-      'secrets.sscs.idam-sscs-oauth2-client-secret',
-      'services.idam.secret'
-    );
+    setSecret('secrets.sscs.idam-sscs-oauth2-client-secret', 'services.idam.secret');
     setSecret('secrets.sscs.postcode-lookup-token', 'postcodeLookup.token');
-    setSecret(
-      'secrets.sscs.app-insights-connection-string',
-      'appInsights.connectionString'
-    );
+    setSecret('secrets.sscs.app-insights-connection-string', 'appInsights.connectionString');
     setSecret('secrets.sscs.pcq-token-key', 'services.pcq.tokenKey');
+    setSecret('secrets.sscs.webchat-kerv-deployment-en', 'services.kerv.deploymentId.en');
+    setSecret('secrets.sscs.webchat-kerv-deployment-cy', 'services.kerv.deploymentId.cy');
+    setSecret('secrets.sscs.webchat-kerv-genesys-base-url', 'services.kerv.genesysBaseUrl');
+    setSecret('secrets.sscs.webchat-kerv-environment', 'services.kerv.environment');
+    setSecret('secrets.sscs.webchat-kerv-kerv-base-url', 'services.kerv.kervBaseUrl');
+    setSecret('secrets.sscs.webchat-kerv-api-key', 'services.kerv.apiKey');
   }
 };
 
