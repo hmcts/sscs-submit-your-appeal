e96099cb1edcd9b06ffcb50b011fca30807b629e
Katalin Kovacs <Katalin.Kovacs@HMCTS.NET>
2026-01-27 16:55:13 +0000

    update sass and sass-loader

--- webpack/scss.js
+++ webpack/scss.js
@@ -28,6 +28,7 @@ module.exports = {
         {
           loader: 'sass-loader',
           options: {
+            implementation: require('sass'),
             sassOptions: {
               quietDeps: true
             }
