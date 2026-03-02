63aa67fb33249d6768c3f8534700aa16db0ca830
Katalin Kovacs <Katalin.Kovacs@HMCTS.NET>
2026-03-02 17:58:03 +0000

    setup s2s service

--- services/s2s.js
+++ services/s2s.js
@@ -1,17 +1,20 @@
 const config = require('config');
 const logger = require('logger');
 const request = require('superagent');
+const { totp } = require('otplib');
 
 const microservice = config.get('s2s.microservice');
+const s2sSecret = config.get('s2s.secret');
 const s2sUrl = config.get('s2s.url');
 const timeout = config.get('s2s.timeout');
 
 async function generateToken() {
+  const oneTimePassword = totp.generate(s2sSecret);
   try {
     const response = await request
-      .post(`${s2sUrl}/testing-support/lease`)
+      .post(`${s2sUrl}/lease`)
       .set('Content-Type', 'application/json')
-      .send({ microservice })
+      .send({ microservice, oneTimePassword })
       .timeout(timeout);
 
     return response.body;
