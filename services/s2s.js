5cbe346466d25c60087bae2598b51238a014aa15
Katalin Kovacs <Katalin.Kovacs@HMCTS.NET>
2026-03-03 10:32:07 +0000

    update s2s

--- services/s2s.js
+++ services/s2s.js
@@ -1,7 +1,7 @@
 const config = require('config');
 const logger = require('logger');
 const request = require('superagent');
-const { totp } = require('otplib');
+const { authenticator } = require('otplib');
 
 const microservice = config.get('s2s.microservice');
 const s2sSecret = config.get('s2s.secret');
@@ -9,7 +9,7 @@ const s2sUrl = config.get('s2s.url');
 const timeout = config.get('s2s.timeout');
 
 async function generateToken() {
-  const oneTimePassword = totp.generate(s2sSecret);
+  const oneTimePassword = authenticator.generate(s2sSecret);
   try {
     const response = await request
       .post(`${s2sUrl}/lease`)
@@ -17,7 +17,7 @@ async function generateToken() {
       .send({ microservice, oneTimePassword })
       .timeout(timeout);
 
-    return response.body;
+    return response.text;
   } catch (error) {
     logger.trace('Error generateToken', error);
     return '';
