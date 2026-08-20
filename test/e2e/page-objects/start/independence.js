392ad34025febedad65e526fbd9d74fce439330d
gokul-sol <gokul.sridharan@HMCTS.NET>
2026-08-20 13:28:56 +0100

    fix lint

--- test/e2e/page-objects/start/independence.js
+++ test/e2e/page-objects/start/independence.js
@@ -1,3 +1,5 @@
+const { expect } = require('@playwright/test');
+
 async function continueFromIndependance(I, commonContent) {
   await expect(I.getByText('The tribunal is separate from DWP.').first()).toBeVisible();
   await I.getByRole('button', { name: commonContent.continue }).first().click();
