bfdf1fb5ba960ffb9a36f93364172ad6775aa07f
gokul-sol <gokul.sridharan@HMCTS.NET>
2026-08-20 13:24:22 +0100

    test fix

--- test/e2e/page-objects/start/independence.js
+++ test/e2e/page-objects/start/independence.js
@@ -1,4 +1,5 @@
 async function continueFromIndependance(I, commonContent) {
+  await expect(I.getByText('The tribunal is separate from DWP.').first()).toBeVisible();
   await I.getByRole('button', { name: commonContent.continue }).first().click();
 }
 
