1e61285b4b90e57496245d5185558a2671ad6321
gokul-sol <gokul.sridharan@HMCTS.NET>
2026-08-21 09:17:59 +0100

    revert change

--- test/e2e/page-objects/start/independence.js
+++ test/e2e/page-objects/start/independence.js
@@ -1,24 +1,4 @@
-// const { expect } = require('@playwright/test');
-// const independenceContentEn = require('steps/start/independence/content.en');
-// const independenceContentCy = require('steps/start/independence/content.cy');
-
 async function continueFromIndependance(I, commonContent) {
-  // const enText = independenceContentEn.separate;
-  // const cyText = independenceContentCy.separate;
-
-  // const enLocator = I.getByText(enText).first();
-  // const cyLocator = I.getByText(cyText).first();
-
-  // if(await enLocator.count() > 0) {
-  //   await expect(enLocator).toBeVisible();
-  // } else if (await cyLocator.count() > 0) {
-  //   await expect(cyLocator).toBeVisible();
-  // } else {
-  //   //Defaulting to english text if neither locator is found, this should not happen but just in case
-  //   await expect(enLocator).toBeVisible();
-  // }
-
-  await I.wait(10); // Wait for the button to be present
   await I.getByRole('button', { name: commonContent.continue }).first().click();
 }
 
