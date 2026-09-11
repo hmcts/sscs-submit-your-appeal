0373eef9f69eea7876993845ffddb813da34042f
Nitin Prabhu <nitin.prabhu2@hmcts.net>
2026-09-11 15:47:15 +0100

    Added managed redis cache for SYA

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -17,6 +17,12 @@ data "azurerm_subnet" "core_infra_redis_subnet" {
   resource_group_name  = "core-infra-${var.env}"
 }
 
+data "azurerm_subnet" "redis_private_endpoint" {
+  name                 = "core-infra-subnet-2-${var.env}"
+  resource_group_name  = "core-infra-${var.env}"
+  virtual_network_name = "core-infra-vnet-${var.env}"
+}
+
 module "redis-cache" {
   source                        = "git@github.com:hmcts/cnp-module-redis?ref=4.x"
   product                       = var.product
@@ -30,6 +36,42 @@ module "redis-cache" {
   sku_name                      = var.sku_name
   family                        = var.family
   capacity                      = var.capacity
+}
+
+module "managed_redis" {
+  # foreach conditional allows selective deployment to desired environments
+  for_each = toset(contains(["ithc"], var.env) ? [var.env] : [])
+  source   = "git@github.com:hmcts/terraform-module-azure-managed-redis?ref=main"
+
+  product     = var.product
+  component   = var.component
+  env         = var.env
+  location    = var.location
+  common_tags = var.common_tags
+
+  # Performance:
+  sku_name          = var.managed_redis_sku
+  high_availability_enabled = var.managed_redis_high_availability_enabled
+  # Networking:
+  public_network_access   = "Disabled"
+  create_private_endpoint = true
+  subnet_id               = data.azurerm_subnet.redis_private_endpoint.id
+  private_dns_zone_ids    = ["/subscriptions/${var.private_dns_subscription_id}/resourceGroups/core-infra-intsvc-rg/providers/Microsoft.Network/privateDnsZones/privatelink.redis.azure.net"]
+
+  access_keys_authentication_enabled = true
+  persistence_rdb_backup_frequency   = "6h"
+}
+
+
+resource "azurerm_key_vault_secret" "managed_redis_access_key" {
+  name         = "managed-redis-access-key"
+  value        = module.managed_redis.primary_access_key
+  key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
+
+  content_type = "secret"
+  tags = merge(var.common_tags, {
+    "source" : "managed-redis ${module.managed_redis.hostname}"
+  })
 }
 
 resource "azurerm_key_vault_secret" "redis_access_key" {
--- infrastructure/prod.tfvars
+++ infrastructure/prod.tfvars
@@ -1,3 +1,5 @@
 sku_name = "Premium"
 family   = "P"
 capacity = "1"
+managed_redis_sku = "Balanced_B3"
+managed_redis_high_availability_enabled = true
