f99357e9767aa3182cc804d8812b15fa92bea4f9
Nitin Prabhu <nitin.prabhu2@hmcts.net>
2026-09-14 15:12:39 +0100

    Make resource unique

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -71,7 +71,7 @@ resource "azurerm_key_vault_secret" "managed_redis_access_key" {
   key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
 }
 
-resource "azurerm_key_vault_secret" "redis_connection_string" {
+resource "azurerm_key_vault_secret" "managed_redis_connection_string" {
   for_each = contains(["ithc"], var.env) ? toset([var.env]) : toset([])
 
   name         = "${var.product}-managed-redis-connection-string"
