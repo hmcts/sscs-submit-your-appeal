860bf650c6d8379c915a8a250fc06cab2c392571
Nitin Prabhu <nitin.prabhu2@hmcts.net>
2026-09-14 15:25:31 +0100

    Fix connection-string secret to create conditionally

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -75,7 +75,7 @@ resource "azurerm_key_vault_secret" "managed_redis_connection_string" {
   for_each = contains(["ithc"], var.env) ? toset([var.env]) : toset([])
 
   name         = "${var.product}-managed-redis-connection-string"
-  value        = "rediss://:${urlencode(module.managed_redis.access_key)}@${module.managed_redis.host_name}:${module.managed_redis.redis_port}?tls=true"
+  value = "rediss://:${urlencode(module.managed_redis[each.value].primary_access_key)}@${module.managed_redis[each.value].host_name}:${module.managed_redis[each.value].redis_port}?tls=true"
   key_vault_id = data.azurerm_key_vault.sscs_key_vault.id
 }
 resource "azurerm_key_vault_secret" "redis_access_key" {
