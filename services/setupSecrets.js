ad0d3f8af7fc212be368baa74bfccab3f8997fbb
Nitin Prabhu <nitin.prabhu2@hmcts.net>
2026-09-16 20:29:35 +0100

    Updated secret config to point to new azure managed redis for all environments

--- services/setupSecrets.js
+++ services/setupSecrets.js
@@ -12,6 +12,8 @@ const setupSecrets = () => {
   if (config.has('secrets.sscs')) {
     setSecret('secrets.sscs.sscs-redis-connection-string', 'redis.url');
     setSecret('secrets.sscs.sscs-redis-access-key', 'redis.secret');
+    setSecret('secrets.sscs.sscs-managed-redis-connection-string', 'redis.url');
+    setSecret('secrets.sscs.sscs-managed-redis-access-key', 'redis.secret');
     setSecret(
       'secrets.sscs.idam-sscs-oauth2-client-secret',
       'services.idam.secret'
