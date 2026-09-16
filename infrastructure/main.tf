f17751fe154fb69d596380988ca02cdb6bcc5204
Nitin Prabhu <142304341+nitinprabhuhmcts@users.noreply.github.com>
2026-09-16 11:22:13 +0100

    Create Azure managed redis for all lower envs (#2166)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -40,7 +40,7 @@ module "redis-cache" {
 
 module "managed_redis" {
   # foreach conditional allows selective deployment to desired environments
-  for_each = toset(contains(["ithc"], var.env) ? [var.env] : [])
+  for_each = toset(contains(["ithc", "aat", "perftest", "demo"], var.env) ? [var.env] : [])
   source   = "git@github.com:hmcts/terraform-module-azure-managed-redis?ref=main"
 
   product     = var.product
@@ -65,7 +65,7 @@ module "managed_redis" {
 
 
 resource "azurerm_key_vault_secret" "managed_redis_access_key" {
-  for_each = contains(["ithc"], var.env) ? toset([var.env]) : toset([])
+  for_each = contains(["ithc", "aat", "perftest", "demo"], var.env) ? toset([var.env]) : toset([])
 
   name         = "${var.product}-managed-redis-access-key"
   value        = module.managed_redis[each.value].primary_access_key
@@ -73,7 +73,7 @@ resource "azurerm_key_vault_secret" "managed_redis_access_key" {
 }
 
 resource "azurerm_key_vault_secret" "managed_redis_connection_string" {
-  for_each = contains(["ithc"], var.env) ? toset([var.env]) : toset([])
+  for_each = contains(["ithc", "aat", "perftest", "demo"], var.env) ? toset([var.env]) : toset([])
 
   name = "${var.product}-managed-redis-connection-string"
 
